1f77923f062a8db7698cc47dc3592342eb751c91
Trey Pendragon <tpendragon@princeton.edu>
2017-08-07 12:46:06 -0700

    Stub out derivative file creation.

---
+++ spec/support/speedup_derivatives.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  config.before(:each) do
+    example_file = Rails.root.join("spec", "fixtures", "files", "example.tif")
+    allow(Hydra::Derivatives::Jpeg2kImageDerivatives).to receive(:create) do |*args|
+      url = args[1][:outputs][0][:url].to_s.gsub("file:","")
+      FileUtils.link(example_file, url, force: true)
+    end
+  end
+end
