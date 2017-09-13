133bb69f6574c5480ca6ad593e019510115a5218
jrgriffiniii@gmail.com <jrg5@princeton.edu>
2017-09-13 11:35:25 -0400

    Extending the test coverage for the Ephemera* Controllers and Decorators

--- spec/decorators/ephemera_template_decorator_spec.rb
+++ spec/decorators/ephemera_template_decorator_spec.rb
@@ -9,13 +9,4 @@ RSpec.describe EphemeraTemplateDecorator do
       expect(resource.decorate).to be_a described_class
     end
   end
-  it 'does not manage files' do
-    expect(decorator.manageable_files?).to be false
-  end
-  it 'does not manage structures' do
-    expect(decorator.manageable_structure?).to be false
-  end
-  it 'exposes the metadata adapter' do
-    expect(resource.decorate.metadata_adapter).to be_a Valkyrie::Persistence::Postgres::MetadataAdapter
-  end
 end
