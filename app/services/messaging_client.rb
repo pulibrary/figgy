47e117159c798053f96f97643833bbc037d25beb
Trey Pendragon <tterrell@princeton.edu>
2017-10-06 18:42:32 -0700

    Fix RabbitMQ messaging. (#333)
    Closes #332

--- app/services/messaging_client.rb
+++ app/services/messaging_client.rb
@@ -1,23 +1,8 @@
 # frozen_string_literal: true
 class MessagingClient
-  class Config < OpenStruct
-    def log_level
-      value = super
-      value = value.constantize if %w[Logger::DEBUG Logger::INFO Logger::WARN Logger::ERROR Logger::FATAL Logger:UNKNOWN].include? value
-      value
-    end
-
-    def log_file
-      value = super
-      value = value.constantize if %w[STDIN STDOUT STDERR].include? value
-      value
-    end
-  end
-
-  attr_reader :amqp_url, :config
-  def initialize(amqp_url, config: {})
+  attr_reader :amqp_url
+  def initialize(amqp_url)
     @amqp_url = amqp_url
-    @config = Config.new(config)
   end
 
   def publish(message)
@@ -28,12 +13,8 @@ class MessagingClient
 
   private
 
-    def bunny_args
-      config.to_h.merge(host: amqp_url)
-    end
-
     def bunny_client
-      @bunny_client ||= Bunny.new(*bunny_args).tap(&:start)
+      @bunny_client ||= Bunny.new(amqp_url).tap(&:start)
     end
 
     def channel
