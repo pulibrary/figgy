e47ffbab3500506ad54338f71a68ba74c88ef1a5
Esmé Cowles <escowles@ticklefish.org>
2017-10-12 21:37:30 -0400

    Index ephemera boxes' collection for ephemera folders

--- app/indexers/collection_indexer.rb
+++ app/indexers/collection_indexer.rb
@@ -7,14 +7,28 @@ class CollectionIndexer
   end
 
   def to_solr
-    return {} unless resource.respond_to?(:member_of_collection_ids)
+    return {} unless collection_titles.any?
     {
-      "member_of_collection_titles_ssim" => collections.map(&:title).to_a
+      "member_of_collection_titles_ssim" => collection_titles
     }
   end
 
+  def collection_titles
+    @collection_titles ||=
+      ephemera_collection_titles.any? ? ephemera_collection_titles : collections.map(&:title).to_a
+  end
+
+  def decorated_resource
+    @decorated_resource ||= resource.decorate
+  end
+
+  def ephemera_collection_titles
+    return [] unless resource.is_a?(EphemeraFolder) && decorated_resource.ephemera_box
+    decorated_resource.ephemera_box.member_of_collections
+  end
+
   def collections
-    return [] if resource.member_of_collection_ids.blank?
+    return [] unless resource.respond_to?(:member_of_collection_ids) && resource.member_of_collection_ids
     @collections ||=
       begin
         query_service.find_references_by(resource: resource, property: :member_of_collection_ids).map(&:decorate)
