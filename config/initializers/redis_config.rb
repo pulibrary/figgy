58e37e844eb03e364d46e8d3d763a768b08e5acc
Trey Pendragon <tpendragon@princeton.edu>
2017-10-17 17:53:47 -0700

    Rubocop.

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'redis'
-config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+config = YAML.safe_load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
 Redis.current = Redis.new(config.merge(thread_safe: true))
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require_relative 'redis_config'
 Sidekiq.configure_server do |config|
   config.redis = Redis.current.client.options
