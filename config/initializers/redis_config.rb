4847e0cbd92ae025c6c66e8c307292b8629abf0a
Trey Pendragon <tpendragon@princeton.edu>
2017-10-17 18:08:33 -0700

    Fix servers.

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 require 'redis'
-config = YAML.safe_load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result, [], [], true)[Rails.env].with_indifferent_access
+config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result, [], [], true)[Rails.env].with_indifferent_access
 Redis.current = Redis.new(config.merge(thread_safe: true))
