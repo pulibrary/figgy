1fb48b44b233eb6bfd27e290e0ddd7707d67e38e
Esmé Cowles <escowles@ticklefish.org>
2017-10-19 07:38:16 -0400

    Reindexing logs progress

--- app/services/reindexer.rb
+++ app/services/reindexer.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 class Reindexer
-  def self.reindex_all
+  def self.reindex_all(logger: Logger.new(STDOUT))
     new(
       solr_adapter: Valkyrie::MetadataAdapter.find(:index_solr),
-      query_service: Valkyrie::MetadataAdapter.find(:postgres).query_service
+      query_service: Valkyrie::MetadataAdapter.find(:postgres).query_service,
+      logger: logger
     ).reindex_all
   end
 
-  attr_reader :solr_adapter, :query_service
-  def initialize(solr_adapter:, query_service:)
+  attr_reader :solr_adapter, :query_service, :logger
+  def initialize(solr_adapter:, query_service:, logger:)
     @solr_adapter = solr_adapter
     @query_service = query_service
+    @logger = logger
   end
 
   def reindex_all
+    logger.info "Clearing Solr"
     solr_adapter.persister.wipe!
+    logger.info "Reindexing all records"
     query_service.custom_queries.memory_efficient_all.each_slice(1000) do |records|
+      logger.info "Indexing #{records.count} records"
       solr_adapter.persister.save_all(resources: records)
     end
+    logger.info "Done"
   end
 end
