12362f2f1d680fd36cf6d78c99f90b70a379978f
Anna Headley <anna3LC@gmail.com>
2017-10-30 10:00:40 -0400

    Clarify IndexCollection

--- app/models/index_collection.rb
+++ app/models/index_collection.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# Model for exposing IIIF Manifests for all Collection resources
+# Model for exposing a IIIF Manifest describing all Collection resources
 class IndexCollection
   # Decorates the object (as this is not a Valkyrie::Resource)
   # @return [IndexCollectionDecorator] an instance of the decorator
