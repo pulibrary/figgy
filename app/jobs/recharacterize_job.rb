ad807e06bde8814ce4c8949622c18fb3355057cb
Esmé Cowles <escowles@ticklefish.org>
2017-11-07 13:37:10 -0500

    Add rake task and background job to re-run characterization

---
+++ app/jobs/recharacterize_job.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+class RecharacterizeJob < ApplicationJob
+  def perform(id)
+    @metadata_adapter = Valkyrie::MetadataAdapter.find(:indexing_persister)
+    r = @metadata_adapter.query_service.find_by(id: Valkyrie::ID.new(id)).decorate
+
+    logger.info "Recharacterizing #{id}"
+    recharacterize(r.file_sets)
+    r.volumes.each do |vol|
+      logger.info "Recharacterizing volume #{vol.id}"
+      recharacterize(vol.file_sets)
+    end
+    logger.info "Recharacterized #{id}"
+  end
+
+  def recharacterize(file_sets)
+    file_sets.each do |file_set|
+      @metadata_adapter.persister.buffer_into_index do |buffered_adapter|
+        Valkyrie::FileCharacterizationService.for(file_node: file_set, persister: buffered_adapter.persister).characterize
+      end
+    end
+  end
+end
