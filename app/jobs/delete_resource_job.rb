9521acabb18978d871cb94cdd4a7d03547487786
Esmé Cowles <escowles@ticklefish.org>
2017-11-24 22:37:17 -0500

    Adding background job to delete resources

---
+++ app/jobs/delete_resource_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+class DeleteResourceJob < ApplicationJob
+  def perform(id)
+    resource = metadata_adapter.query_service.find_by(id: Valkyrie::ID.new(id))
+    indexing_adapter.persister.delete(resource: resource)
+    change_set_persister.delete(change_set: DynamicChangeSet.new(resource))
+  end
+
+  def indexing_adapter
+    Valkyrie::MetadataAdapter.find(:index_solr)
+  end
+
+  def metadata_adapter
+    Valkyrie::MetadataAdapter.find(:postgres)
+  end
+
+  def change_set_persister
+    PlumChangeSetPersister.new(metadata_adapter: metadata_adapter, storage_adapter: Valkyrie.config.storage_adapter)
+  end
+end
