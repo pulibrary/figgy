d4fb2cf740c34be8b1da7d012df23826a29555ee
Shaun Ellis <shaun@sdellis.com>
2017-11-27 16:23:13 -0500

    attempt to fix webpacker issue on CI

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -47,7 +47,7 @@ module.exports = {
     // just be aware of this issue when enabling this option.
     cssSourceMap: false,
   },
-  
+
   build: {
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
@@ -61,17 +61,17 @@ module.exports = {
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
-    
+
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
-    
+
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
