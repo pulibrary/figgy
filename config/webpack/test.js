fbd1f2e8bd6bf23b0a1f4ed0a039d6c8d4d344c2
Shaun Ellis <shaun@sdellis.com>
2017-11-29 11:04:18 -0500

    fixes circleci webpacker issue

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,9 +1,3 @@
-// const merge = require('webpack-merge')
-// const environment = require('./environment')
-// const customConfig = require('./custom')
-//
-// module.exports = merge(environment.toWebpackConfig(), customConfig)
-
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
