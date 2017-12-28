a5f8b0241bdee7010c9bbb9b4dfb34807b2ded1b
Trey Pendragon <tterrell@princeton.edu>
2017-12-28 10:33:02 -0800

    Set visibility based on Date Created (#708)
    Closes #691.

--- app/change_set_persisters/plum_change_set_persister.rb
+++ app/change_set_persisters/plum_change_set_persister.rb
@@ -14,6 +14,7 @@ class PlumChangeSetPersister
     {
       before_save: [
         ApplyRemoteMetadata,
+        ApplyVisibilityByDate,
         CreateFile::Factory.new(file_appender: FileAppender),
         PropagateVisibilityAndState
       ],
---
+++ app/change_set_persisters/plum_change_set_persister/apply_visibility_by_date.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+class PlumChangeSetPersister
+  class ApplyVisibilityByDate
+    attr_reader :change_set_persister, :change_set
+    def initialize(change_set_persister:, change_set:, post_save_resource: nil)
+      @change_set = change_set
+      @change_set_persister = change_set_persister
+    end
+
+    def run
+      return unless change_set.try(:set_visibility_by_date?)
+      return unless date_object
+      change_set.validate(visibility: calculated_visibility, rights_statement: calculated_rights_statement)
+      change_set.sync
+      change_set
+    end
+
+    private
+
+      def date
+        Array.wrap(change_set.resource.primary_imported_metadata.created).first
+      end
+
+      def date_object
+        return unless date
+        Time.zone.parse(date)
+      rescue
+        Rails.logger.warn("Unable to parse created date: #{change_set.resource.primary_imported_metadata.created}")
+        nil
+      end
+
+      def limit_date
+        Time.zone.parse("1924-01-01T00:00:00Z")
+      end
+
+      def public_access?
+        date_object < limit_date
+      end
+
+      def calculated_visibility
+        if public_access?
+          Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+        else
+          Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+        end
+      end
+
+      def calculated_rights_statement
+        if public_access?
+          "http://rightsstatements.org/vocab/NKC/1.0/"
+        else
+          "http://rightsstatements.org/vocab/InC/1.0/"
+        end
+      end
+  end
+end
