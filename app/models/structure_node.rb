bce8676dc96f2e4eb1fcab9e3ca7b2c87701937b
Trey Pendragon <tpendragon@princeton.edu>
2018-01-11 15:59:50 -0800

    Don't persist empty terms in structure_node.

--- app/models/structure_node.rb
+++ app/models/structure_node.rb
@@ -3,4 +3,8 @@ class StructureNode < Valkyrie::Resource
   attribute :label, Valkyrie::Types::Set
   attribute :proxy, Valkyrie::Types::Set.member(Valkyrie::Types::ID.optional)
   attribute :nodes, Valkyrie::Types::Array.member(StructureNode)
+
+  def to_hash
+    super.compact
+  end
 end
---
+++ spec/models/structure_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Structure do
+  let(:node) { described_class.new(nodes: [StructureNode.new]) }
+  describe "#as_json" do
+    it "does not build updated_at/created_at for structure nodes" do
+      expect(node.as_json["nodes"].first).not_to have_key "created_at"
+    end
+  end
+end
