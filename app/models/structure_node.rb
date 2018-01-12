75b7c69a8972576c114b653ac1e6c7b6bf36c661
Trey Pendragon <tpendragon@princeton.edu>
2018-01-11 16:20:53 -0800

    Documentation.

--- app/models/structure_node.rb
+++ app/models/structure_node.rb
@@ -4,6 +4,8 @@ class StructureNode < Valkyrie::Resource
   attribute :proxy, Valkyrie::Types::Set.member(Valkyrie::Types::ID.optional)
   attribute :nodes, Valkyrie::Types::Array.member(StructureNode)
 
+  # Prevents empty updated_at/created_at properties from filling up the postgres
+  # column.
   def to_hash
     super.compact
   end
