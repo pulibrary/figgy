a7f4f8d5bbc995ed3a3d57fba15962a8c15e2335
Anna Headley <anna3LC@gmail.com>
2018-01-23 14:44:52 -0500

    Subsume SKOS schema into common, refs #339

--- app/models/schema/common.rb
+++ app/models/schema/common.rb
@@ -6,8 +6,7 @@ module Schema
     extend ActiveSupport::Concern
 
     def self.attributes
-      SKOS.attributes +
-        DublinCore.attributes +
+      DublinCore.attributes +
         Local.attributes +
         BIBFRAME.attributes +
         Schema::IIIF.attributes +
@@ -18,7 +17,8 @@ module Schema
           :relative_path, # http://scholarsphere.psu.edu/ns#relativePath
           :visibility, # No RDF URI - See hydra-access-controls
           :sort_title, # http://opaquenamespace.org/ns/mods/titleForSort
-          :rights_statement # http://www.europeana.eu/schemas/edm/rights
+          :rights_statement, # http://www.europeana.eu/schemas/edm/rights
+          :portion_note # http://www.w3.org/2004/02/skos/core#scopeNote
         ]
     end
 
