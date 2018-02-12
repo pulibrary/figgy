1c51da1b9ce9d9d7b2967d4b5802c34efb8c600f
Anna Headley <anna3LC@gmail.com>
2018-02-12 12:06:07 -0500

    Make fixity recursive job call non-recursive job

--- app/jobs/check_fixity_recursive_job.rb
+++ app/jobs/check_fixity_recursive_job.rb
@@ -4,9 +4,7 @@ class CheckFixityRecursiveJob < ApplicationJob
 
   def perform
     file_set = query_service.custom_queries.least_recently_updated_file_set
-    original_file_metadata = file_set.run_fixity
-    file_set.file_metadata = file_set.file_metadata.select { |x| !x.original_file? } + Array.wrap(original_file_metadata)
-    metadata_adapter.persister.save(resource: file_set)
+    CheckFixityJob.perform_now(file_set.id)
     CheckFixityRecursiveJob.set(queue: queue_name).perform_later
   end
 
