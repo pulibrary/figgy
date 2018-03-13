2564ea13be86a2b303c9f23b8b64f1b646da85da
Anna Headley <anna3LC@gmail.com>
2018-03-13 11:25:37 -0400

    Add formatting for fixity success valule, fixes #901

--- app/helpers/fixity_dashboard_helper.rb
+++ app/helpers/fixity_dashboard_helper.rb
@@ -3,4 +3,15 @@ module FixityDashboardHelper
   def format_fixity_success_date(date)
     date.nil? ? 'in progress' : date.strftime("%m/%d/%y %I:%M:%S %p %Z")
   end
+
+  def format_fixity_success(val)
+    case val
+    when nil
+      "in progress"
+    when 0
+      "failed"
+    when 1
+      "succeeded"
+    end
+  end
 end
