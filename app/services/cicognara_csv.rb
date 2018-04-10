44f008067c1a0815a5df18866589805c8d59ab3f
Esmé Cowles <escowles@ticklefish.org>
2018-04-10 10:22:39 -0400

    Updating Cicognara export

--- app/services/cicognara_csv.rb
+++ app/services/cicognara_csv.rb
@@ -9,31 +9,35 @@ class CicognaraCSV
 
   def self.values(col_id)
     col = Valkyrie.config.metadata_adapter.query_service.find_by(id: Valkyrie::ID.new(col_id))
-    col.decorate.members.map(&:decorate).map do |r|
-      value(r) if extract_dclnum(r) && r.state.first == "complete"
+    col.decorate.members.select { |r| extract_dclnum(r) && first(r.state) == "complete" }.map do |r|
+      value(r.decorate)
     end
   end
 
   def self.value(r)
-    [extract_dclnum(r), label(r), Rails.application.routes.url_helpers.manifest_scanned_resource_url(r),
-     "Princeton University Library", r.imported_call_number.first, r.source_metadata_identifier.first,
-     r.identifier.first, nil, r.imported_publisher.first, date(r), nil, nil, r.imported_extent.first,
-     r.rights_statement.first.to_s, original?(r)]
+    dclnum = extract_dclnum(r)
+    orig = original?(r)
+    sysid = orig ? dclnum : first(r.source_metadata_identifier)
+    label = orig ? "Microfiche" : "Princeton University Library"
+    contrib = orig ? "Bibliotheca Apostolica Vaticana" : "Princeton University Library"
+
+    [dclnum, label, Rails.application.routes.url_helpers.manifest_scanned_resource_url(r),
+     contrib, first(r.imported_call_number), sysid, first(r.identifier), nil, first(r.imported_publisher),
+     date(r), nil, nil, first(r.imported_extent), first(r.rights_statement).to_s, orig]
+  end
+
+  def self.first(value)
+    Array.wrap(value).first
   end
 
   def self.date(r)
-    Date.parse(r.imported_created.first).strftime("%Y")
+    Date.parse(first(r.imported_created)).strftime("%Y")
   rescue
     nil
   end
 
-  def self.label(r)
-    return "Microfiche" if original?(r)
-    "Princeton University Library"
-  end
-
   def self.original?(r)
-    r.rights_statement.first == 'http://cicognara.org/microfiche_copyright'
+    first(r.rights_statement) == 'http://cicognara.org/microfiche_copyright'
   end
 
   def self.extract_dclnum(r)
