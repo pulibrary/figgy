5f10556c5b5cd82695f7b2a34caeb89e58e904be
James R. Griffin III <jrgriffiniii@gmail.com>
2018-04-11 10:39:04 -0400

    Introducing a shared spec for ScannedResourcesController and SimpleResourcesController; Resolving issues for the nested resource indexing and ChangeSet for FileSet member resources

--- app/controllers/simple_resources_controller.rb
+++ app/controllers/simple_resources_controller.rb
@@ -2,7 +2,7 @@
 
 # A base controller for resources, intended for inheritance
 class SimpleResourcesController < BaseResourceController
-  self.change_set_class = SimpleResourceChangeSet
+  self.change_set_class = DynamicChangeSet
   self.resource_class = SimpleResource
   self.change_set_persister = ::PlumChangeSetPersister.new(
     metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
