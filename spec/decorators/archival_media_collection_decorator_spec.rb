92831d8debb465df583f3c12c517de23aae1b6cf
Eliot Jordan <eliotj@princeton.edu>
2018-05-01 17:17:09 -0500

    Display imported metadata on ArchivalMediaCollection

--- spec/decorators/archival_media_collection_decorator_spec.rb
+++ spec/decorators/archival_media_collection_decorator_spec.rb
@@ -16,4 +16,30 @@ RSpec.describe ArchivalMediaCollectionDecorator do
       expect(decorator.display_attributes[:identifier]).to eq ["ark:/99999/fk4"]
     end
   end
+
+  context 'with imported metadata' do
+    let(:collection) do
+      FactoryBot.build(:archival_media_collection,
+                       imported_metadata: [{
+                         created: "2018-05-02T11:41:06.371-05:00",
+                         language: "eng"
+                       }])
+    end
+
+    describe "#imported_created" do
+      it "maps date to readable string" do
+        expect(decorator.imported_created).to eq ["May 2, 2018"]
+      end
+    end
+    describe "#display_imported_language" do
+      it "maps keys to english strings" do
+        expect(decorator.display_imported_language).to eq ["English"]
+      end
+    end
+    describe "raw imported metadata" do
+      it "is not displayed" do
+        expect(decorator.display_attributes.keys).not_to include :source_metadata
+      end
+    end
+  end
 end
