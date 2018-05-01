98f04096d60f3c22c0994795683f69f8a202dad4
Eliot Jordan <eliotj@princeton.edu>
2018-05-01 14:53:51 -0500

    Update BagPathValidator to check if bag directory exists and allow nil values

--- app/change_sets/archival_media_collection_change_set.rb
+++ app/change_sets/archival_media_collection_change_set.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
-class ArchivalMediaCollectionChangeSet < Valkyrie::ChangeSet
-  include RemoteMetadataProperty
+class ArchivalMediaCollectionChangeSet < Valhalla::ChangeSet
+  apply_workflow(WorkflowRegistry.workflow_for(ArchivalMediaCollection))
   delegate :human_readable_type, to: :model
 
+  include RemoteMetadataProperty
   property :source_metadata_identifier, multiple: false, required: true
   property :bag_path, multiple: false, required: true, virtual: true
   property :visibility, multiple: false, required: false
 
   validates :source_metadata_identifier, presence: true
-  validates_with SourceMetadataIdentifierValidator
   validates_with BagPathValidator
+  validates_with SourceMetadataIdentifierValidator
 
   def primary_terms
     [:source_metadata_identifier, :bag_path]
