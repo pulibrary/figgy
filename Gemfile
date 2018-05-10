3e498f00b08184353d46ff6cf9eb478b2ac67bbf
James R. Griffin III <jrgriffiniii@gmail.com>
2018-05-10 12:48:04 -0400

    Using the BagIt to provide Bag validation within IngestArchivalMediaBagJob::ArchivalMediaBagParser

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 source "https://rubygems.org"
 
 gem "autoprefixer-rails"
+gem 'bagit'
 gem "blacklight"
 gem 'bunny'
 gem 'ddtrace'
--- app/change_set_persisters/plum_change_set_persister/ingest_bag.rb
+++ app/change_set_persisters/plum_change_set_persister/ingest_bag.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class PlumChangeSetPersister
   class IngestBag
     attr_reader :change_set_persister, :change_set, :post_save_resource
