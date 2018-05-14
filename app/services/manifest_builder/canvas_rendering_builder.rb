ad500ee5335bb73979752b0b827acff5f1606522
James R. Griffin III <jrgriffiniii@gmail.com>
2018-05-14 12:18:11 -0400

    Extending the ManifestBuilder::CanvasRenderingBuilder in order to insert links to original files for download

--- app/services/manifest_builder/canvas_rendering_builder.rb
+++ app/services/manifest_builder/canvas_rendering_builder.rb
@@ -2,22 +2,83 @@
 class ManifestBuilder
   class CanvasRenderingBuilder
     attr_reader :record
+    # Constructor
+    # @param record [ManifestBuilder::LeafNode, FileSet]
     def initialize(record)
       @record = record
     end
 
+    # Modify the manifest for the Canvas
+    # @param manifest [IIIFManifest::ManifestBuilder::IIIFManifest::Canvas]
     def apply(manifest)
-      return unless record.try(:ocr_content).present?
+      if record.try(:ocr_content).present?
+        manifest["rendering"] ||= []
+        manifest["rendering"] << {
+          "@id" => helper.polymorphic_url([:text, record]),
+          "format" => "text/plain",
+          "label" => "Download page text"
+        }
+      end
+
+      return unless downloadable?
       manifest["rendering"] ||= []
-      manifest["rendering"] << {
-        "@id" => helper.polymorphic_url([:text, record]),
-        "format" => "text/plain",
-        "label" => "Download page text"
-      }
+      manifest['rendering'] << download_hash
     end
 
+    # Construct a helper Object
+    # @return [ManifestBuilder::ManifestHelper]
     def helper
       @helper || ManifestBuilder::ManifestHelper.new
     end
+
+    private
+
+      # FileSet resource being presented using the IIIF Manifest
+      # @return [FileSet]
+      def resource
+        @record.respond_to?(:resource) ? @record.resource : @record
+      end
+
+      # Can the resource be downloaded by the user?
+      # @return [TrueClass, FalseClass]
+      def downloadable?
+        @record.respond_to?(:resource) || @record.is_a?(FileSet)
+      end
+
+      # Retrieve the URL options for the App.
+      # @return [Hash]
+      def default_url_options
+        Figgy.default_url_options
+      end
+
+      # Retrieve the URL from the App. settings
+      # @return [String]
+      def host
+        default_url_options[:host]
+      end
+
+      # Retrieve the web protocol from the App. settings
+      # @return [String]
+      def protocol
+        default_url_options[:protocol] || 'http'
+      end
+
+      # Retrieve the helper Module for the routes
+      # @return [Module]
+      def url_helpers
+        Valhalla::Engine.routes.url_helpers
+      end
+
+      # Generate a download Hash for the FileSet and members
+      # @return [Hash]
+      def download_hash
+        original_file = resource.original_file
+        original_file_id = original_file.id.to_s
+        {
+          '@id' => url_helpers.download_url(resource_id: resource.id.to_s, id: original_file_id, protocol: protocol, host: host),
+          'label' => 'Download the original file',
+          'format' => original_file.mime_type.first
+        }
+      end
   end
 end
