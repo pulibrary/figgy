91b7b8dcd6f1bd73f275bd04975785f10c049cb7
Eliot Jordan <eliotj@princeton.edu>
2018-05-16 11:24:45 -0500

    Add PublishDerivativesDeletedMessage class to change set persister

--- app/change_set_persisters/change_set_persister.rb
+++ app/change_set_persisters/change_set_persister.rb
@@ -37,7 +37,8 @@ class ChangeSetPersister
         CleanupStructure,
         DeleteReferenced::Factory.new(property: :member_of_vocabulary_id),
         CleanupMembership::Factory.new(property: :member_ids),
-        CleanupMembership::Factory.new(property: :member_of_collection_ids)
+        CleanupMembership::Factory.new(property: :member_of_collection_ids),
+        PublishMessage::Factory.new(operation: :derivatives_delete)
       ],
       after_delete_commit: [
         DeleteMembers::Factory.new(property: :member_ids),
