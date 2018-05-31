66cb6b54ab5cce1496bf870673a8f53e5b1c12ed
James R. Griffin III <jrgriffiniii@gmail.com>
2018-05-31 17:02:41 -0400

    Ensures that the ManifestBuilder::ThumbnailBuilder#file_set handles cases where ManifestBuilder::RootNode#file_set_presenters is return empty Arrays

--- app/services/manifest_builder/thumbnail_builder.rb
+++ app/services/manifest_builder/thumbnail_builder.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 class ManifestBuilder
+  # Builder Class for thumbnail resources within IIIF Presentation Manifests
+  # @see http://iiif.io/api/presentation/2.1/#thumbnail
   class ThumbnailBuilder
     attr_reader :resource
 
-    ##
-    # @param [Resource] resource the Resource being viewed
+    # Constructor
+    # @param resource [Valkyrie::Resource] the Resource being viewed
     def initialize(resource)
       @resource = resource
     end
 
+    # Adds the thumbnail URI to the Manifest object
+    # @param manifest [IIIFManifest::ManifestBuilder::IIIFManifest] the manifest
+    # @return [IIIFManifest::ManifestBuilder::IIIFManifest] the updated manifest
     def apply(manifest)
       manifest["thumbnail"] = thumbnail if thumbnail.present?
       manifest
@@ -16,10 +21,15 @@ class ManifestBuilder
 
     private
 
+      # Construct or retrieve the memoized ManifestHelper Object
+      # @return [ManifestHelper]
       def helper
         @helper ||= ManifestHelper.new
       end
 
+      # Generate the value Hash modeling the thumbnail resource for the Manifest
+      # @see http://iiif.io/api/presentation/2.1/#resource-structure
+      # @return [Hash, nil]
       def thumbnail
         return nil unless thumbnail_id && file_set && file_set.derivative_file
         {
@@ -32,12 +42,18 @@ class ManifestBuilder
         }
       end
 
+      # Retrieve the FileSet Resource for the thumbnail
+      # @return [FileSet, nil]
       def file_set
         @file_set ||= find_thumbnail_file_set(thumbnail_id)
       rescue Valkyrie::Persistence::ObjectNotFoundError
+        return if resource.file_set_presenters.empty?
         @file_set ||= query_service.find_by(id: resource.file_set_presenters.first.id)
       end
 
+      # Retrieve the FileSet resource for a given Valkyrie ID
+      # @param record_id [Valkyrie::ID, String] the ID for the FileSet
+      # @return [FileSet]
       def find_thumbnail_file_set(record_id)
         return unless record_id.present?
         record = query_service.find_by(id: Valkyrie::ID.new(record_id.to_s))
@@ -48,10 +64,14 @@ class ManifestBuilder
         end
       end
 
+      # Retrieve the query service from the metadata adapter
+      # @return [Valkyrie::Persistence::Postgres::QueryService]
       def query_service
         Valkyrie.config.metadata_adapter.query_service
       end
 
+      # Retrieve the ID for the resource used as a thumbnail
+      # @return [Valkyrie::ID]
       def thumbnail_id
         Array.wrap(resource.thumbnail_id).first
       end
