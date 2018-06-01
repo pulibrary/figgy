af15082b20941fd45acddd430ee901ef5b09ed13
James R. Griffin III <jrgriffiniii@gmail.com>
2018-06-01 12:46:38 -0400

    Ensures that IngestFolderLocator#folder_pathname handles cases where the directory cannot be found

--- app/services/ingest_folder_locator.rb
+++ app/services/ingest_folder_locator.rb
@@ -15,21 +15,27 @@ class IngestFolderLocator
     file_system_config[:home]
   end
 
-  # Generate the path to parent directory
+  # Generate the path to the studio directory used for ingestion
   # @return [Pathname]
   def root_path
     Pathname.new(upload_path_value).join("studio_new")
   end
 
+  # Determines whether or not the directory exists
+  # @return [TrueClass, FalseClass]
   def exists?
     folder_location.present?
   end
 
+  # Retrieves the relative path from the studio ingestion directory
+  # @return [Pathname]
   def location
     return unless exists?
     folder_pathname.relative_path_from(root_path)
   end
 
+  # Counts the number of files in the directory
+  # @return [Integer]
   def file_count
     return unless exists?
     Dir.glob(folder_pathname.join("**")).select do |file|
@@ -37,6 +43,8 @@ class IngestFolderLocator
     end.count
   end
 
+  # Counts the number of child directories in the directory
+  # @return [Integer]
   def volume_count
     return unless exists?
     Dir.glob(folder_pathname.join("**")).select do |file|
@@ -44,12 +52,17 @@ class IngestFolderLocator
     end.count
   end
 
+  # Construct or retrieve the memoized path name for the directory
+  # @return [Pathname]
   def folder_pathname
+    return unless exists?
     @folder_pathname ||= Pathname.new(folder_location)
   end
 
   private
 
+    # Construct or retrieve the memoized file system path for the directory whose name matches the ID
+    # @return [String]
     def folder_location
       @folder_location ||= Dir.glob(root_path.join("**/#{id}")).first
     end
