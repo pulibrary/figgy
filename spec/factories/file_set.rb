be32fdef0bd010c67c97fd06b79fb38597d201c7
Trey Pendragon <tpendragon@princeton.edu>
2018-06-07 09:43:24 -0700

    Use PreservationMasterFile

--- spec/factories/file_set.rb
+++ spec/factories/file_set.rb
@@ -30,7 +30,7 @@ FactoryBot.define do
 
     factory :audio_file_set do
       file_metadata [
-        FileMetadata.new(mime_type: "audio/wav", use: Valkyrie::Vocab::PCDMUse.OriginalFile, id: "original"),
+        FileMetadata.new(mime_type: "audio/wav", use: Valkyrie::Vocab::PCDMUse.PreservationMasterFile, id: "original"),
         FileMetadata.new(mime_type: "audio/mp3", use: Valkyrie::Vocab::PCDMUse.ServiceFile, id: "derivative"),
         FileMetadata.new(mime_type: "audio/wav", use: Valkyrie::Vocab::PCDMUse.IntermediateFile, id: "intermediate")
       ]
