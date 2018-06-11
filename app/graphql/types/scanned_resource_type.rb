10673f2162be137762533ce7b1659b43b0bb6201
Anna Headley <anna3LC@gmail.com>
2018-06-11 16:56:24 -0400

    Add rspec-graphql_matchers and label field to scannedResource

--- app/graphql/types/scanned_resource_type.rb
+++ app/graphql/types/scanned_resource_type.rb
@@ -1,8 +1,12 @@
 class Types::ScannedResourceType < Types::BaseObject
-  field :title, [String], null: true
+  field :label, String, null: true
   field :viewing_hint, String, null: true
 
   def viewing_hint
     Array.wrap(super).first
+  end
+
+  def label
+    Array.wrap(object.title).first
   end
 end
---
+++ spec/graphql/types/scanned_resource_type_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe Types::ScannedResourceType do
+  let(:scanned_resource) { FactoryBot.create_for_repository(:scanned_resource, viewing_hint: "individuals", title: ["I'm a little teapot", "short and stout"]) }
+  subject(:type) { described_class.new(scanned_resource, {}) }
+
+  describe "class methods" do
+    subject { described_class }
+
+    # Note! These field names use a javascript-y camel-case variable style
+    it { is_expected.to have_field(:viewingHint).of_type(String) }
+    it { is_expected.to have_field(:label).of_type(String) }
+  end
+
+  describe "#viewing_hint" do
+    it "returns a singular value" do
+      expect(type.viewing_hint).to eq "individuals"
+    end
+  end
+
+  describe "#label" do
+    it "maps to a resource's first title" do
+      expect(type.label).to eq "I'm a little teapot"
+    end
+  end
+end
