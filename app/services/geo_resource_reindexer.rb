c54bf6896bc3839311618a50e561ef899631bfbe
Eliot Jordan <eliotj@princeton.edu>
2018-06-12 17:53:43 -0500

    Add reindexing service for geo resources

---
+++ app/services/geo_resource_reindexer.rb
@@ -0,0 +1,133 @@
+# frozen_string_literal: true
+class GeoResourceReindexer
+  # Reindexes GeoBlacklight by sending record updated messages
+  # for all complete geo resources.
+  # @param optional logger [Logger] instance of logger class
+  def self.reindex_geoblacklight(logger: Logger.new(STDOUT))
+    new(logger: logger, ogm_repo_path: nil).reindex_geoblacklight
+  end
+
+  # Reindexes GeoServer by sending derivative created messages
+  # for all complete geo resources.
+  # @param optional logger [Logger] instance of logger class
+  def self.reindex_geoserver(logger: Logger.new(STDOUT))
+    new(logger: logger, ogm_repo_path: nil).reindex_geoserver
+  end
+
+  # Builds an open geo metadata repository with all complete geo resources.
+  # The repository contains geoblacklight documents and a layers.json file
+  # that matches document identifiers with their location in the directory structure.
+  # @param optional logger [Logger] instance of logger class
+  # @param optional ogm_repo_path [String] path of ogm repository
+  def self.reindex_ogm(logger: Logger.new(STDOUT), ogm_repo_path: "./tmp/edu.princeton.arks")
+    new(logger: logger, ogm_repo_path: ogm_repo_path).reindex_ogm
+  end
+
+  attr_reader :logger, :ogm_repo_path
+  def initialize(logger:, ogm_repo_path:)
+    @logger = logger
+    @ogm_repo_path = ogm_repo_path
+  end
+
+  def reindex_geoblacklight
+    all_geo_resources.each do |resources|
+      resources.each do |resource|
+        begin
+          decorator = resource.decorate
+          messenger.record_updated(decorator)
+          logger.info("Indexed into GeoBlacklight: #{resource.id}")
+        rescue StandardError => e
+          logger.warn("Error: #{e.message}")
+        end
+      end
+    end
+  end
+
+  def reindex_geoserver
+    all_geo_resources.each do |resources|
+      resources.each do |resource|
+        begin
+          decorator = resource.decorate
+          file_set = decorator.geo_members.try(:first)
+          next unless file_set
+          messenger.derivatives_created(file_set)
+          logger.info("Indexed into GeoServer: #{file_set.id}")
+        rescue StandardError => e
+          logger.warn("Error: #{e.message}")
+        end
+      end
+    end
+  end
+
+  def reindex_ogm
+    @layers = {}
+    all_geo_resources.each do |resources|
+      resources.each do |resource|
+        begin
+          save_document(resource: resource)
+          logger.info("GeoBlacklight document created: #{resource.id}")
+        rescue StandardError => e
+          logger.warn("Error: #{e.message}")
+        end
+      end
+    end
+    save_layers_document(layers: @layers) unless @layers.empty?
+  end
+
+  private
+
+    def all_geo_resources
+      [
+        complete_resources(model: RasterResource),
+        complete_resources(model: VectorResource),
+        complete_resources(model: ScannedMap)
+      ]
+    end
+
+    def base_document_path(identifier:)
+      id = identifier.gsub(%r(ark:/\d{5}/), "")
+      id.scan(/.{1,2}/).join("/")
+    end
+
+    def complete_resources(model:)
+      resources = query_service.find_all_of_model(model: model)
+      resources.select { |r| r.state == ["complete"] }
+    end
+
+    def create_directory(identifier:)
+      base = base_document_path(identifier: identifier)
+      directory = "#{ogm_repo_path}/#{base}"
+      FileUtils.mkdir_p directory
+      directory
+    end
+
+    def document_has_errors?(document:)
+      error = document["error"] || document[:error]
+      logger.warn("Error: #{resource.id} #{document}") if error
+      error ? true : false
+    end
+
+    def messenger
+      @messenger ||= EventGenerator.new
+    end
+
+    def query_service
+      Valkyrie.config.metadata_adapter.query_service
+    end
+
+    def save_document(resource:)
+      document = GeoDiscovery::DocumentBuilder.new(resource, GeoDiscovery::GeoblacklightDocument.new).to_hash
+      return if document_has_errors?(document: document)
+      directory = create_directory(identifier: resource.identifier.first)
+      file_path = "#{directory}/geoblacklight.json"
+      File.write(file_path, JSON.pretty_generate(document))
+      identifier = resource.identifier.first
+      @layers[identifier] = base_document_path(identifier: identifier)
+    end
+
+    def save_layers_document(layers:)
+      path = "#{ogm_repo_path}/layers.json"
+      output = JSON.pretty_generate(Hash[layers.sort])
+      File.write(path, output)
+    end
+end
---
+++ spec/services/geo_resource_reindexer_spec.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+require "rails_helper"
+include ActionDispatch::TestProcess
+
+RSpec.describe GeoResourceReindexer do
+  let(:geo_work) do
+    FactoryBot.build(:vector_resource,
+                     title: "Geo Work",
+                     coverage: coverage.to_s,
+                     visibility: "open",
+                     state: "complete",
+                     identifier: "ark:/99999/fk4")
+  end
+  let(:coverage) { GeoCoverage.new(43.039, -69.856, 42.943, -71.032) }
+  let(:change_set) { VectorResourceChangeSet.new(geo_work) }
+  let(:change_set_persister) { ChangeSetPersister.new(metadata_adapter: metadata_adapter, storage_adapter: Valkyrie.config.storage_adapter) }
+  let(:metadata_adapter) { Valkyrie.config.metadata_adapter }
+  let(:logger) { instance_double(Logger, info: nil, warn: nil) }
+
+  before do
+    change_set_persister.save(change_set: change_set)
+  end
+
+  describe "#reindex_geoblacklight" do
+    let(:messenger) { instance_double(EventGenerator) }
+
+    before do
+      allow(EventGenerator).to receive(:new).and_return(messenger)
+      allow(messenger).to receive(:record_updated)
+    end
+
+    context "with a valid geo resource" do
+      it "sends an updated record message" do
+        described_class.reindex_geoblacklight(logger: logger)
+        expect(messenger).to have_received(:record_updated)
+      end
+    end
+
+    context "with a geo resource that throws an exception" do
+      let(:resource) { instance_double(VectorResource, state: "complete") }
+
+      before do
+        allow(EventGenerator).to receive(:new).and_raise("error")
+      end
+
+      it "does not send an updated record message" do
+        described_class.reindex_geoblacklight(logger: logger)
+        expect(messenger).not_to have_received(:record_updated)
+      end
+    end
+  end
+
+  describe "#reindex_geoserver" do
+    let(:file) { fixture_file_upload("files/vector/geo.json", "application/vnd.geo+json") }
+    let(:change_set) { VectorResourceChangeSet.new(geo_work, files: [file]) }
+    let(:messenger) { instance_double(EventGenerator) }
+
+    before do
+      allow(EventGenerator).to receive(:new).and_return(messenger)
+      allow(messenger).to receive(:derivatives_created)
+    end
+
+    context "with a valid geo resource and geo member" do
+      it "sends a derivatives created message" do
+        described_class.reindex_geoserver(logger: logger)
+        expect(messenger).to have_received(:derivatives_created)
+      end
+    end
+
+    context "with a geo resource that throws an exception" do
+      before do
+        allow(EventGenerator).to receive(:new).and_raise("error")
+      end
+
+      it "does not send a derivatives created message" do
+        described_class.reindex_geoserver(logger: logger)
+        expect(messenger).not_to have_received(:derivatives_created)
+      end
+    end
+  end
+
+  describe "#reindex_ogm" do
+    let(:ogm_repo_path) { "./tmp/edu.princeton.arks" }
+
+    context "with a valid geo resource" do
+      after do
+        FileUtils.rm_r(ogm_repo_path)
+      end
+
+      it "creates an OpenGeoMetadata repository and a layers.json file" do
+        described_class.reindex_ogm(logger: logger, ogm_repo_path: ogm_repo_path)
+        layers = JSON.parse(File.read("#{ogm_repo_path}/layers.json"))
+        expect(File).to exist("#{ogm_repo_path}/fk/4/geoblacklight.json")
+        expect(layers["ark:/99999/fk4"]).to eq("fk/4")
+      end
+    end
+
+    context "when a geo resource has an invalid geoblacklight document" do
+      let(:geo_work) { FactoryBot.build(:vector_resource) }
+
+      it "does not save a geoblacklight document in the ogm repository" do
+        described_class.reindex_ogm(logger: logger, ogm_repo_path: ogm_repo_path)
+        expect(File).not_to exist("#{ogm_repo_path}/fk/4/geoblacklight.json")
+      end
+    end
+
+    context "with a geo resource that throws an exception" do
+      before do
+        allow(GeoDiscovery::DocumentBuilder).to receive(:new).and_raise("error")
+      end
+
+      it "logs the exception and does not save a geoblacklight document in the ogm repository" do
+        described_class.reindex_ogm(logger: logger, ogm_repo_path: ogm_repo_path)
+        expect(File).not_to exist("#{ogm_repo_path}/fk/4/geoblacklight.json")
+        expect(logger).to have_received(:warn)
+      end
+    end
+  end
+end
