9ce676c309752cd4cd0f77d5ab7c0c69a7f68923
Anna Headley <anna3LC@gmail.com>
2018-06-29 14:23:52 -0400

    Raise actionmailer deliver errors in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,4 +6,5 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = { address: "lib-ponyexpr.princeton.edu" }
   config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST", "localhost"), protocol: ENV.fetch("APPLICATION_HOST_PROTOCOL", "http") }
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
 end
