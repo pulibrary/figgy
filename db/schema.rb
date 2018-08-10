3d4f40f7bde40d2169adebe8d158ba0aa19a4c9b
Trey Pendragon <tpendragon@princeton.edu>
2018-08-10 15:06:26 -0700

    Update to RC2.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306193020) do
+ActiveRecord::Schema.define(version: 20180810215750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180306193020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "internal_resource"
+    t.integer "lock_version"
     t.index "metadata jsonb_path_ops", name: "index_orm_resources_on_metadata_jsonb_path_ops", using: :gin
     t.index ["internal_resource"], name: "index_orm_resources_on_internal_resource"
     t.index ["metadata"], name: "index_orm_resources_on_metadata", using: :gin
