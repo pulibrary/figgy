b87fb147b10ddb2d0b72517e4aca4c8c6efa11fc
Eliot Jordan <eliotj@princeton.edu>
2018-08-21 14:00:09 -0500

    Improve geoserver delete message behavior

--- spec/services/event_generator/geoserver_event_generator_spec.rb
+++ spec/services/event_generator/geoserver_event_generator_spec.rb
@@ -23,9 +23,9 @@ RSpec.describe EventGenerator::GeoserverEventGenerator do
   end
 
   describe "#derivatives_deleted" do
-    it "publishes a persistent JSON message" do
+    it "publishes two persistent JSON messages, one for each workspace" do
       event_generator.derivatives_deleted(record)
-      expect(rabbit_connection).to have_received(:publish)
+      expect(rabbit_connection).to have_received(:publish).twice
     end
   end
 
