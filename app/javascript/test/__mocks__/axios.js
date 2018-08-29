2c1cc5bf878126bb9d12cd003451f189a71eff62
Shaun Ellis <shaun@sdellis.com>
2018-08-29 16:29:02 -0400

    removes tests as these are now done in LUX

--- app/javascript/test/__mocks__/axios.js
+++
@@ -1,9 +0,0 @@
-import manifest from "./mock-manifest"
-
-const axios = {
-  get: () => new Promise(res => res({ data: manifest }) ),
-  defaults: { headers: { common: { 'X-CSRF-Token': null, 'Accept': null } } },
-  patch: () => new Promise(res => res({ status: 200 }) ),
-  all: () => new Promise(res => res({ data: 'all' }) )
-}
-export default axios
