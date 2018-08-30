ade2e90ac68fe4540ad1048580313578c6ab76ec
Eliot Jordan <eliotj@princeton.edu>
2018-08-30 11:55:42 -0500

    Add graphql ScannedMapType

--- app/graphql/types/resource.rb
+++ app/graphql/types/resource.rb
@@ -2,7 +2,7 @@
 module Types::Resource
   include Types::BaseInterface
   description "A resource in the system."
-  orphan_types Types::ScannedResourceType, Types::FileSetType
+  orphan_types Types::ScannedResourceType, Types::FileSetType, Types::ScannedMapType
 
   field :id, String, null: true
   field :label, String, null: true
