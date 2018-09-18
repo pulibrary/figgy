5ad18a514939f043f56073cb321985952e4f1715
Anna Headley <anna3LC@gmail.com>
2018-09-18 10:02:14 -0400

    Allow for orphaned file sets in fixity dashboard
    fixes #1376

---
+++ spec/views/fixity_dashboard/_fixity_table.html.erb_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "fixity_dashboard/_fixity_table" do
+  context "with an orphaned file set" do
+    let(:file_set) { FactoryBot.create_for_repository(:file_set, title: "Page 1") }
+    let(:original_file) { instance_double FileMetadata }
+
+    before do
+      allow(file_set).to receive(:original_file).and_return(original_file)
+      allow(original_file).to receive(:fixity_success).and_return(nil)
+      allow(original_file).to receive(:fixity_last_success_date).and_return(nil)
+      render partial: "fixity_table", locals: { resources: [file_set.decorate] }
+    end
+
+    it "links to file set without parent" do
+      expect(rendered).to have_link "Page 1"
+    end
+  end
+end
