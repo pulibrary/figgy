9e02db21da94bb0bb94cafb157c8afe14cf9564b
hackmastera <anna.headley@gmail.com>
2018-09-21 13:27:11 -0400

    Update link text for members of a collection (#1900)
    gives number of members,
    uses word "items" instead of "members", which was confusing
    fixes #1781

--- spec/features/archival_media_collection_spec.rb
+++ spec/features/archival_media_collection_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "Refresh" do
       collection = persister.save(resource: FactoryBot.build(:archival_media_collection))
       persister.save(resource: FactoryBot.build(:complete_media_resource, member_of_collection_ids: [collection.id]))
       visit "catalog/#{collection.id}"
-      expect(page).to have_link "View Members List"
+      expect(page).to have_link "View all 1 items in this collection"
       expect(page).to have_link "View ARK / Component ID report"
       expect(page).to have_link "Edit This Archival Media Collection", href: edit_archival_media_collection_path(collection)
       expect(page).to have_link "Delete This Archival Media Collection", href: archival_media_collection_path(collection)
