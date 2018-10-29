e91c74a1e220d9f9a9c4cf6bc9f355464849914c
Anna Headley <anna3LC@gmail.com>
2018-10-29 14:53:34 -0400

    Delete sections of manifest_service_locator_v3 that aren't used yet
    They can be migrated as-needed as we move to v3 completely

--- app/services/manifest_builder/manifest_service_locator_v3.rb
+++ app/services/manifest_builder/manifest_service_locator_v3.rb
@@ -21,37 +21,6 @@ class ManifestBuilder
         )
       end
 
-      # Provides the builders injected into the factory for manifests of collections
-      # @see IIIFManifest::ManifestServiceLocator#collection_manifest_builder
-      # @return [IIIFManifest::ManifestBuilder::CompositeBuilderFactory] the factory of multiple builders
-      def collection_manifest_builders
-        composite_builder_factory.new(
-          record_property_builder,
-          child_manifest_builder_factory,
-          see_also_builder,
-          license_builder,
-          thumbnail_builder,
-          rendering_builder,
-          composite_builder: composite_builder
-        )
-      end
-
-      # Provides the builders injected into the factory for manifests of sammelbands
-      # @see IIIFManifest::ManifestServiceLocator#sammelband_manifest_builders
-      # @return [IIIFManifest::ManifestBuilder::CompositeBuilderFactory] the factory of multiple builders
-      def sammelband_manifest_builders
-        composite_builder_factory.new(
-          record_property_builder,
-          sammelband_sequence_builder,
-          structure_builder,
-          see_also_builder,
-          license_builder,
-          thumbnail_builder,
-          rendering_builder,
-          composite_builder: composite_builder
-        )
-      end
-
       ##
       # Class accessor for the "see also" builder
       # @return [Class]
@@ -91,22 +60,6 @@ class ManifestBuilder
         )
       end
 
-      def start_canvas_builder
-        IIIFManifest::ManifestServiceLocator::InjectedFactory.new(
-          ::ManifestBuilder::StartCanvasBuilder,
-          canvas_builder: canvas_builder
-        )
-      end
-
-      ##
-      # Objects are either in 'manifests' or 'collections' depending on if the
-      # child resource is a IIIF::Manifest or a IIIF::Collection. This special
-      # class will pick the builder which puts it in the right place. Replace
-      # when we put everything in `members` per IIIF 2.1.
-      def child_manifest_builder
-        ConditionalCollectionManifest.new(manifest_builder: real_child_manifest_builder, collection_builder: child_collection_builder)
-      end
-
       ##
       # Builder to add thumbnails to manifests.
       def thumbnail_builder
@@ -123,73 +76,6 @@ class ManifestBuilder
           choice_builder: choice_builder,
           iiif_annotation_page_factory: iiif_annotation_page_factory
         )
-      end
-
-      ##
-      # Builder for a manifest which is a sub-item in a collection.
-      def real_child_manifest_builder
-        IIIFManifest::ManifestServiceLocator::InjectedFactory.new(
-          IIIFManifest::ManifestBuilder,
-          builders:
-          composite_builder_factory.new(
-            child_record_property_builder,
-            thumbnail_builder,
-            composite_builder: composite_builder
-          ),
-          top_record_factory: iiif_manifest_factory
-        )
-      end
-
-      ##
-      # Builder for record properties to be applied when rendering as a
-      # "manifest" sub-item in a collection.
-      def child_record_property_builder
-        ManifestBuilder::ChildRecordPropertyBuilder
-      end
-
-      ##
-      # Builder for collections which are sub-collections.
-      def child_collection_builder
-        IIIFManifest::ManifestServiceLocator::InjectedFactory.new(
-          CollectionManifestBuilder,
-          builders:
-          composite_builder_factory.new(
-            record_property_builder,
-            see_also_builder,
-            license_builder,
-            rendering_builder,
-            composite_builder: composite_builder
-          ),
-          top_record_factory: iiif_collection_factory
-        )
-      end
-
-      def sequence_factory
-        IIIFManifest::ManifestBuilder::IIIFManifest::Sequence
-      end
-
-      class CollectionManifestBuilder < IIIFManifest::ManifestBuilder
-        def apply(collection)
-          collection["collections"] ||= []
-          collection["collections"] << to_h
-          collection
-        end
-      end
-
-      class ConditionalCollectionManifest
-        attr_reader :manifest_builder, :collection_builder
-        def initialize(manifest_builder:, collection_builder:)
-          @manifest_builder = manifest_builder
-          @collection_builder = collection_builder
-        end
-
-        def new(work)
-          if work.is_a?(ManifestBuilder::CollectionNode)
-            collection_builder.new(work)
-          else
-            manifest_builder.new(work)
-          end
-        end
       end
     end
   end
