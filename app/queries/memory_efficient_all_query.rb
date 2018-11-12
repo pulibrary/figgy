a60ed58dd85dd41e10f6be26d8ae6c10f921b402
Trey Pendragon <tpendragon@princeton.edu>
2018-11-12 09:08:07 -0800

    Use cursors over streams.

--- app/queries/memory_efficient_all_query.rb
+++ app/queries/memory_efficient_all_query.rb
@@ -15,9 +15,9 @@ class MemoryEfficientAllQuery
 
   def memory_efficient_all(except_models: [])
     connection.transaction(savepoint: true) do
-      relation = orm_class
+      relation = orm_class.use_cursor
       relation = relation.exclude(internal_resource: Array(except_models).map(&:to_s)) if except_models.present?
-      relation.stream.lazy.map do |object|
+      relation.lazy.map do |object|
         resource_factory.to_resource(object: object)
       end
     end
