529e3eed8df684c255ba69effb0321a2a0b52c23
Trey Pendragon <tpendragon@princeton.edu>
2018-11-16 14:28:07 -0800

    No instance variables in ChangeSetPersister
    Fixes thread-safety when saving multiple resources through the same
    ChangeSetPersister. Closes #2190

--- app/change_set_persisters/change_set_persister.rb
+++ app/change_set_persisters/change_set_persister.rb
@@ -99,11 +99,11 @@ class ChangeSetPersister
     end
 
     def buffer_into_index
+      delayed_queue = nil
       metadata_adapter.persister.buffer_into_index do |buffered_adapter|
         with(metadata_adapter: buffered_adapter) do |buffered_changeset_persister|
           yield(buffered_changeset_persister)
-          @created_file_sets = buffered_changeset_persister.created_file_sets
-          @delayed_queue = buffered_changeset_persister.delayed_queue
+          delayed_queue = buffered_changeset_persister.delayed_queue
         end
       end
       delayed_queue.run
