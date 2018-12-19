f141e72544e273409f3560e6e6e9f87c92a939c1
Eliot Jordan <eliotj@princeton.edu>
2018-12-19 11:16:35 -0600

    Fix bounding box display on geo resource edit page

--- spec/helpers/bounding_box_helper_spec.rb
+++ spec/helpers/bounding_box_helper_spec.rb
@@ -3,11 +3,13 @@ require "rails_helper"
 
 describe BoundingBoxHelper do
   let(:property) { :coverage }
-  let(:change_set) { ScannedMapChangeSet.new(ScannedMap.new) }
+  let(:coverage) { ["northlimit=37.894363; eastlimit=-121.988754; southlimit=37.622934; westlimit=-122.481766; units=degrees; projection=EPSG:4326"] }
+  let(:change_set) { ScannedMapChangeSet.new(ScannedMap.new(coverage: coverage)) }
 
   describe "#bbox_input" do
     it "builds bounding box selector" do
       expect(helper.bbox_input(property, change_set)).to include("data-input-id='scanned_map_coverage'")
+      expect(helper.bbox_input(property, change_set)).to include("data-coverage='#{coverage}'")
     end
   end
 
