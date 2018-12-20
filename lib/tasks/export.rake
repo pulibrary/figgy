97597160294f8c640288ed27e9fa388a1b295766
Esmé Cowles <escowles@ticklefish.org>
2018-12-20 17:12:20 -0500

    Adding background job and rake task to export Bags

--- lib/tasks/export.rake
+++ lib/tasks/export.rake
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 namespace :export do
-  desc "Imports a resource from Plum"
+  desc "Exports an object to disk in a BagIt bag"
+  task bag: :environment do
+    id = ENV["ID"]
+    abort "usage: rake export:bag ID=[object id]" unless id
+
+    ExportBagJob.perform_now(id)
+  end
+
+  desc "Exports Cicognara resource metadata to CSV"
   task cicognara: :environment do
     coll = ENV["COLL"]
     abort "usage: rake export:cicognara COLL=[collection id]" unless coll
---
+++ spec/jobs/export_bag_job_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require "rails_helper"
+include ActionDispatch::TestProcess
+
+RSpec.describe ExportBagJob do
+  describe ".perform" do
+    let(:resource) { FactoryBot.create_for_repository(:scanned_resource, files: [file]) }
+    let(:file) { fixture_file_upload("files/abstract.tiff", "image/tiff") }
+    let(:bag_metadata) { Valkyrie::MetadataAdapter.find(:bags).for(bag_id: resource.id) }
+    let(:bag_storage) { Valkyrie::StorageAdapter.find(:bags) }
+    let(:bag_query) { bag_metadata.query_service }
+
+    it "exports the object" do
+      described_class.perform_now(resource.id)
+      bag_resource = bag_query.find_by(id: resource.id)
+      bag_fileset = bag_query.find_by(id: bag_resource.member_ids.first)
+      file_id = bag_fileset.original_file.file_identifiers.first
+      expect {  bag_storage.for(bag_id: bag_resource.id).find_by(id: file_id) }.not_to raise_error
+    end
+  end
+end
