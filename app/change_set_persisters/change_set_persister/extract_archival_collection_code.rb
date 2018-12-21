7dadea1ad2c77c82090763e5259d54098f6cf9cc
Anna Headley <anna3LC@gmail.com>
2018-12-21 09:33:34 -0500

    Accommodate more forms of finding aid IDs

--- app/change_set_persisters/change_set_persister/extract_archival_collection_code.rb
+++ app/change_set_persisters/change_set_persister/extract_archival_collection_code.rb
@@ -10,25 +10,25 @@ class ChangeSetPersister
     def run
       return unless change_set.respond_to?(:source_metadata_identifier)
       return unless change_set.model.respond_to?(:archival_collection_code)
-      return unless updated_values
+      return unless updated_values?
       return if PulMetadataServices::Client.bibdata?(change_set.source_metadata_identifier)
       change_set.model.archival_collection_code = extract_collection_code(change_set.source_metadata_identifier)
       change_set
     end
 
+    def extract_collection_code(pulfa_id)
+      m = pulfa_id.match(/^([a-zA-Z0-9.-]+)_.*/)
+      m[1] if m
+    end
+
     private
 
       # either the source metadata identifier has changed or we're refreshing remote metadata
-      def updated_values
+      def updated_values?
         change_set.changed?(:source_metadata_identifier) || (
           change_set.respond_to?(:apply_remote_metadata?) &&
           change_set.apply_remote_metadata?
         )
-      end
-
-      def extract_collection_code(pulfa_id)
-        m = pulfa_id.match(/^([a-zA-Z]+[0-9]+)_.*/)
-        m[1] if m
       end
   end
 end
