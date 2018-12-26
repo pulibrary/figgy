b5b3796a829b93f8a4d5787b9a1b21209e882856
Anna Headley <anna3LC@gmail.com>
2018-12-26 16:03:13 -0500

    Add finding aids updater with rake tasks, cron and queuing
    fixes #2324

---
+++ app/jobs/finding_aids_update_job.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class FindingAidsUpdateJob < ApplicationJob
+  # Update given resource with finding aids metadata
+  # @param id <String>
+  def perform(id:)
+    resource = query_service.find_by(id: id)
+    change_set = DynamicChangeSet.new(resource)
+    change_set.validate(refresh_remote_metadata: true)
+    logger.info "Refreshing pulfa metadata for #{resource.id}, #{resource.source_metadata_identifier}"
+    persister.save(change_set: change_set)
+  end
+
+  private
+
+    def persister
+      @persister ||= ChangeSetPersister.new(metadata_adapter: metadata_adapter, storage_adapter: Valkyrie.config.storage_adapter)
+    end
+
+    def metadata_adapter
+      @metadata_adapter ||= Valkyrie.config.metadata_adapter
+    end
+
+    def query_service
+      @query_service ||= metadata_adapter.query_service
+    end
+end
---
+++ app/services/finding_aids_updater.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class FindingAidsUpdater
+  def yesterday
+    yesterdays_date = Time.zone.yesterday
+    SvnParser.new.updated_collection_codes(yesterdays_date).each do |code|
+      resources = query_service.custom_queries.find_by_string_property(property: :archival_collection_code, value: code)
+      # there might be multiple resources with the same collection code
+      resources.each do |resource|
+        FindingAidsUpdateJob.perform_later(id: resource.id.to_s)
+      end
+    end
+  end
+
+  def all
+    query_service.find_all_of_model(model: ScannedResource).each do |resource|
+      next unless resource.source_metadata_identifier.present?
+      next if PulMetadataServices::Client.bibdata?(resource.source_metadata_identifier.first)
+      FindingAidsUpdateJob.perform_later(id: resource.id.to_s)
+    end
+  end
+
+  private
+
+    class SvnParser
+      # @param [Date] date
+      def updated_collection_codes(date)
+        svn_config = Rails.application.config_for :svn
+        date = date.to_formatted_s(:iso8601)
+        stdout, status = Open3.capture2("svn diff --summarize -r {#{date}}:HEAD --username #{svn_config['user']} --password #{svn_config['pass']} #{File.join(svn_config['url'], 'pulfa/trunk/eads')}")
+        raise StandardError unless status.success?
+        parse_collection_ids(stdout)
+      end
+
+      def parse_collection_ids(svn_output)
+        svn_output.split("\n").map do |line|
+          line.rpartition("/").last.partition(".").first
+        end
+      end
+    end
+
+    def query_service
+      @query_service ||= Valkyrie.config.metadata_adapter.query_service
+    end
+end
