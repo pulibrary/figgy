b1ec97b8c57d6053654a0d5d21414539b2293f72
Trey Pendragon <tpendragon@princeton.edu>
2019-02-08 13:39:47 -0800

    Remove Coveralls
    CircleCI has access to the simplecov HTML report, and will now error if
    it drops below 100%.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 ENV["RACK_ENV"] = "test"
 require "simplecov"
-require "coveralls"
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  [
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-)
 SimpleCov.start "rails"
 
 require File.expand_path("../../config/environment", __FILE__)
