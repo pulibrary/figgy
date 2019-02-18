3c5d6aa0eab7e21c6d95bb0ae8fdd4d1ab82a2c9
Anna Headley <anna3LC@gmail.com>
2019-02-18 13:13:07 -0500

    Refactor to split search more cleanly from processing

--- app/controllers/bulk_edit_controller.rb
+++ app/controllers/bulk_edit_controller.rb
@@ -8,34 +8,32 @@ class BulkEditController < ApplicationController
   end
 
   def resources_update
-    batch_size = params["batch_size"] || 50
     args = {}.tap do |hash|
       hash[:mark_complete] = (params["mark_complete"] == "1")
     end
-    builder_params = { q: params["search_params"]["q"], f: params["search_params"]["f"] }
-    builder = search_builder.with(builder_params)
-    builder.rows = batch_size
-    resources_count = spawn_update_jobs(builder, args)
+    batches = prepare_batches
+    batches.each do |ids|
+      BulkUpdateJob.perform_later(ids: ids, args: args)
+    end
+    resources_count = batches.map(&:count).reduce(:+)
     flash[:notice] = "#{resources_count} resources were queued for bulk update."
     redirect_to root_url
   end
 
   private
 
-    # collects batches of ids then enqueues jobs
-    def spawn_update_jobs(builder, args)
-      batches = []
-      num_results = 0
-      loop do
-        response = repository.search(builder)
-        num_results = response["response"]["numFound"]
-        batches << response.documents.map(&:id)
-        break if (builder.page * builder.rows) >= num_results
-        builder.page += 1
+    # Prepare / execute the search and process into id arrays
+    def prepare_batches
+      builder_params = { q: params["search_params"]["q"], f: params["search_params"]["f"] }
+      builder = search_builder.with(builder_params)
+      builder.rows = params["batch_size"] || 50
+      [].tap do |batches|
+        loop do
+          response = repository.search(builder)
+          batches << response.documents.map(&:id)
+          break if (builder.page * builder.rows) >= response["response"]["numFound"]
+          builder.page += 1
+        end
       end
-      batches.each do |ids|
-        BulkUpdateJob.perform_later(ids: ids, args: args)
-      end
-      num_results
     end
 end
