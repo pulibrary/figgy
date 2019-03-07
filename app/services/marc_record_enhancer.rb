55e7bfcc1e69eb162677e07074996ee0d439995d
Anna Headley <anna3LC@gmail.com>
2019-03-07 12:21:57 -0500

    Add marc record enhancer service

---
+++ app/services/marc_record_enhancer.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+class MarcRecordEnhancer
+  attr_accessor :marc, :resource
+  def initialize(marc:, resource:)
+    @marc = marc
+    @resource = resource
+  end
+
+  # Factory
+  # @param [Resource] any resource with source_metadata_identifier
+  # @return [MarcRecordEnhancer]
+  def self.for(resource)
+    return unless resource.try(:source_metadata_identifier)
+    bibid = resource.source_metadata_identifier.first
+    return unless PulMetadataServices::Client.bibdata? bibid
+    xml_str = PulMetadataServices::Client.retrieve_from_bibdata(bibid)
+    record = MARC::XMLReader.new(StringIO.new(xml_str), parser: "magic").first
+    new(marc: record, resource: resource)
+  end
+
+  def enhance_cicognara
+    add_856es
+    add_024
+    add_510
+    marc
+  end
+
+  private
+
+    def add_856es
+      return unless resource.try(:identifier)&.present?
+      ark = Ark.new(resource.identifier.first).uri
+      manifest = Rails.application.routes.url_helpers.polymorphic_url([:manifest, resource])
+      marc.append(MARC::DataField.new("856", "4", "1", MARC::Subfield.new("u", manifest))) unless url_strings.include? manifest
+      marc.append(MARC::DataField.new("856", "4", "1", MARC::Subfield.new("u", ark))) unless url_strings.include? ark
+    end
+
+    def add_024
+      return unless resource.try(:local_identifier)&.present?
+      dcl = resource.local_identifier.first
+      return if standard_identifiers.include? dcl
+      marc.append(
+        MARC::DataField.new(
+          "024", "7", " ",
+          MARC::Subfield.new("a", dcl),
+          MARC::Subfield.new("2", "dclib")
+        )
+      )
+    end
+
+    def add_510
+      return unless resource.try(:imported_metadata)&.first&.references&.present?
+      cico_reference = resource.imported_metadata.first.references.first
+      return unless cico_reference =~ /Cicognara/
+      cico_number = cico_reference.match(/(\d+$)/)[1]
+      return if references.include? cico_number
+      marc.append(
+        MARC::DataField.new(
+          "510", "4", " ",
+          MARC::Subfield.new("a", "Cicognara,"),
+          MARC::Subfield.new("c", cico_number)
+        )
+      )
+    end
+
+    def url_strings
+      @url_strings ||= begin
+        url_fields = marc.fields("856").select do |field|
+          field.indicator1.eql? "4"
+          field.indicator2.eql? "1"
+        end
+        url_fields.flat_map(&:subfields).select { |s| s.code == "u" }.map(&:value)
+      end
+    end
+
+    def standard_identifiers
+      marc.fields("024").map do |field|
+        field.subfields.select { |s| s.code == "a" }.map(&:value).first
+      end
+    end
+
+    def references
+      marc.fields("510")
+          .select { |field| field.subfields.select { |subfield| subfield.code == "a" }.first.value =~ /Cicognara/ }
+          .flat_map { |field| field.subfields.select { |subfield| subfield.code == "c" } }
+          .map(&:value)
+    end
+end
---
+++ spec/services/marc_record_enhancer_spec.rb
@@ -0,0 +1,280 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe MarcRecordEnhancer do
+  describe ".for" do
+    context "when the resource has a bibid" do
+      let(:resource) do
+        FactoryBot.create_for_repository(
+          :scanned_resource,
+          source_metadata_identifier: "8543429",
+          rights_statement: RDF::URI("http://cicognara.org/microfiche_copyright")
+        )
+      end
+      before do
+        stub_bibdata(bib_id: "8543429")
+        stub_bibdata(bib_id: "8543429", content_type: BibdataStubbing::CONTENT_TYPE_MARC_XML)
+      end
+      it "returns a MarcRecordEnhancer with a marc record" do
+        service = described_class.for(resource)
+        expect(service).to be_a described_class
+        expect(service.marc).to be_a MARC::Record
+        expect(service.resource.id.to_s).to eq resource.id.to_s
+      end
+    end
+
+    context "when the resource has no source_metadata_identifier field" do
+      it "returns nil" do
+        resource = FactoryBot.create_for_repository(:ephemera_folder)
+        expect(described_class.for(resource)).to be_nil
+      end
+    end
+
+    context "when the resource has nil source_metadata_identifier" do
+      it "returns nil" do
+        resource = FactoryBot.create_for_repository(:scanned_resource)
+        expect(described_class.for(resource)).to be_nil
+      end
+    end
+
+    context "when the resource has a pulfa id" do
+      it "returns nil" do
+        stub_pulfa(pulfa_id: "RBD1_c13076")
+        resource = FactoryBot.create_for_repository(:scanned_resource, source_metadata_identifier: "RBD1_c13076")
+        expect(described_class.for(resource)).to be_nil
+      end
+    end
+  end
+
+  describe "#enhance_cicognara" do
+    let(:marc_record) do
+      MARC::Record.new.tap do |r|
+        r.append(ark_856)
+        r.append(manifest_856)
+      end
+    end
+    let(:enhancer) { described_class.new(marc: marc_record, resource: resource) }
+    let(:ark_856) do
+      MARC::DataField.new(
+        "856", "4", "1",
+        MARC::Subfield.new("u", "http://arks.princeton.edu/ark:/88435/jm214s442")
+      )
+    end
+    let(:manifest_856) do
+      MARC::DataField.new(
+        "856", "4", "1",
+        MARC::Subfield.new("u", manifest_url)
+      )
+    end
+    let(:resource) do
+      FactoryBot.create_for_repository(
+        :scanned_resource,
+        source_metadata_identifier: "8543429",
+        identifier: "ark:/88435/jm214s442",
+        local_identifier: "dcl:xjt"
+      )
+    end
+    let(:manifest_url) { Rails.application.routes.url_helpers.polymorphic_url([:manifest, resource]) }
+    let(:metadata_mock) { double }
+    before do
+      stub_bibdata(bib_id: "8543429")
+      stub_bibdata(bib_id: "8543429", content_type: BibdataStubbing::CONTENT_TYPE_MARC_XML)
+      allow(resource).to receive(:imported_metadata).and_return([metadata_mock])
+      allow(metadata_mock).to receive(:references).and_return(["Cicognara, 3723"])
+    end
+
+    context "when the record has 856es for manifest and ark" do
+      it "leaves them in the record" do
+        urls = enhancer.enhance_cicognara.fields("856").select do |field|
+          field.indicator1 == "4" &&
+            field.indicator2 == "1" &&
+            field.subfields.select { |subfield| subfield.code == "u" }.count == 1
+        end
+        expect(urls.count).to eq 2
+        subfields_u = urls.flat_map(&:subfields).select { |s| s.code == "u" }
+        expect(subfields_u.map(&:value)).to include(
+          "http://arks.princeton.edu/ark:/88435/jm214s442",
+          manifest_url
+        )
+      end
+    end
+
+    context "when the record has some other 856" do
+      let(:marc_record) do
+        MARC::Record.new.tap do |r|
+          r.append(other_856)
+        end
+      end
+      let(:other_856) do
+        MARC::DataField.new(
+          "856", "4", "1",
+          MARC::Subfield.new("u", "http://example.com/some_other_thing")
+        )
+      end
+      it "replaces it with the manifest and ark links" do
+        urls = enhancer.enhance_cicognara.fields("856").select do |field|
+          field.indicator1 == "4" &&
+            field.indicator2 == "1" &&
+            field.subfields.select { |subfield| subfield.code == "u" }.count == 1
+        end
+        expect(urls.count).to eq 3
+        subfields_u = urls.flat_map(&:subfields).select { |s| s.code == "u" }
+        expect(subfields_u.map(&:value)).to include(
+          "http://example.com/some_other_thing",
+          "http://arks.princeton.edu/ark:/88435/jm214s442",
+          manifest_url
+        )
+      end
+    end
+
+    context "when the record didn't have any 856es" do
+      let(:marc_record) { MARC::Record.new }
+      it "adds the manifest and ark links" do
+        urls = enhancer.enhance_cicognara.fields("856").select do |field|
+          field.indicator1 == "4" &&
+            field.indicator2 == "1" &&
+            field.subfields.select { |subfield| subfield.code == "u" }.count == 1
+        end
+        expect(urls.count).to eq 2
+        subfields_u = urls.flat_map(&:subfields).select { |s| s.code == "u" }
+        expect(subfields_u.map(&:value)).to include(
+          "http://arks.princeton.edu/ark:/88435/jm214s442",
+          manifest_url
+        )
+      end
+    end
+
+    context "when the record had the dcl in an 024" do
+      let(:marc_record) do
+        MARC::Record.new.tap do |r|
+          r.append(dcl_024)
+        end
+      end
+      let(:dcl_024) do
+        MARC::DataField.new(
+          "024", "7", " ",
+          MARC::Subfield.new("a", "dcl:xjt"),
+          MARC::Subfield.new("2", "dclib")
+        )
+      end
+      it "leaves that 024" do
+        std_ids = enhancer.enhance_cicognara.fields("024").select do |field|
+          field.indicator1 == "7" &&
+            field.indicator2 == " " &&
+            field.subfields.select { |subfield| subfield.code == "a" }.count == 1 &&
+            field.subfields.select { |subfield| subfield.code == "2" }.first.value == "dclib"
+        end
+        expect(std_ids.count).to eq 1
+        subfield_a = std_ids.first.subfields.select { |s| s.code == "a" }.first
+        expect(subfield_a.value).to eq "dcl:xjt"
+      end
+    end
+
+    context "when the record had something other than dcl in 024 8_ $a" do
+      let(:marc_record) do
+        MARC::Record.new.tap do |r|
+          r.append(other_024)
+        end
+      end
+      let(:other_024) do
+        MARC::DataField.new(
+          "024", "8", " ",
+          MARC::Subfield.new("a", "rando id")
+        )
+      end
+      it "adds another 024 with our dcl #" do
+        expect(enhancer.enhance_cicognara.fields("024").count).to eq 2
+        std_ids = enhancer.marc.fields("024").select do |field|
+          field.indicator1 == "7" &&
+            field.indicator2 == " " &&
+            field.subfields.select { |subfield| subfield.code == "a" }.count == 1 &&
+            field.subfields.select { |subfield| subfield.code == "2" }.first&.value == "dclib"
+        end
+        expect(std_ids.count).to eq 1
+        subfield_a = std_ids.first.subfields.select { |s| s.code == "a" }.first
+        expect(subfield_a.value).to eq "dcl:xjt"
+      end
+    end
+
+    context "when the record had no 024" do
+      let(:marc_record) { MARC::Record.new }
+      it "adds an 024 with our dcl #" do
+        std_ids = enhancer.enhance_cicognara.fields("024").select do |field|
+          field.indicator1 == "7" &&
+            field.indicator2 == " " &&
+            field.subfields.select { |subfield| subfield.code == "a" }.count == 1 &&
+            field.subfields.select { |subfield| subfield.code == "2" }.first.value == "dclib"
+        end
+        expect(std_ids.count).to eq 1
+        subfield_a = std_ids.first.subfields.select { |s| s.code == "a" }.first
+        expect(subfield_a.value).to eq "dcl:xjt"
+      end
+    end
+
+    context "when the record had the cico number in a 510" do
+      let(:marc_record) do
+        MARC::Record.new.tap do |r|
+          r.append(cico_510)
+        end
+      end
+      let(:cico_510) do
+        MARC::DataField.new(
+          "510", "4", " ",
+          MARC::Subfield.new("a", "Cicognara,"),
+          MARC::Subfield.new("c", "3723")
+        )
+      end
+      it "leaves that 510" do
+        references = enhancer.enhance_cicognara.fields("510").select do |field|
+          field.indicator1 == "4" &&
+            field.indicator2 == " " &&
+            field.subfields.select { |subfield| subfield.code == "a" }.first.value.starts_with?("Cicognara") &&
+            field.subfields.select { |subfield| subfield.code == "c" }.count == 1
+        end
+        expect(references.count).to eq 1
+        subfield_c = references.first.subfields.select { |s| s.code == "c" }.first
+        expect(subfield_c.value).to eq "3723"
+      end
+    end
+
+    context "when the record had some other 510" do
+      let(:marc_record) do
+        MARC::Record.new.tap do |r|
+          r.append(other_510)
+        end
+      end
+      let(:other_510) do
+        MARC::DataField.new(
+          "510", "4", " ",
+          MARC::Subfield.new("a", "Another identifier"),
+          MARC::Subfield.new("c", "1")
+        )
+      end
+      it "adds a 510 with the cicognara number" do
+        expect(enhancer.enhance_cicognara.fields("510").count).to eq 2
+        references = enhancer.marc.fields("510").select do |field|
+          field.indicator1 == "4" &&
+            field.indicator2 == " " &&
+            field.subfields.select { |subfield| subfield.code == "a" }.first.value.starts_with?("Cicognara")
+        end
+        expect(references.count).to eq 1
+        subfield_c = references.first.subfields.select { |s| s.code == "c" }.first
+        expect(subfield_c.value).to eq "3723"
+      end
+    end
+
+    context "when the record had no 510" do
+      let(:marc_record) { MARC::Record.new }
+      it "adds a 510 with the cicognara number" do
+        expect(enhancer.enhance_cicognara.fields("510").count).to eq 1
+        references = enhancer.marc.fields("510").select do |field|
+          field.indicator1 == "4" &&
+            field.indicator2 == " " &&
+            field.subfields.select { |subfield| subfield.code == "a" }.first.value.starts_with?("Cicognara")
+        end
+        subfield_c = references.first.subfields.select { |s| s.code == "c" }.first
+        expect(subfield_c.value).to eq "3723"
+      end
+    end
+  end
+end
