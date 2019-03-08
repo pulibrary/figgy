e8b135dabba1da5679d239a296062999bc978dd3
Anna Headley <anna3LC@gmail.com>
2019-03-08 11:06:28 -0500

    Refactor to a more ruby-ish style

--- app/services/cicognara_marc.rb
+++ app/services/cicognara_marc.rb
@@ -10,37 +10,42 @@ class CicognaraMarc
   end
 
   def run
-    collection = query_service.find_by(id: collection_id)
-    wayfinder = Wayfinder.for(collection)
-    wayfinder
-      .members
-      .select { |member| visible?(member) && readable_state?(member) }
-      .group_by(&:source_metadata_identifier)
-      .values
-      .map { |group| retrieve_ehanced_marc(group) }
-      .compact
-      .each { |record| write_marc_record(record) }
+    marc_records.each { |record| write_marc_record(record) }
   end
 
   def retrieve_ehanced_marc(arr)
     arr
       .map { |r| MarcRecordEnhancer.for(r)&.enhance_cicognara }
-      .reduce do |init, record|
-        ["856", "024", "510"].each do |field|
-          # can't use uniq because that compares object ID; include? uses `==`
-          all_vals = init.fields(field).tap do |vals|
-            record.fields(field).each do |potential_val|
-              vals << potential_val unless init.fields(field).include? potential_val
-            end
-          end
-          init.fields(field).to_a.each { |f| init.fields.delete(f) }
-          all_vals.each { |val| init.append val }
-        end
-        init
-      end
+      .reduce { |init, record| combine_records(init, record) }
   end
 
   private
+
+    def marc_records
+      publishable_resource_groups.map { |group| retrieve_ehanced_marc(group) }.compact
+    end
+
+    def publishable_resource_groups
+      publishable_resources.group_by(&:source_metadata_identifier).values
+    end
+
+    def publishable_resources
+      wayfinder.members.select { |member| visible?(member) && readable_state?(member) }
+    end
+
+    def combine_records(init, record)
+      ["856", "024", "510"].each do |field|
+        # can't use uniq because that compares object ID; include? uses `==`
+        all_vals = init.fields(field).tap do |vals|
+          record.fields(field).each do |potential_val|
+            vals << potential_val unless init.fields(field).include? potential_val
+          end
+        end
+        init.fields(field).to_a.each { |f| init.fields.delete(f) }
+        all_vals.each { |val| init.append val }
+      end
+      init
+    end
 
     def visible?(member)
       member.visibility.first == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
@@ -54,6 +59,14 @@ class CicognaraMarc
       bib_number = record["001"].value
       writer = MARC::XMLWriter.new(File.join(out_dir, "#{bib_number}_marc.xml"))
       writer.write(record)
+    end
+
+    def wayfinder
+      Wayfinder.for(collection)
+    end
+
+    def collection
+      query_service.find_by(id: collection_id)
     end
 
     def query_service
