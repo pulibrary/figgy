fce3b09836b40f2e669d70777a85443431c06569
christinach <actspatial@gmail.com>
2019-03-12 08:39:34 -0400

    updates form title if change set value is recording

--- app/helpers/metadata_form_helper.rb
+++ app/helpers/metadata_form_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: false
 module MetadataFormHelper
   def form_title(params)
-    "#{params['action'].capitalize} #{params['controller'].humanize.singularize.downcase}"
+    if form_custom_title(params)
+      "#{params['action'].capitalize} #{params['change_set'].humanize.singularize.downcase}"
+    else
+      "#{params['action'].capitalize} #{params['controller'].humanize.singularize.downcase}"
+    end
+  end
+
+  def form_custom_title(params)
+    params["change_set"].present? && params["change_set"] == "recording"
   end
 end
--- spec/helpers/metadata_form_helper_spec.rb
+++ spec/helpers/metadata_form_helper_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe MetadataFormHelper, type: :helper do
       params = { "action" => "edit", "controller" => "ephemera_folders" }
       expect(helper.form_title(params)).to eq "Edit ephemera folder"
     end
+    it "constucts a title for a new recording" do
+      params = { "change_set" => "recording", "action" => "new", "controller" => "scanned_resources" }
+      expect(helper.form_title(params)).to eq "New recording"
+    end
+    it "constucts a title for editing a recording" do
+      params = { "change_set" => "recording", "action" => "edit", "controller" => "scanned_resources" }
+      expect(helper.form_title(params)).to eq "Edit recording"
+    end
   end
 end
