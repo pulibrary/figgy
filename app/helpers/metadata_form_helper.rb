93fe9239d03b5f03980916ccca4d9d4201235c45
christinach <actspatial@gmail.com>
2019-03-12 10:25:44 -0400

    add word resource in the form title name when the change_set value is present

--- app/helpers/metadata_form_helper.rb
+++ app/helpers/metadata_form_helper.rb
@@ -2,13 +2,13 @@
 module MetadataFormHelper
   def form_title(params)
     if form_custom_title(params)
-      "#{params['action'].capitalize} #{params['change_set'].humanize.singularize.downcase}"
+      "#{params['action'].capitalize} #{params['change_set'].humanize.singularize.downcase} resource"
     else
       "#{params['action'].capitalize} #{params['controller'].humanize.singularize.downcase}"
     end
   end
 
   def form_custom_title(params)
-    params["change_set"].present? && params["change_set"] == "recording"
+    params["change_set"].present?
   end
 end
