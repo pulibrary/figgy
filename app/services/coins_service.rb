f35759dccf7fb5efbf65e1fe9313c33e8061b16f
Eliot Jordan <eliotj@princeton.edu>
2019-03-28 11:53:37 -0500

    Add service to clean issues and coins

---
+++ app/services/coins_service.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class CoinsService
+  def self.clean
+    clean_issues
+    clean_coins
+  end
+
+  def self.clean_issues
+    query_service.find_all_of_model(model: NumismaticIssue).each do |issue|
+      logger.info "Deleting NumismaticIssue: #{issue.id}"
+      delete(issue)
+    end
+  end
+
+  def self.clean_coins
+    query_service.find_all_of_model(model: Coin).each do |coin|
+      logger.info "Deleting Coin: #{coin.id}"
+      delete(coin)
+    end
+  end
+
+  def self.delete(resource)
+    change_set_class = DynamicChangeSet
+    change_set_persister = ::ChangeSetPersister.new(
+      metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
+      storage_adapter: Valkyrie.config.storage_adapter
+    )
+    change_set = change_set_class.new(resource)
+    change_set_persister.buffer_into_index do |persist|
+      persist.delete(change_set: change_set)
+    end
+  end
+
+  def self.logger
+    Logger.new(STDOUT)
+  end
+
+  def self.query_service
+    Valkyrie.config.metadata_adapter.query_service
+  end
+end
