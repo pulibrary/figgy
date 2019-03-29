663433e56a2ac0bfe329f3aa7d60353ad657edc9
Eliot Jordan <eliotj@princeton.edu>
2019-03-29 17:58:39 -0500

    Attach coin images

--- app/services/numismatics_import_service.rb
+++ app/services/numismatics_import_service.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
 class NumismaticsImportService
-  attr_reader :logger, :db_adapter
-  def initialize(db_adapter:, logger: nil)
-    @logger = logger || Logger.new(STDOUT)
+  attr_reader :db_adapter, :file_root, :logger
+  def initialize(db_adapter:, file_root:, logger: nil)
     @db_adapter = db_adapter
+    @file_root = file_root
+    @logger = logger || Logger.new(STDOUT)
   end
 
   def ingest_issue(issue_number:)
-    Importer.new(issue_number: issue_number, logger: logger, db_adapter: db_adapter).import!
+    Importer.new(issue_number: issue_number, db_adapter: db_adapter, file_root: file_root, logger: logger).import!
   end
 
   class Importer
-    attr_reader :issue_number, :logger, :change_set_persister, :db_adapter
-    def initialize(issue_number:, logger:, change_set_persister: nil, db_adapter:)
+    attr_reader :issue_number, :db_adapter, :file_root, :logger, :change_set_persister
+    def initialize(issue_number:, db_adapter:, file_root:, logger:, change_set_persister: nil)
       @issue_number = issue_number
+      @db_adapter = db_adapter
+      @file_root = Pathname.new(file_root.to_s)
       @logger = logger
       @change_set_persister = change_set_persister
-      @db_adapter = db_adapter
     end
 
     def import!
@@ -25,11 +27,37 @@ class NumismaticsImportService
       create_issue(coin_ids: coin_ids)
     end
 
+    def coin_file_title(file_path:)
+      basename = File.basename(file_path)
+      if basename =~ /O/
+        "Obverse"
+      elsif basename =~ /R/
+        "Reverse"
+      end
+    end
+
+    def coin_files(coin_number:)
+      files = Dir.glob(file_root.join("**/#{coin_number}{O,R}.*"))
+      files.map do |file_path|
+        IngestableFile.new(
+          file_path: file_path,
+          mime_type: "image/tiff",
+          original_filename: File.basename(file_path),
+          copyable: false,
+          container_attributes: {
+            title: coin_file_title(file_path: file_path)
+          }
+        )
+      end
+    end
+
     def create_coins
       resources = []
       coin_numbers = coins.ids(column: "IssueID", value: issue_number)
       coin_numbers.each do |number|
         attributes = coins.base_attributes(id: number).to_h
+        files = coin_files(coin_number: number)
+        attributes[:files] = files
         resources << new_resource(klass: Coin, **attributes)
       end
 
