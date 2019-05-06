63f345c41f771384f727042b3866f01b7f4116fa
Eliot Jordan <eliotj@princeton.edu>
2019-05-06 18:33:54 -0500

    Add all specs

---
+++ app/jobs/numismatic_issue_import_job.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+class NumismaticIssueImportJob < ApplicationJob
+  def perform(issue_number:, file_root:, db_adapter_name: "NumismaticsImportService::SqliteAdapter", **db_options)
+    db_adapter = db_adapter_name.constantize.new(db_options)
+    logger.info "Importing numismatic issue: #{issue_number}"
+    importer = NumismaticsImportService.new(db_adapter: db_adapter, logger: logger, file_root: file_root)
+    importer.ingest_issue(issue_number: issue_number)
+  end
+end
