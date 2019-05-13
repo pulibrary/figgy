2ce3523df4642a019cf6f1847e2265636fe0fe63
Trey Pendragon <tpendragon@princeton.edu>
2019-05-13 10:29:31 -0700

    Include local HLS.js
    Closes #3005

--- public/uv/lib/uv-av-extension-dependencies.js
+++ public/uv/lib/uv-av-extension-dependencies.js
@@ -10,18 +10,7 @@ define(function () {
         return isFormatAvailable(formats, 'application/dash+xml');
     }
     function canPlayHls() {
-        var doc = typeof document === 'object' && document, videoelem = doc && doc.createElement('video'), isvideosupport = Boolean(videoelem && videoelem.canPlayType), canPlay = [
-            'application/vnd.apple.mpegurl',
-            'audio/mpegurl',
-            'audio/x-mpegurl',
-            'application/x-mpegurl',
-            'video/x-mpegurl',
-            'video/mpegurl',
-            'application/mpegurl'
-        ];
-        return isvideosupport && canPlay.some(function (canItPlay) {
-            return /maybe|probably/i.test(videoelem.canPlayType(canItPlay));
-        });
+      return true;
     }
     return function (formats) {
         var alwaysRequired = ['TreeComponent', 'AVComponent', 'MetadataComponent', 'jquery-ui.min', 'jquery.ui.touch-punch.min', 'jquery.binarytransport', 'waveform-data'];
