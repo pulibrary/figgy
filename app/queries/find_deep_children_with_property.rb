75e675f0cfabcc0ce56304d98ea92b68cb83f770
Trey Pendragon <tpendragon@princeton.edu>
2019-05-16 14:03:29 -0700

    Find deep children with a given property.

---
+++ app/queries/find_deep_children_with_property.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+class FindDeepChildrenWithProperty
+  def self.queries
+    [:find_deep_children_with_property]
+  end
+
+  attr_reader :query_service
+  delegate :resource_factory, to: :query_service
+  delegate :run_query, to: :query_service
+  def initialize(query_service:)
+    @query_service = query_service
+  end
+
+  def find_deep_children_with_property(resource:, model:, property:, value:)
+    run_query(relationship_query, id: resource.id.to_s, model: model.to_s, property_query: { property => Array.wrap(value) }.to_json)
+  end
+
+  def relationship_query
+    <<-SQL
+        WITH RECURSIVE deep_members AS (
+          select member.*
+          FROM orm_resources a,
+          jsonb_array_elements(a.metadata->'member_ids') AS b(member)
+          JOIN orm_resources member ON (b.member->>'id')::UUID = member.id
+          WHERE a.id = :id
+          UNION
+          SELECT mem.*
+          FROM deep_members f,
+          jsonb_array_elements(f.metadata->'member_ids') AS g(member)
+          JOIN orm_resources mem ON (g.member->>'id')::UUID = mem.id
+        )
+        select * from deep_members
+        WHERE internal_resource = :model
+        AND metadata @> :property_query
+    SQL
+  end
+
+  def id_type
+    "UUID"
+  end
+end
