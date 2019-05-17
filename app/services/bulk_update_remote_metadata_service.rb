20ecacdcc0f7d130a6084bf2d50424f9ed7e8601
Trey Pendragon <tpendragon@princeton.edu>
2019-05-17 11:31:26 -0700

    Add bulk update remote metadata service.

---
+++ app/services/bulk_update_remote_metadata_service.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class BulkUpdateRemoteMetadataService
+  def self.call(batch_size: 50)
+    metadata_adapter.query_service.custom_queries.find_ids_with_property_not_empty(property: :source_metadata_identifier).each_slice(batch_size) do |slice|
+      VoyagerUpdateJob.perform_later(slice.map(&:to_s))
+    end
+  end
+
+  def self.metadata_adapter
+    Valkyrie::MetadataAdapter.find(:postgres)
+  end
+end
