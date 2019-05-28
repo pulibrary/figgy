1cb81b65e49c0bc135bc3dce87026dc22fe3f347
Anna Headley <anna3LC@gmail.com>
2019-05-28 14:32:47 -0400

    Add playlists to recordings_undownloadable task

--- spec/services/recording_downloadable_migrator_spec.rb
+++ spec/services/recording_downloadable_migrator_spec.rb
@@ -10,10 +10,11 @@ RSpec.describe RecordingDownloadableMigrator do
   end
 
   describe ".call" do
-    it "changes downloadable to 'none' in all Recordings" do
+    it "changes downloadable to 'none' in all Recordings and Playlists" do
       recordings = Array.new(2) do
         FactoryBot.create_for_repository(:recording, downloadable: "public")
       end
+      playlist = FactoryBot.create_for_repository(:playlist, downloadable: "public")
       sr = FactoryBot.create_for_repository(:scanned_resource, downloadable: "public")
 
       described_class.call
@@ -22,6 +23,7 @@ RSpec.describe RecordingDownloadableMigrator do
       recordings = query_service.find_many_by_ids(ids: recordings.map(&:id))
       expect(recordings.flat_map(&:downloadable)).to eq ["none", "none"]
       expect(query_service.find_by(id: sr.id).downloadable).to eq ["public"]
+      expect(query_service.find_by(id: playlist.id).downloadable).to eq ["none"]
     end
   end
 end
