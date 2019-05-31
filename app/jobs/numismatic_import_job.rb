219a4534f2768d55f56e46f0409e0a0f4922ac98
christinach <actspatial@gmail.com>
2019-05-31 09:28:29 -0400

    refactored depositor query

--- app/jobs/numismatic_import_job.rb
+++ app/jobs/numismatic_import_job.rb
@@ -3,7 +3,7 @@ class NumismaticImportJob < ApplicationJob
   def perform(file_root:, collection_id:, depositor:, db_adapter_name: "NumismaticsImportService::SqliteAdapter", **db_options)
     db_adapter = db_adapter_name.constantize.new(db_options)
     # Use the first admin user as a depositor in the numismatic import
-    depositor = User.joins(:roles).where("roles.name LIKE ?", "admin").first.uid
+    depositor = Role.where(name: "admin").first.users.first.uid
 
     importer = NumismaticsImportService.new(db_adapter: db_adapter, collection_id: collection_id, depositor: depositor, logger: logger, file_root: file_root)
 
