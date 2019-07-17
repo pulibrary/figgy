9614442d045039363a52475b1e454b5c9a220843
Trey Pendragon <tpendragon@princeton.edu>
2019-07-17 09:37:28 -0700

    Add an auto-closing login.

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "Logging in" do
+  context "when given a login_popup parameter" do
+    it "closes the window" do
+      user = FactoryBot.create(:admin)
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.add_mock(:cas, uid: user)
+
+      visit user_cas_omniauth_authorize_path(login_popup: "true")
+
+      expect(page.html).to eq "<html><head><script>window.close();</script></head><body></body></html>"
+    end
+  end
+end
