621687b9f12a73ad3b16c8568f55f5fa652523fa
Anna Headley <anna3LC@gmail.com>
2019-08-19 16:25:10 -0400

    Check that change in release notes adds a new message
    to prevent false positives on this check
    fixes #3267

---
+++ bin/ci_check_release_notes.sh
@@ -0,0 +1,8 @@
+output=`git diff --numstat --exit-code origin/master RELEASE_NOTES.md`
+# there was no difference in the file
+if [ $? -eq 0 ]
+  then
+    exit 1
+fi
+# there was a difference; make sure it was an addition
+! echo $output | cut -c1 | grep 0
