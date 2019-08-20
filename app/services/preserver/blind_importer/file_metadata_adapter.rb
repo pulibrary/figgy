a3f63531504342fd41d89e527d15b6870ef33fe3
Trey Pendragon <tpendragon@princeton.edu>
2019-08-20 12:54:08 -0700

    Extract more generic resource processor.

--- app/services/preserver/blind_importer/file_metadata_adapter.rb
+++ app/services/preserver/blind_importer/file_metadata_adapter.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 class Preserver::BlindImporter
   class FileMetadataAdapter
-    attr_reader :storage_adapter, :parents
-    def initialize(storage_adapter:, parents: [])
+    attr_reader :storage_adapter, :parents, :resource_processor
+    def initialize(storage_adapter:, parents: [], resource_processor: QueryService::ConvertLocalStorageIDs)
       @storage_adapter = storage_adapter
       @parents = parents
+      @resource_processor = resource_processor
     end
 
     def query_service
--- app/services/preserver/blind_importer/file_metadata_adapter/query_service.rb
+++ app/services/preserver/blind_importer/file_metadata_adapter/query_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Preserver::BlindImporter::FileMetadataAdapter
   class QueryService
-    delegate :storage_adapter, to: :adapter
+    delegate :storage_adapter, :resource_processor, to: :adapter
     attr_reader :adapter
     def initialize(adapter:)
       @adapter = adapter
@@ -11,35 +11,11 @@ class Preserver::BlindImporter::FileMetadataAdapter
       file = storage_adapter.find_by(id: storage_id_from_resource_id(id))
       json = JSON.parse(file.read)
       attributes = Valkyrie::Persistence::Shared::JSONValueMapper.new(json).result.symbolize_keys
-      fix_file_metadata(Valkyrie::Types::Anything[attributes])
+      resource_processor.call(resource: Valkyrie::Types::Anything[attributes], adapter: adapter)
     # Rescue NoMethodError because it's what the GCS shrine adapter is throwing
     # right now when a file isn't found.
     rescue NoMethodError, Valkyrie::StorageAdapter::FileNotFound
       raise Valkyrie::Persistence::ObjectNotFoundError
-    end
-
-    def fix_file_metadata(resource)
-      return resource unless resource.try(:file_metadata).present?
-      resource.file_metadata.map! do |file_metadata|
-        file_metadata.file_identifiers.map! do |identifier|
-          preservation_location = storage_id_from_resource_id(resource.id, original_filename: original_filename(identifier, file_metadata))
-          begin
-            storage_adapter.find_by(id: preservation_location).id
-          # Rescue NoMethodError because the GCS Shrine Valkyrie adapter has a
-          # bug with NotFound not returning right.
-          rescue Valkyrie::StorageAdapter::FileNotFound, NoMethodError
-            nil
-          end
-        end.compact!
-        file_metadata
-      end
-      resource.file_metadata = resource.file_metadata.select { |x| x.file_identifiers.present? }
-      resource
-    end
-
-    def original_filename(identifier, file_metadata)
-      original_filename = Pathname.new(identifier.to_s).basename.to_s.split(".")
-      "#{original_filename[0]}-#{file_metadata.id}.#{original_filename[1]}"
     end
 
     def storage_id_from_resource_id(id, original_filename: nil)
@@ -54,6 +30,44 @@ class Preserver::BlindImporter::FileMetadataAdapter
     # TODO: I wonder if Valkyrie could provide some method of doing this?
     def path_prefix
       storage_adapter.is_a?(Valkyrie::Storage::Disk) ? "disk" : "shrine"
+    end
+
+    class ConvertLocalStorageIDs
+      def self.call(resource:, adapter:)
+        new(resource: resource, adapter: adapter).convert!
+      end
+
+      delegate :storage_adapter, :query_service, to: :adapter
+      delegate :storage_id_from_resource_id, to: :query_service
+      attr_reader :resource, :adapter
+      def initialize(resource:, adapter:)
+        @resource = resource
+        @adapter = adapter
+      end
+
+      def convert!
+        return resource unless resource.try(:file_metadata).present?
+        resource.file_metadata.map! do |file_metadata|
+          file_metadata.file_identifiers.map! do |identifier|
+            preservation_location = storage_id_from_resource_id(resource.id, original_filename: original_filename(identifier, file_metadata))
+            begin
+              storage_adapter.find_by(id: preservation_location).id
+              # Rescue NoMethodError because the GCS Shrine Valkyrie adapter has a
+              # bug with NotFound not returning right.
+            rescue Valkyrie::StorageAdapter::FileNotFound, NoMethodError
+              nil
+            end
+          end.compact!
+          file_metadata
+        end
+        resource.file_metadata = resource.file_metadata.select { |x| x.file_identifiers.present? }
+        resource
+      end
+
+      def original_filename(identifier, file_metadata)
+        original_filename = Pathname.new(identifier.to_s).basename.to_s.split(".")
+        "#{original_filename[0]}-#{file_metadata.id}.#{original_filename[1]}"
+      end
     end
   end
 end
