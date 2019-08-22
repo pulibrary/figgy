2a266d146a11644410757cdbf5824c5b768f50bb
Trey Pendragon <tpendragon@princeton.edu>
2019-08-22 12:37:14 -0700

    Remote parents argument.

--- app/services/preserver/blind_importer/file_metadata_adapter.rb
+++ app/services/preserver/blind_importer/file_metadata_adapter.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 class Preserver::BlindImporter
   class FileMetadataAdapter
-    attr_reader :storage_adapter, :parents, :resource_processor
-    def initialize(storage_adapter:, parents: [], resource_processor: QueryService::ConvertLocalStorageIDs)
+    attr_reader :storage_adapter, :resource_processor
+    def initialize(storage_adapter:, resource_processor: QueryService::ConvertLocalStorageIDs)
       @storage_adapter = storage_adapter
-      @parents = parents
       @resource_processor = resource_processor
     end
 
