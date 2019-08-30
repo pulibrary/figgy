30d011573d6a16ee39458d08e8b7ea85191028a4
Trey Pendragon <tpendragon@princeton.edu>
2019-08-30 10:23:35 -0700

    Move NestedStoragePath to preserver namespace.

--- app/services/preserver/blind_importer/file_metadata_adapter/query_service/blind_importer_metadata_wayfinder.rb
+++ app/services/preserver/blind_importer/file_metadata_adapter/query_service/blind_importer_metadata_wayfinder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Preserver::BlindImporter::FileMetadataAdapter::QueryService
   # Wayfinder so that FileMetadataResources can return a parent used by
-  # NestedStoragePath, as a way to figure out where a JSON or binary file would
+  # Preserver::NestedStoragePath, as a way to figure out where a JSON or binary file would
   # be stored in a nested hierarchy.
   class BlindImporterMetadataWayfinder < BaseWayfinder
     def parent
---
+++ app/services/preserver/nested_storage_path.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+module Preserver
+  class NestedStoragePath
+    attr_reader :base_path
+    def initialize(base_path:)
+      @base_path = base_path
+    end
+
+    def generate(resource:, file:, original_filename:)
+      raise ArgumentError, "original_filename must be provided" unless original_filename
+      Pathname.new(base_path).join(*nested_path(resource)).join(original_filename)
+    end
+
+    def nested_path(resource)
+      parent = Wayfinder.for(resource).try(:parent)
+      return(nested_path(parent) + ["data", resource.id.to_s]) if parent
+      [resource.id.to_s]
+    end
+  end
+end
