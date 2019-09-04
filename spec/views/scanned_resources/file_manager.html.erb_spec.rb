927ab61fc61420a9723d8839aee231160ec1ae86
Trey Pendragon <tpendragon@princeton.edu>
2019-09-03 17:07:30 -0700

    Add UI for reinstating a tombstone.
    Closes #3311
    Closes #3299

--- spec/views/scanned_resources/file_manager.html.erb_spec.rb
+++ spec/views/scanned_resources/file_manager.html.erb_spec.rb
@@ -24,6 +24,34 @@ RSpec.describe "base/file_manager.html.erb", type: :view do
     expect(rendered).to have_selector("img[src='#{ManifestBuilder::ManifestHelper.new.manifest_image_path(member.thumbnail_id)}/full/!200,150/0/default.jpg']")
   end
 
+  context "when the resource has a tombstone for a deleted FileSet" do
+    let(:parent) do
+      tombstone
+      ScannedResourceChangeSet.new(scanned_resource)
+    end
+    let(:tombstone) do
+      unpreserved_tombstone
+      FactoryBot.create_for_repository(
+        :tombstone,
+        parent_id: scanned_resource.id,
+        file_set_id: "1",
+        file_set_title: "Test Title",
+        file_set_original_filename: "02.tif",
+        preservation_object: PreservationObject.new
+      )
+    end
+    let(:unpreserved_tombstone) do
+      FactoryBot.create_for_repository(:tombstone, parent_id: scanned_resource.id, file_set_id: "1", file_set_title: "Test Title 2", file_set_original_filename: "01.tif")
+    end
+    it "renders a form to reinstate it" do
+      expect(rendered).to include "<h2>Deleted Files</h2>"
+      expect(rendered).to include "Test Title 2 (01.tif)"
+      expect(rendered).to have_selector "input[name='scanned_resource[tombstone_restore_ids][]'][value='#{tombstone.id}']", visible: false
+      expect(rendered).not_to have_selector "input[name='scanned_resource[tombstone_restore_ids][]'][value='#{unpreserved_tombstone.id}']", visible: false
+      expect(rendered).to have_button "Reinstate"
+    end
+  end
+
   context "when a FileSet has errors" do
     let(:original_file) { FileMetadata.new(use: [Valkyrie::Vocab::PCDMUse.OriginalFile], error_message: ["errors"]) }
     let(:file_set) { FactoryBot.build(:file_set, file_metadata: [original_file]) }
