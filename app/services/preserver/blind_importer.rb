529ec8a2abae8f913c078c895ad0f36f5d49aff5
GriffinJ <jrgriffiniii@gmail.com>
2019-09-04 16:19:59 -0400

    Fixing the created_file_sets iteration in Preserver::BlindImporter#import!

--- app/services/preserver/blind_importer.rb
+++ app/services/preserver/blind_importer.rb
@@ -34,12 +34,10 @@ class Preserver::BlindImporter
     # Get rid of non-preserved members.
     source_change_set.try(:member_ids=, member_ids)
     # Delete any tombstones
-    source_change_set.created_file_sets.each do |_created_file_set|
-      persisted_tombstones = change_set_persister.query_service.custom_queries.find_by_property(property: :resource_id, value: source_resource.id)
-      persisted_tombstones.each do |tombstone|
-        tombstone_change_set = TombstoneChangeSet.new(tombstone)
-        change_set_persister.delete(change_set: tombstone_change_set)
-      end
+    persisted_tombstones = change_set_persister.query_service.custom_queries.find_by_property(property: :resource_id, value: source_resource.id)
+    persisted_tombstones.each do |tombstone|
+      tombstone_change_set = TombstoneChangeSet.new(tombstone)
+      change_set_persister.delete(change_set: tombstone_change_set)
     end
     output = change_set_persister.save(change_set: source_change_set)
     output
