67fc18009561875909c6b6c203fe845a54513fc7
Eliot Jordan <eliotj@princeton.edu>
2019-09-30 16:22:47 -0500

    Use global variable to store order manager fetching state

--- app/javascript/store/resource/index.js
+++ app/javascript/store/resource/index.js
@@ -33,7 +33,7 @@ export const resourceMutations = {
   },
   SAVED_STATE(state, saveStatus) {
     state.resource.saveState = saveStatus
-    state.fetching = false
+    window.fetching = false
   },
   SET_RESOURCE(state, resource) {
     state.resource.id = resource.id
