46d3ceb5493acb05bcbc9a7e5f56931c26e8ee73
Clifford Wulfman <cwulfman@princeton.edu>
2019-11-05 14:09:19 -0500

    addresses comments by Hathi about meta.yml

--- app/exporters/hathi/image_metadata.rb
+++ app/exporters/hathi/image_metadata.rb
@@ -2,47 +2,49 @@
 require "mini_magick"
 
 module Hathi
+  # Extrapolates image data for entire resource from the first image
   class ImageMetadata
-    attr_reader :resource, :repr_image
+    attr_reader :resource, :original_image
     def initialize(resource:)
       @resource = resource
       wayfinder = Wayfinder.for(resource)
-      page = wayfinder.members.first
-      metadata = page.original_file
-      tiff_file = Valkyrie::StorageAdapter.find_by(id: metadata.file_identifiers.first)
-      @repr_image = MiniMagick::Image.new(tiff_file.disk_path)
+      page = ContentPackage::Page.new(wayfinder.members.first, "representative page")
+      # page = wayfinder.members.first
+
+      @original_image = MiniMagick::Image.new(page.tiff_path)
+      @derivative_image = MiniMagick::Image.new(page.derivative_path)
     end
 
     def capture_date
-      repr_image.data["properties"]["tiff:timestamp"]
+      original_image.data["properties"]["xmp:CreateDate"]
     end
 
     def scanner_make
-      repr_image.data["properties"]["tiff:make"]
+      original_image.data["properties"]["tiff:make"]
     end
 
     def scanner_model
-      repr_image.data["properties"]["tiff:model"]
+      original_image.data["properties"]["tiff:model"]
     end
 
     def scanner_user
-      "Princeton University Library: Digital Photography Studio"
+      %{"Princeton University Library: Digital Photography Studio"}
     end
 
     def bitonal?
-      repr_image["%z"] == 1
+      original_image["%z"] == 1
     end
 
     def resolution
-      repr_image["resolution"].first
+      original_image["resolution"].first
     end
 
     def scanning_order
-      "left_to_right"
+      "left-to-right"
     end
 
     def reading_order
-      "left_to_right"
+      "left-to-right"
     end
 
     def pagedata; end
--- spec/exporters/hathi/image_metadata_spec.rb
+++ spec/exporters/hathi/image_metadata_spec.rb
@@ -4,19 +4,14 @@ include ActionDispatch::TestProcess
 
 RSpec.describe Hathi::ImageMetadata do
   subject(:adapter) { described_class.new(resource: dummy_resource) }
-
+  with_queue_adapter :inline
   let(:dummy_resource) do
     file1 = fixture_file_upload("files/example.tif", "image/tiff")
     file2 = fixture_file_upload("files/example.tif", "image/tiff")
     scanned_resource = FactoryBot.create_for_repository(:scanned_resource,
                                                         source_metadata_identifier: "123456",
+                                                        ocr_language: "eng",
                                                         files: [file1, file2])
-    Wayfinder.for(scanned_resource).members.each_with_index do |file_set, idx|
-      pagename = (idx + 1).to_s.rjust(8, "0")
-      file_set.ocr_content = "the OCR for page " + pagename
-      file_set.hocr_content = "the hOCR for page " + pagename
-      Valkyrie::MetadataAdapter.find(:indexing_persister).persister.save(resource: file_set)
-    end
     scanned_resource
   end
 
@@ -26,14 +21,19 @@ RSpec.describe Hathi::ImageMetadata do
 
   it "supplies a capture date" do
     value = adapter.capture_date
-    expect(value).to eq("2014:12:03 12:40:50")
+    expect(value).to eq("2014-07-01T05:31:54")
   end
 
   it "supplies a scanner make" do
     expect(adapter.scanner_make).to eq("Phase One")
   end
+
   it "supplies a scanner model" do
     expect(adapter.scanner_model).to eq("P65+")
+  end
+
+  it "supplies a scanner user" do
+    expect(adapter.scanner_user).to eq("\"Princeton University Library: Digital Photography Studio\"")
   end
 
   it "knows it isn't bitonal" do
