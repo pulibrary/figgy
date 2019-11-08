e10c852f8878054b4afdb8cb7b5055c97079f0de
Eliot Jordan <eliotj@princeton.edu>
2019-11-08 12:19:58 -0600

    Remove WFS endpoints from raster geoblacklight docs and add WCS endpoints (#3481)

--- app/services/geo_discovery/abstract_document.rb
+++ app/services/geo_discovery/abstract_document.rb
@@ -6,7 +6,7 @@ module GeoDiscovery
                   :fgdc, :format, :geom_type, :identifier, :iiif, :iiif_manifest, :iso19139,
                   :issued, :language, :layer_modified, :layer_year, :mods, :held_by, :publisher,
                   :resource_type, :slug, :solr_coverage, :source, :spatial, :subject, :suppressed,
-                  :temporal, :thumbnail, :title, :url, :wfs_path, :wms_path, :wxs_identifier
+                  :temporal, :thumbnail, :title, :url, :wcs_path, :wfs_path, :wms_path, :wxs_identifier
 
     # Cleans the document hash by removing unused fields.
     # @param [Hash] document hash
