919501b8e0b1eb860e21e4f74bcef17f63b86aca
Esmé Cowles <escowles@ticklefish.org>
2019-11-13 09:40:23 -0500

    Enable test coverage calculation by default.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
-if ENV.fetch("COVERAGE", false)
-  require "simplecov"
-
-  if ENV["CIRCLE_ARTIFACTS"]
-    dir = File.join(ENV["CIRCLE_ARTIFACTS"], "coverage")
-    SimpleCov.coverage_dir(dir)
-  end
-
-  SimpleCov.start "rails"
+require "simplecov"
+if ENV["CIRCLE_ARTIFACTS"]
+  dir = File.join(ENV["CIRCLE_ARTIFACTS"], "coverage")
+  SimpleCov.coverage_dir(dir)
 end
+SimpleCov.start "rails"
 
 require "webmock/rspec"
 
