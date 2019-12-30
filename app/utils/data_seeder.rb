273127d4d2a5a5175e7ea84d5a8db09929bfc72e
Anna Headley <anna.headley@gmail.com>
2019-12-30 12:36:34 -0500

    Fix data seeder for optimistic locking

--- app/utils/data_seeder.rb
+++ app/utils/data_seeder.rb
@@ -188,7 +188,7 @@ class DataSeeder
   private
 
     def add_child_resource(child:, parent_id:)
-      change_set = DynamicChangeSet.new(child)
+      change_set = DynamicChangeSet.new(query_service.find_by(id: child.id))
       change_set.append_id = parent_id
       change_set_persister.save(change_set: change_set)
     end
@@ -243,13 +243,13 @@ class DataSeeder
 
     def add_file(resource:, file: nil)
       ingestable_file = file || IngestableFile.new(file_path: Rails.root.join("spec", "fixtures", "files", "example.tif"), mime_type: "image/tiff", original_filename: "example.tif")
-      change_set = DynamicChangeSet.new(resource)
+      change_set = DynamicChangeSet.new(query_service.find_by(id: resource.id))
       change_set.files = [ingestable_file]
       change_set_persister.save(change_set: change_set)
     end
 
     def add_member(parent:, member:)
-      member_change_set = DynamicChangeSet.new(member)
+      member_change_set = DynamicChangeSet.new(query_service.find_by(id: member.id))
       member_change_set.append_id = parent.id
       change_set_persister.save(change_set: member_change_set)
       logger.info "Added #{member.class} #{member.id} to #{parent.class} #{parent.title || parent.box_number}"
