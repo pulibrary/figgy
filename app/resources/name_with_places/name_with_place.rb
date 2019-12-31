7ace574d0f606fb35b9a72b08ba587420b81a1a0
Anna Headley <anna.headley@gmail.com>
2019-12-31 12:28:56 -0500

    enable optimistic locking on resources subclassed from Valkyrie::Resource directly

--- app/resources/name_with_places/name_with_place.rb
+++ app/resources/name_with_places/name_with_place.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class NameWithPlace < Valkyrie::Resource
+  enable_optimistic_locking
+
   attribute :name, Valkyrie::Types::String
   attribute :place, Valkyrie::Types::String
 end
