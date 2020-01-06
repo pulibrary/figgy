4f15c362323090f54941a454c29bb63cacec1801
Trey Pendragon <tpendragon@princeton.edu>
2020-01-06 10:25:05 -0800

    Fix slow query on Catalog Show Page.
    This index speeds up the query from 11s to 20ms.

--- db/structure.sql
+++ db/structure.sql
@@ -5,8 +5,23 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
 
 --
 -- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
@@ -574,6 +589,20 @@ CREATE INDEX orm_resources_first_issue_number_idx ON public.orm_resources USING 
 
 
 --
+-- Name: preserved_object_id_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX preserved_object_id_idx ON public.orm_resources USING btree ((((((metadata -> 'preserved_object_id'::text) -> 0) ->> 'id'::text))::uuid));
+
+
+--
+-- Name: resource_id_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX resource_id_idx ON public.orm_resources USING btree ((((((metadata -> 'resource_id'::text) -> 0) ->> 'id'::text))::uuid));
+
+
+--
 -- PostgreSQL database dump complete
 --
 
@@ -602,6 +631,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20181030210350'),
 ('20181115195544'),
 ('20190102173711'),
-('20190521200107');
+('20200106182149');
 
 
