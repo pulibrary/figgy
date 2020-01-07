ef37c0a574dff42043577c417c81c94b38798745
GriffinJ <jrgriffiniii@gmail.com>
2020-01-07 17:55:29 -0500

    Fixing the FindResourcesWithoutMembers test suite

--- spec/queries/find_resources_without_members_spec.rb
+++ spec/queries/find_resources_without_members_spec.rb
@@ -4,8 +4,7 @@ require "rails_helper"
 RSpec.describe FindResourcesWithoutMembers do
   subject(:query) { described_class.new(query_service: query_service) }
   let(:coin1) { FactoryBot.create_for_repository(:coin) }
-  let(:file) { fixture_file_upload('numismatics/coin-images/1O.tif') }
-  let(:coin2) { FactoryBot.create_for_repository(:coin, member_ids: []) }
+  let(:coin2) { FactoryBot.create_for_repository(:coin, member_ids: [Valkyrie::ID.new("foo")]) }
   let(:query_service) { Valkyrie.config.metadata_adapter.query_service }
 
   describe "#find_by_property_and_model" do
@@ -14,8 +13,8 @@ RSpec.describe FindResourcesWithoutMembers do
         coin1
         coin2
         output = query.find_resources_without_members(model: Numismatics::Coin)
-        expect(output.to_a.map(&:id)).to include(coin2.id)
-        expect(output.to_a.map(&:id)).not_to include(coin1.id)
+        expect(output.to_a.map(&:id)).to include(coin1.id)
+        expect(output.to_a.map(&:id)).not_to include(coin2.id)
       end
     end
   end
