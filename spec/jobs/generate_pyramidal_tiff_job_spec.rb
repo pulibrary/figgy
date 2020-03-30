50c72cfb15ed70a25d988b22f887e3c0fb91eb45
Trey Pendragon <tpendragon@princeton.edu>
2020-03-30 15:31:27 -0700

    Finish up coverage.

---
+++ spec/jobs/generate_pyramidal_tiff_job_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe GeneratePyramidalTiffJob do
+  describe "#perform" do
+    context "with a valid file set id" do
+      with_queue_adapter :inline
+      let(:file) { fixture_file_upload("files/example.tif", "image/tiff") }
+      it "cleans up existing pyramidal derivatives and generates new ones", run_real_derivatives: true, run_real_characterization: true do
+        resource = FactoryBot.create_for_repository(:scanned_resource, files: [file])
+        file_set = Wayfinder.for(resource).file_sets[0]
+        pyramidal_derivative = file_set.pyramidal_derivative
+        jp2_derivative = file_set.jp2_derivative
+
+        described_class.perform_now(file_set.id.to_s)
+
+        reloaded = Valkyrie.config.metadata_adapter.query_service.find_by(id: file_set.id)
+        expect(reloaded.pyramidal_derivative.id).not_to eq pyramidal_derivative.id
+        # Ensure JP2 didn't get regenerated.
+        expect(reloaded.jp2_derivative.id).to eq jp2_derivative.id
+        expect(reloaded.file_metadata.length).to eq 3
+      end
+    end
+
+    context "with an invalid file set id" do
+      let(:logger) { instance_double(ActiveSupport::Logger) }
+
+      before do
+        allow(Rails).to receive(:logger).and_return(logger)
+        allow(logger).to receive(:error)
+      end
+
+      it "logs the exception" do
+        described_class.perform_now("bogus")
+        expect(logger).to have_received(:error)
+      end
+    end
+  end
+end
