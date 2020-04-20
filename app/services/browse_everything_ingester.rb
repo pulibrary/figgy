64e710ce120e66f325447f253750bacb7a8a8b3a
Anna Headley <anna.headley@gmail.com>
2020-04-20 10:21:59 -0400

    Extract method in browse everything ingester

--- app/services/browse_everything_ingester.rb
+++ app/services/browse_everything_ingester.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+# This class was extracted from BulkIngestController and is covered by its specs
 class BrowseEverythingIngester
-  attr_reader :change_set_persister, :multi_volume_work, :uploads, :resource_class
+  attr_reader :multi_volume_work, :uploads, :resource_class
   def initialize(change_set_persister:, multi_volume_work:, uploads:, resource_class:)
     @change_set_persister = change_set_persister
     @multi_volume_work = multi_volume_work
@@ -11,7 +12,7 @@ class BrowseEverythingIngester
 
   def ingest_from_cloud
     return false unless selected_cloud_files?
-    change_set_persister.buffer_into_index do |buffered_changeset_persister|
+    @change_set_persister.buffer_into_index do |buffered_changeset_persister|
       if multi_volume_work
         ingest_multi_volume_works(buffered_changeset_persister)
       else
@@ -27,28 +28,52 @@ class BrowseEverythingIngester
       !uploads.first.provider.is_a?(BrowseEverything::Provider::FileSystem)
     end
 
+    def pending_uploads(upload)
+      # Create a PendingUpload for each file to be uploaded,
+      #   add it to a lookup table with container id as its key
+      file_tree = Hash.new([])
+      upload.files.each do |upload_file|
+        new_pending_upload = PendingUpload.new(
+          id: SecureRandom.uuid,
+          upload_id: upload.id,
+          upload_file_id: upload_file.id
+        )
+
+        if new_pending_upload.in_container?
+          file_tree[upload_file.container_id] += [new_pending_upload]
+        end
+      end
+      file_tree
+    end
+
+    def ingest_works(change_set_persister)
+      uploads.each do |upload|
+        container_lookup = pending_uploads(upload)
+
+        upload.containers.each do |container|
+          # Are there files for this container?
+          next unless container_lookup.key?(container.id)
+          # Create the volume work
+          children = container_lookup[container.id]
+          volume_name = container.name
+          member_change_set = build_change_set(title: volume_name, pending_uploads: children, files: children)
+
+          change_set_persister.save(change_set: member_change_set)
+        end
+      end
+    end
+
     def ingest_multi_volume_works(change_set_persister)
       uploads.each do |upload|
-        file_tree = Hash.new([])
-        upload.files.each do |upload_file|
-          new_pending_upload = PendingUpload.new(
-            id: SecureRandom.uuid,
-            upload_id: upload.id,
-            upload_file_id: upload_file.id
-          )
+        container_lookup = pending_uploads(upload)
 
-          if new_pending_upload.in_container?
-            file_tree[upload_file.container_id] += [new_pending_upload]
-          end
-        end
-
-        directory_tree = {}
+        directory_tree = Hash.new([])
         parent_containers = []
         upload.containers.each do |container|
           # Are there files for this container?
-          if file_tree.key?(container.id)
+          if container_lookup.key?(container.id)
             # Create the volume work
-            children = file_tree[container.id]
+            children = container_lookup[container.id]
             volume_name = container.name
             member_change_set = build_change_set(title: volume_name, pending_uploads: children, files: children)
 
@@ -56,8 +81,7 @@ class BrowseEverythingIngester
 
             parent_id = container.parent_id
             if parent_id
-              members = directory_tree[parent_id] || []
-              directory_tree[parent_id] = members + [persisted]
+              directory_tree[parent_id] += [persisted]
             end
           else
             parent_containers << container
@@ -72,35 +96,6 @@ class BrowseEverythingIngester
 
           parent_change_set = build_change_set(title: parent_name, member_ids: member_ids)
           change_set_persister.save(change_set: parent_change_set)
-        end
-      end
-    end
-
-    def ingest_works(change_set_persister)
-      uploads.each do |upload|
-        file_tree = {}
-        upload.files.each do |upload_file|
-          new_pending_upload = PendingUpload.new(
-            id: SecureRandom.uuid,
-            upload_id: upload.id,
-            upload_file_id: upload_file.id
-          )
-
-          if new_pending_upload.in_container?
-            pending_uploads = file_tree[upload_file.container_id] || []
-            file_tree[upload_file.container_id] = pending_uploads + [new_pending_upload]
-          end
-        end
-
-        upload.containers.each do |container|
-          # Are there files for this container?
-          next unless file_tree.key?(container.id)
-          # Create the volume work
-          children = file_tree[container.id]
-          volume_name = container.name
-          member_change_set = build_change_set(title: volume_name, pending_uploads: children, files: children)
-
-          change_set_persister.save(change_set: member_change_set)
         end
       end
     end
