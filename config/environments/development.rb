4ac270d78acdb3ba4a6066a75e1b90392070fdb2
Anna Headley <anna.headley@gmail.com>
2020-04-23 11:06:41 -0400

    Use local storage service for active storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,5 +24,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_controller.action_on_unpermitted_parameters = false
-  config.active_storage.service = :test
+  config.active_storage.service = :local
 end
