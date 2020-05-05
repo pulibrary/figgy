ce7bb5b5fae7dbcfa0c48bcaa4a83f03846503f0
Anna Headley <anna.headley@gmail.com>
2020-05-05 16:02:21 -0400

    remove stubbing from some resource controller tests

--- spec/resources/scanned_resources/scanned_resources_controller_spec.rb
+++ spec/resources/scanned_resources/scanned_resources_controller_spec.rb
@@ -387,6 +387,60 @@ RSpec.describe ScannedResourcesController, type: :controller do
     # This block tests functionality defined in `app/controllers/concerns/browse_everythingable.rb`
     #   Acts as a 'master spec' in this regard
     describe "POST /concern/scanned_resources/:id/browse_everything_files" do
+      it "uploads files" do
+        resource = FactoryBot.create_for_repository(:scanned_resource)
+        file_path = Rails.root.join("spec", "fixtures", "files", "example.tif")
+        session = BrowseEverything::Session.build(
+          provider_id: "fast_file_system"
+        ).tap(&:save)
+        upload = BrowseEverything::Upload.build(
+          bytestream_ids: [file_path],
+          session_id: session.id
+        ).tap(&:save)
+        params = {
+          "id": resource.id,
+          "browse_everything" => { "uploads" => [upload.id] }
+        }
+        post :browse_everything_files, params: params
+
+        reloaded = adapter.query_service.find_by(id: resource.id)
+
+        expect(reloaded.member_ids.length).to eq 1
+        expect(reloaded.pending_uploads).to be_empty
+
+        file_sets = Valkyrie.config.metadata_adapter.query_service.find_members(resource: reloaded)
+        expect(file_sets.first.file_metadata.length).to eq 3
+      end
+
+      context "the user selects hidden files" do
+        it "filters the uploads" do
+          file_path = Rails.root.join("spec", "fixtures", "files", "example.tif")
+          hidden_file_path = Rails.root.join("spec", "fixtures", "hidden_files", "32101075851400", ".hidden_file.txt")
+          resource = FactoryBot.create_for_repository(:scanned_resource)
+          session = BrowseEverything::Session.build(
+            provider_id: "fast_file_system"
+          ).tap(&:save)
+          upload = BrowseEverything::Upload.build(
+            bytestream_ids: [file_path, hidden_file_path],
+            session_id: session.id
+          ).tap(&:save)
+          params = {
+            "id": resource.id,
+            "browse_everything" => { "uploads" => [upload.id] }
+          }
+          post :browse_everything_files, params: params
+
+          reloaded = adapter.query_service.find_by(id: resource.id)
+
+          expect(reloaded.member_ids.length).to eq 1
+          expect(reloaded.decorate.file_sets.first.title).to eq [File.basename(file_path)]
+        end
+      end
+    end
+
+    # This block tests functionality defined in `app/controllers/concerns/browse_everythingable.rb`
+    #   Acts as a 'master spec' in this regard
+    describe "POST /concern/scanned_resources/:id/browse_everything_files" do
       let(:file) { File.open(Rails.root.join("spec", "fixtures", "files", "example.tif")) }
       let(:bytestream) { instance_double(ActiveStorage::Blob) }
       let(:upload_file) { double }
@@ -440,44 +494,7 @@ RSpec.describe ScannedResourcesController, type: :controller do
           expect(reloaded.member_ids).to be_empty
         end
       end
-      it "uploads files" do
-        # Ensure that indexing is always safe and done at the end.
-        allow(Valkyrie::MetadataAdapter.find(:index_solr)).to receive(:persister).and_return(Valkyrie::MetadataAdapter.find(:index_solr).persister)
-        allow(Valkyrie::MetadataAdapter.find(:index_solr).persister).to receive(:save).and_call_original
-        allow(Valkyrie::MetadataAdapter.find(:index_solr).persister).to receive(:save_all).and_call_original
 
-        post :browse_everything_files, params: params
-        reloaded = adapter.query_service.find_by(id: resource.id)
-
-        expect(reloaded.member_ids.length).to eq 1
-        expect(reloaded.pending_uploads).to be_empty
-        expect(Valkyrie::MetadataAdapter.find(:index_solr).persister).not_to have_received(:save)
-        expect(Valkyrie::MetadataAdapter.find(:index_solr).persister).to have_received(:save_all).at_least(1).times
-
-        file_sets = Valkyrie.config.metadata_adapter.query_service.find_members(resource: reloaded)
-        expect(file_sets.first.file_metadata.length).to eq 3
-      end
-
-      context "when the pending uploads have file URIs for hidden files" do
-        let(:upload_file2) { double }
-        let(:upload_file_id2) { "test-upload-file-id2" }
-        before do
-          allow(upload_file2).to receive(:name).and_return(".hidden.tif")
-          allow(upload_file2).to receive(:id).and_return(upload_file_id2)
-
-          allow(BrowseEverything::UploadFile).to receive(:find).with([upload_file_id2]).and_return([upload_file2])
-          allow(BrowseEverything::UploadFile).to receive(:find).with([upload_file_id]).and_return([upload_file])
-          allow(upload).to receive(:files).and_return([upload_file, upload_file2])
-        end
-
-        it "filters the uploads" do
-          post :browse_everything_files, params: params
-          reloaded = adapter.query_service.find_by(id: resource.id)
-
-          expect(reloaded.member_ids.length).to eq 1
-          expect(reloaded.decorate.file_sets.first.title).to eq [File.basename(file.path)]
-        end
-      end
     end
   end
 
