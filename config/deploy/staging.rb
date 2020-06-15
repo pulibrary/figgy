9e1c0acbe6d77f5d858f6e11afe304d4f2c44b1b
Trey Pendragon <tpendragon@princeton.edu>
2020-06-15 11:14:42 -0700

    Deploy to figgy-staging2.
    Our staging server changed.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server "figgy-staging1", user: "deploy", roles: %w[app db web worker]
+server "figgy-staging2", user: "deploy", roles: %w[app db web worker]
 set :google_fixity_request_topic, "figgy-staging-fixity-request"
 set :google_service_account, "figgy-staging@pulibrary-figgy-storage-1.iam.gserviceaccount.com"
 set :google_fixity_bucket, "figgy-staging-preservation"
