d725d0e28803d1b71dd04f17b7ca0569a0a5b21b
Trey Pendragon <tpendragon@princeton.edu>
2020-06-23 09:16:41 -0700

    Add PluckEarliestUpdatedAt Query

---
+++ app/queries/pluck_earliest_updated_at.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+class PluckEarliestUpdatedAt
+  def self.queries
+    [:pluck_earliest_updated_at]
+  end
+
+  attr_reader :query_service
+  delegate :orm_class, to: :resource_factory
+  delegate :resource_factory, to: :query_service
+  def initialize(query_service:)
+    @query_service = query_service
+  end
+
+  def pluck_earliest_updated_at
+    orm_class.select(:updated_at).order(:updated_at).limit(1).first[:updated_at]
+  end
+end
---
+++ spec/queries/pluck_earliest_updated_at_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe PluckEarliestUpdatedAt do
+  it "returns all ids which are persisted" do
+    resource1 = nil
+    Timecop.freeze(Time.now.utc - 10.minutes) do
+      resource1 = FactoryBot.create_for_repository(:scanned_resource)
+    end
+    Timecop.freeze(Time.now.utc - 5.minutes) do
+      FactoryBot.create_for_repository(:scanned_resource)
+    end
+    query_service = Valkyrie::MetadataAdapter.find(:indexing_persister).query_service
+    expect(query_service.custom_queries.pluck_earliest_updated_at).to eq resource1.updated_at
+  end
+end
