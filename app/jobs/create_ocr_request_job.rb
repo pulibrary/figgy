fce448f7844859901646d2933ebada8866f5ddd2
Eliot Jordan <eliotj@princeton.edu>
2020-06-26 12:00:38 -0500

    Queue PDF OCR request jobs as 'high'

--- app/jobs/create_ocr_request_job.rb
+++ app/jobs/create_ocr_request_job.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class CreateOcrRequestJob < ApplicationJob
   include ActiveStorage::Downloading
+  queue_as :high
 
   def perform(file_path:)
     logger.info("Create OCR Request job initiated for: #{file_path}")
