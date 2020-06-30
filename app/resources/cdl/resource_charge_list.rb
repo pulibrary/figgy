1976e6f4797f1ba718221435d89119b1e1a32648
Anna Headley <anna.headley@gmail.com>
2020-06-30 14:08:58 -0400

    Implement CDL::ChargeManager
    The `CDL::ChargeManager` is a business logic object which creates and
    manages `CDL::ResourceChargeList` and its `CDL::ChargeItem`s.
    
    The implementation currently stubs the EligibleItemService, but after
     #4033 it will pull from bibdata.
    
    Closes #4026
    
    Co-authored-by: Anna Headley <anna.headley@gmail.com>

---
+++ app/resources/cdl/resource_charge_list.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Controlled Digital Lending
+module CDL
+  class ResourceChargeList < Valkyrie::Resource
+    attribute :resource_id, Valkyrie::Types::ID
+    attribute :charged_items, Valkyrie::Types::Set.of(CDL::ChargedItem)
+  end
+end
---
+++ spec/resources/cdl/resource_charge_list_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+describe CDL::ResourceChargeList do
+  describe "#resource_id" do
+    it "returns the figgy id of the resource it manages charges for" do
+      resource = FactoryBot.create_for_repository(:scanned_resource)
+      charge_manager = described_class.new(resource_id: resource.id)
+      expect(charge_manager.resource_id).to eq resource.id
+    end
+  end
+
+  describe "#charged_items" do
+    it "stores an array of ChargedItems" do
+      charge_manager = described_class.new
+
+      charged_item = CDL::ChargedItem.new(item_id: "1", netid: "skye", expiration_time: Time.zone.at(0))
+      charge_manager.charged_items = [charged_item]
+
+      expect(charge_manager.charged_items[0]).to eq charged_item
+    end
+  end
+end
