ce3afafd5fd85b891120576e848d9d5e0727552c
christinach <actspatial@gmail.com>
2020-07-01 08:49:42 -0400

    Adds CDL service, retrieves items with on_cdl= "Y" - from a given bibid resolves #4033

---
+++ app/services/cdl/bibdata_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Cdl
+  class BibdataService
+    class << self
+      def cdl_items(id)
+        item_ids = []
+        conn = Faraday.new(url: "https://bibdata.princeton.edu/")
+        response = conn.get("#{id}/items")
+        response.body
+        items = JSON.parse(response.body).values.map { |l| l[0]["items"] }
+        items.each do |i|
+          if i[0]["on_cdl"] == "Y"
+            item_ids << i[0]["id"]
+          end
+        end
+        item_ids
+      end
+    end
+  end
+end
---
+++ spec/services/cdl/bibdata_service_spec.rb
@@ -0,0 +1,122 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe Cdl::BibdataService do
+  before do
+    stub_request(:get, "https://bibdata.princeton.edu/7214786/items")
+      .to_return(status: 200,
+                 body:
+             {
+               :f => [
+                 {
+                   :holding_id => 1_581_046,
+                   :call_number => "NA203 .G5 1967",
+                   :items => [
+                     {
+                       :id => 1_666_778,
+                       :on_cdl => "N",
+                       :on_reserve => "N",
+                       :copy_number => 1,
+                       :item_sequence_number => 1,
+                       :temp_location => "null",
+                       :perm_location => "f",
+                       :circ_group_id => 1,
+                       :pickup_location_code => "fcirc",
+                       :pickup_location_id => 299,
+                       :enum => "null",
+                       :chron => "null",
+                       :barcode => "32101003160700",
+                       :item_type => "Gen",
+                       :due_date => "null",
+                       :status => [
+                         "Not Charged"
+                       ]
+                     }
+                   ],
+                   :sortable_call_number => "NA.0203.G5.1967"
+                 }
+               ],
+               :st => [
+                 {
+                   :holding_id => 1_581_047,
+                   :call_number => "NA200 .G38 1967",
+                   :items => [
+                     {
+                       :id => 1_666_779,
+                       :on_cdl => "Y",
+                       :on_reserve => "N",
+                       :copy_number => 1,
+                       :item_sequence_number => 1,
+                       :temp_location => "null",
+                       :perm_location => "st",
+                       :circ_group_id => 14,
+                       :pickup_location_code => "stcirc",
+                       :pickup_location_id => 345,
+                       :enum => "null",
+                       :chron => "null",
+                       :barcode => "32101015237520",
+                       :item_type => "Gen",
+                       :due_date => "null",
+                       :status => [
+                         "Not Charged"
+                       ]
+                     }
+                   ]
+                 }
+               ],
+               :uesrf => [
+                 {
+                   :holding_id => 1_581_050,
+                   :call_number => "NA203 .G5 1967",
+                   :items => [
+                     {
+                       :id => 1_666_782,
+                       :on_cdl => "Y",
+                       :on_reserve => "N",
+                       :copy_number => 2,
+                       :item_sequence_number => 1,
+                       :temp_location => "null",
+                       :perm_location => "uesrf",
+                       :circ_group_id => 5,
+                       :pickup_location_code => "uescirc",
+                       :pickup_location_id => 356,
+                       :enum => "null",
+                       :chron => "null",
+                       :barcode => "32101019151941",
+                       :item_type => "NoCirc",
+                       :due_date => "null",
+                       :status => [
+                         "Not Charged"
+                       ]
+                     }
+                   ]
+                 }
+               ]
+             }.to_json,
+                 headers: { "Content-Type" => "application/json" })
+  end
+
+  describe ".cdl_items" do
+    let(:id) { "7214786" }
+
+    it "is a cdl charged item" do
+    end
+
+    it "is not a cdl charged item" do
+    end
+
+    it "will not error if the on_cdl is null" do
+      # return []  if on_cdl = []
+    end
+
+    it "will not error if bib is suppressed," do
+      # it will return an []
+    end
+
+    context "a bib_id with items in more than one locations" do
+      it "returns only the cdl charged items" do
+        expect(described_class.cdl_items(id)).to eq [1_666_779, 1_666_782]
+      end
+    end
+  end
+end
