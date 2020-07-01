2b4bb3dbb91797b05c465235b61495f4c8816271
christinach <actspatial@gmail.com>
2020-07-01 16:25:21 -0400

    update the item_ids method

--- app/services/cdl/eligible_item_service.rb
+++ app/services/cdl/eligible_item_service.rb
@@ -6,16 +6,10 @@ module Cdl
     # on_cdl == "Y"
     class << self
       def item_ids(source_metadata_identifier:)
-        item_ids = []
         conn = Faraday.new(url: "https://bibdata.princeton.edu/")
         response = conn.get("#{source_metadata_identifier}/items")
         items = JSON.parse(response.body).values.map { |l| l[0]["items"] }.compact
-        items.each do |i|
-          if i[0]["on_cdl"] == "Y"
-            item_ids << i[0]["id"]
-          end
-        end
-        item_ids
+        items.select { |i| i[0]["on_cdl"] == "Y" }.map { |i| i[0]["id"] }
       end
     end
   end
