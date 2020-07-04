81cd7405c4c61290ea135dd80dfc69c3a8830de8
Eliot Jordan <eliotj@princeton.edu>
2020-07-04 02:04:17 -0500

    Rescue ActiveJob::DeserializationError (#4052)

---
+++ config/initializers/analyze_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Monkey patch AnalyzeJob so that an ActiveJob::DeserializationError is rescued and
+# does not cause the job to be moved to the retry queue. This is used mainly for
+# OcrRequest blobs that are purged before AnalyzeJob is run.
+ActiveStorage::AnalyzeJob.class_eval do
+  discard_on ActiveJob::DeserializationError
+end
