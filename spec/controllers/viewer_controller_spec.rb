13299ff7226cb69b57f2a29c4d5468eab8b1fa3d
Trey Pendragon <tpendragon@princeton.edu>
2020-07-06 11:54:56 -0700

    Add login button for CDL items.
    Closes #4029

--- spec/controllers/viewer_controller_spec.rb
+++ spec/controllers/viewer_controller_spec.rb
@@ -43,6 +43,18 @@ RSpec.describe ViewerController do
           expect(response).to redirect_to viewer_index_path(anchor: "?manifest=http://www.example.com/concern/scanned_resources/#{resource.id}/manifest")
         end
       end
+      context "and the resource is CDL eligible" do
+        it "displays a CDL-specific login button" do
+          allow(CDL::EligibleItemService).to receive(:item_ids).with(source_metadata_identifier: "123456").and_return(["12345"])
+          stub_bibdata(bib_id: "123456")
+          resource = FactoryBot.create_for_repository(:complete_private_scanned_resource, source_metadata_identifier: "123456")
+
+          get :auth, params: { id: resource.id.to_s }
+
+          expect(response).to be_success
+          expect(response.body).to have_link "Princeton Users: Log In to Digitally Check Out"
+        end
+      end
     end
     context "when the user is logged in" do
       it "redirects back to the viewer" do
