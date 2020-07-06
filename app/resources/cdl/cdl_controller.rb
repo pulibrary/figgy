6523c52e2b1ea67bb631a1abfcccdd39aabd8c2c
Trey Pendragon <tpendragon@princeton.edu>
2020-07-06 14:48:48 -0700

    Use charge manager for status.
    Will make using the charge manager for charging cleaner.

--- app/resources/cdl/cdl_controller.rb
+++ app/resources/cdl/cdl_controller.rb
@@ -3,12 +3,22 @@
 module Cdl
   class CdlController < ApplicationController
     def status
-      resource = query_service.find_by(id: params[:id])
-      render json: CDL::Status.new(resource: resource, user: current_user)
+      @charge_manager = charge_manager(params[:id])
+      render json: CDL::Status.new(charge_manager: @charge_manager, user: current_user)
+    end
+    def charge_manager(resource_id)
+      CDL::ChargeManager.new(
+        resource_id: resource_id,
+        eligible_item_service: CDL::EligibleItemService,
+        change_set_persister: change_set_persister
+      )
     end
 
-    def query_service
-      Valkyrie.config.metadata_adapter.query_service
+    def change_set_persister
+      ChangeSetPersister.new(
+        metadata_adapter: Valkyrie.config.metadata_adapter,
+        storage_adapter: Valkyrie.config.storage_adapter
+      )
     end
   end
 end
