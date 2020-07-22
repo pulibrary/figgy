4c50ce73ed2de8c7bd6e5eaa346687c2d68f519f
Trey Pendragon <tpendragon@princeton.edu>
2020-07-22 13:46:42 -0700

    Ensure holds are activated.

--- spec/resources/cdl/bulk_hold_processor_spec.rb
+++ spec/resources/cdl/bulk_hold_processor_spec.rb
@@ -8,13 +8,38 @@ RSpec.describe CDL::BulkHoldProcessor do
     allow(CDL::EligibleItemService).to receive(:item_ids).and_return(["1"])
     User.create!(uid: "one", email: "one@princeton.edu")
     User.create!(uid: "two", email: "two@princeton.edu")
-    hold1 = FactoryBot.create_for_repository(:resource_charge_list, resource_id: FactoryBot.create_for_repository(:complete_private_scanned_resource).id, expired_hold_netids: ["one", "two"])
-    hold2 = FactoryBot.create_for_repository(:resource_charge_list, resource_id: FactoryBot.create_for_repository(:complete_private_scanned_resource).id)
+    resource_charge_list1 = FactoryBot.create_for_repository(
+      :resource_charge_list,
+      resource_id: FactoryBot.create_for_repository(:complete_private_scanned_resource).id,
+      expired_hold_netids: ["one", "two"]
+    )
+    resource_charge_list2 = FactoryBot.create_for_repository(:resource_charge_list, resource_id: FactoryBot.create_for_repository(:complete_private_scanned_resource).id)
 
     described_class.new(change_set_persister: ScannedResourcesController.change_set_persister).process!
 
-    reloaded_hold1 = query_service.find_by(id: hold1.id)
-    expect(reloaded_hold1.updated_at).not_to eq hold1.updated_at
-    expect(query_service.find_by(id: hold2.id).updated_at).to eq hold2.updated_at
+    reloaded_resource_charge_list1 = query_service.find_by(id: resource_charge_list1.id)
+    expect(reloaded_resource_charge_list1.updated_at).not_to eq resource_charge_list1.updated_at
+    expect(reloaded_resource_charge_list1.hold_queue).to be_empty
+    expect(query_service.find_by(id: resource_charge_list2.id).updated_at).to eq resource_charge_list2.updated_at
+  end
+  it "activates any eligible holds" do
+    allow(CDL::EligibleItemService).to receive(:item_ids).and_return(["1"])
+    User.create!(uid: "one", email: "one@princeton.edu")
+    User.create!(uid: "two", email: "two@princeton.edu")
+    resource_charge_list1 = FactoryBot.create_for_repository(
+      :resource_charge_list,
+      resource_id: FactoryBot.create_for_repository(:complete_private_scanned_resource).id,
+      inactive_hold_netids: ["one", "two"]
+    )
+    resource_charge_list2 = FactoryBot.create_for_repository(:resource_charge_list, resource_id: FactoryBot.create_for_repository(:complete_private_scanned_resource).id)
+
+    described_class.new(change_set_persister: ScannedResourcesController.change_set_persister).process!
+
+    reloaded_resource_charge_list1 = query_service.find_by(id: resource_charge_list1.id)
+    expect(reloaded_resource_charge_list1.updated_at).not_to eq resource_charge_list1.updated_at
+    expect(reloaded_resource_charge_list1.hold_queue.size).to eq 2
+    expect(reloaded_resource_charge_list1.active_holds.size).to eq 1
+    expect(reloaded_resource_charge_list1.active_holds[0].netid).to eq "one"
+    expect(query_service.find_by(id: resource_charge_list2.id).updated_at).to eq resource_charge_list2.updated_at
   end
 end
