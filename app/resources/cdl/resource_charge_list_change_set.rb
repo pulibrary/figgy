ac120ede1a3c5c7c8e80c11ab2b722aa075186ec
Trey Pendragon <tpendragon@princeton.edu>
2020-07-23 15:10:01 -0700

    Enable and test optimistic locking for ResourceChargeList.

--- app/resources/cdl/resource_charge_list_change_set.rb
+++ app/resources/cdl/resource_charge_list_change_set.rb
@@ -4,5 +4,9 @@ module CDL
   class ResourceChargeListChangeSet < Valkyrie::ChangeSet
     property :charged_items
     property :hold_queue
+    property :optimistic_lock_token,
+             multiple: true,
+             required: true,
+             type: Valkyrie::Types::Set.of(Valkyrie::Types::OptimisticLockToken)
   end
 end
--- spec/resources/cdl/resource_charge_list_spec.rb
+++ spec/resources/cdl/resource_charge_list_spec.rb
@@ -20,4 +20,15 @@ describe CDL::ResourceChargeList do
       expect(charge_manager.charged_items[0]).to eq charged_item
     end
   end
+
+  it "has optimistic locking enabled" do
+    resource_charge_list = FactoryBot.create_for_repository(:resource_charge_list)
+    change_set = ChangeSet.for(resource_charge_list)
+    change_set.validate(resource_id: SecureRandom.uuid)
+    ScannedResourcesController.change_set_persister.save(change_set: change_set)
+
+    change_set = ChangeSet.for(resource_charge_list)
+    change_set.validate(resource_id: SecureRandom.uuid)
+    expect { ScannedResourcesController.change_set_persister.save(change_set: change_set) }.to raise_error Valkyrie::Persistence::StaleObjectError
+  end
 end
