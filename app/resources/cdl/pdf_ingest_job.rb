d92a71c4095a20eb2472190aa5126a5850b3fe3a
Trey Pendragon <tpendragon@princeton.edu>
2020-08-04 14:52:03 -0700

    Add some comments.

--- app/resources/cdl/pdf_ingest_job.rb
+++ app/resources/cdl/pdf_ingest_job.rb
@@ -5,9 +5,12 @@ module CDL
     queue_as :high
     def perform(file_name:)
       file_path = Pathname.new(Figgy.config["cdl_in_path"]).join(file_name)
+      # Open the file so it'll error if it doesn't exist.
       file = File.open(file_path)
+      # Use IngestableFile to decorate the necessary methods to ingest via
+      # FileAppender. copyable: true makes it "mv" instead of creating a
+      # duplicate file.
       ingestable_file = IngestableFile.new(file_path: file.path, mime_type: "application/pdf", original_filename: file_path.basename, copyable: true)
-
       source_metadata_identifier = file_path.basename(".*").to_s
       change_set = CDL::ResourceChangeSet.new(ScannedResource.new)
       change_set.validate(files: [ingestable_file], source_metadata_identifier: source_metadata_identifier, depositor: "cdl_auto_ingest")
