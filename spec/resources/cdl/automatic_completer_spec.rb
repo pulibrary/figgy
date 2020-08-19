647e64c27c48788a6044028a63d4cd0ca8c89e58
Trey Pendragon <tpendragon@princeton.edu>
2020-08-19 13:37:15 -0700

    CDL AutomaticCompleter should check for Draft instead of Pending
    Closes #4204

--- spec/resources/cdl/automatic_completer_spec.rb
+++ spec/resources/cdl/automatic_completer_spec.rb
@@ -9,18 +9,18 @@ RSpec.describe CDL::AutomaticCompleter do
   describe ".run" do
     context "when there's in process CDL items" do
       it "does not complete them" do
-        resource = FactoryBot.create_for_repository(:pending_cdl_resource, files: [file])
+        resource = FactoryBot.create_for_repository(:draft_cdl_resource, files: [file])
 
         described_class.run
 
         resource = query_service.find_by(id: resource.id)
-        expect(resource.state).to eq ["pending"]
+        expect(resource.state).to eq ["draft"]
       end
     end
     context "when the PDF page count doesn't match the file count" do
       with_queue_adapter :inline
       it "doesn't complete them" do
-        resource = FactoryBot.create_for_repository(:pending_cdl_resource, files: [file])
+        resource = FactoryBot.create_for_repository(:draft_cdl_resource, files: [file])
         cs = ChangeSet.for(resource)
         cs.validate(files: [file])
         change_set_persister.save(change_set: cs)
@@ -28,7 +28,9 @@ RSpec.describe CDL::AutomaticCompleter do
         described_class.run
 
         resource = query_service.find_by(id: resource.id)
-        expect(resource.state).to eq ["pending"]
+        expect(resource.state).to eq ["draft"]
+        # Ensure emails aren't sent if nothing's completed.
+        expect(ActionMailer::Base.deliveries.size).to eq 0
       end
     end
     context "when there's CDL items with processed files", run_real_derivatives: true, run_real_characterization: true do
@@ -39,7 +41,7 @@ RSpec.describe CDL::AutomaticCompleter do
         User.create!(uid: "skye", email: "skye@princeton.edu")
         User.create!(uid: "zelda", email: "zelda@princeton.edu")
         collection = FactoryBot.create_for_repository(:collection, slug: "cdl", title: "CDL", owners: ["skye", "zelda"])
-        resource = FactoryBot.create_for_repository(:pending_cdl_resource, files: [file], member_of_collection_ids: [collection.id], source_metadata_identifier: "123456")
+        resource = FactoryBot.create_for_repository(:draft_cdl_resource, files: [file], member_of_collection_ids: [collection.id], source_metadata_identifier: "123456")
 
         described_class.run
 
@@ -55,13 +57,13 @@ RSpec.describe CDL::AutomaticCompleter do
     context "when the manifest builder fails to generate", run_real_derivatives: true, run_real_characterization: true do
       with_queue_adapter :inline
       it "doesn't complete it" do
-        resource = FactoryBot.create_for_repository(:pending_cdl_resource, files: [file])
+        resource = FactoryBot.create_for_repository(:draft_cdl_resource, files: [file])
         allow_any_instance_of(ManifestBuilder).to receive(:build).and_raise("Broken")
 
         described_class.run
 
         resource = query_service.find_by(id: resource.id)
-        expect(resource.state).to eq ["pending"]
+        expect(resource.state).to eq ["draft"]
       end
     end
   end
