eecd1df4b89dde942d0856da6d9dfde4a4f40bbf
Trey Pendragon <tpendragon@princeton.edu>
2020-09-23 10:50:08 -0700

    Fix spec description.

--- spec/queries/latest_member_timestamp_spec.rb
+++ spec/queries/latest_member_timestamp_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe LatestMemberTimestamp do
   let(:query_service) { Valkyrie.config.metadata_adapter.query_service }
 
   describe "#latest_member_timestamp" do
-    it "finds all children through a hierarchy with a given property" do
+    it "returns the most recent timestamp for a resource's members" do
       child1 = FactoryBot.create_for_repository(:file_set)
       child2 = FactoryBot.create_for_repository(:file_set)
       parent = FactoryBot.create_for_repository(:scanned_resource, member_ids: [child1.id, child2.id])
