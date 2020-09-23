c984790eec1602d204f6b44184cdb2beca8e7ee2
Trey Pendragon <tpendragon@princeton.edu>
2020-09-23 10:52:50 -0700

    Add some documentation.

--- app/values/manifest_key.rb
+++ app/values/manifest_key.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Calculates a unique cache key for use with `Rails.cache.fetch` which
+# auto-invalidates when the resource or its children change.
 class ManifestKey
   def self.for(resource)
     new(resource: resource, query_service: query_service)
@@ -10,11 +12,14 @@ class ManifestKey
   end
 
   attr_reader :resource, :query_service
+  # @param resource [Valkyrie::Resource]
+  # @param query_service [Valkyrie::MetadataAdapter::QueryService]
   def initialize(resource:, query_service:)
     @resource = resource
     @query_service = query_service
   end
 
+  # @return [String] unique cache key.
   def to_s
     # Call #to_f first so the microsecond timestamps carry over.
     "#{resource.class}/#{resource.updated_at.to_f}/#{member_updated_at.to_f}"
