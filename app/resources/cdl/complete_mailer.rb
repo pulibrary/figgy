1b5fbd56110a403713e0133deb78a6c1ae097c62
Kate Lynch <katelynch@princeton.edu>
2020-09-25 15:37:36 -0400

    Fixed reserve email

--- app/resources/cdl/complete_mailer.rb
+++ app/resources/cdl/complete_mailer.rb
@@ -11,7 +11,7 @@ module CDL
         User.where(uid: owner).first
       end
       mail(
-        to: ["reserves@princeton.edu"] + @users.map(&:email),
+        to: ["reserve@princeton.edu"] + @users.map(&:email),
         subject: "#{@resources.size} CDL Item(s) Completed"
       )
     end
