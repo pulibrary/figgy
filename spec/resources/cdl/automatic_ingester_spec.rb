dcd4249215aab28f73421226039aa300041cdbf5
Trey Pendragon <tpendragon@princeton.edu>
2020-09-29 13:57:26 -0700

    Run CDL a lot more often.
    This will ingest any CDL file that hasn't been modified in the last
    hour. Should decrease maximum time between copy and ingest to two hours.

--- spec/resources/cdl/automatic_ingester_spec.rb
+++ spec/resources/cdl/automatic_ingester_spec.rb
@@ -4,10 +4,15 @@ require "rails_helper"
 
 RSpec.describe CDL::AutomaticIngester do
   context "when given multiple files, some of which are PDFs" do
-    it "queues a background job to ingest each of the PDFs with a valid source metadata identifier" do
+    it "queues a background job to ingest each of the PDFs with a valid source metadata identifier that are older than 1 hour" do
+      allow(File).to receive(:mtime).and_call_original
       file_path = Pathname.new(Figgy.config["cdl_in_path"])
       FileUtils.mkdir_p(file_path.join("ingesting")) unless File.exist?(file_path.join("ingesting"))
       FileUtils.cp(Rails.root.join("spec", "fixtures", "files", "sample.pdf"), file_path.join("123456.pdf"))
+      allow(File).to receive(:mtime).with(file_path.join("123456.pdf").to_s).and_return(1.hour.ago)
+      # Recently copied file - don't ingest.
+      FileUtils.cp(Rails.root.join("spec", "fixtures", "files", "sample.pdf"), file_path.join("56789.pdf"))
+      # Not a bib - don't ingest.
       FileUtils.cp(Rails.root.join("spec", "fixtures", "files", "sample.pdf"), file_path.join("notabib.pdf"))
       # Ensure already ingesting files aren't ingested again.
       FileUtils.cp(Rails.root.join("spec", "fixtures", "files", "sample.pdf"), file_path.join("ingesting", "45678.pdf"))
