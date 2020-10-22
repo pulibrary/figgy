dd661c20fda958cb996f3c217b94e4c0438ec269
Cliff Wulfman <cwulfman@gmail.com>
2020-10-22 16:29:53 -0400

    pull candidate

--- app/services/ingest_ephemera_csv.rb
+++ app/services/ingest_ephemera_csv.rb
@@ -14,23 +14,18 @@ class IngestEphemeraCSV
   end
 
   def ingest
-    ingested = []
-    mdata_table.each do |row|
+    mdata_table.collect do |row|
       folder_data = FolderData.new(base_path: imgdir, **row.to_h)
-      logger.info "validating #{row.to_s}"
       change_set.validate(folder_data.attributes)
       change_set.validate(files: folder_data.files)
       change_set.validate(append_id: project_id)
-      logger.info "persisting #{row.to_s}"
-      ingested << change_set_persister.save(change_set: change_set)
-      logger.info "finished #{row.to_s}"
+      change_set_persister.save(change_set: change_set)
     end
-    ingested
   end
 
   class FolderData
     attr_accessor :image_path, :fields
-    
+
     def initialize(base_path:, **arg_fields)
       @image_path = File.join(base_path, arg_fields[:path])
       @fields = arg_fields.except(:path)
@@ -38,13 +33,17 @@ class IngestEphemeraCSV
 
     def attributes
       {
+        subject: fields[:subject],
+        title: fields[:title],
+        language: fields[:language],
+        genre: fields[:genre],
+        page_count: fields[:page_count],
         creator: fields[:creator],
         description: fields[:description],
         date_created: fields[:date_created],
-        geo_subject:  [fields[:geographic_subject]]
+        geo_subject: [fields[:geographic_subject]]
       }
-    end        
-
+    end
 
     def files
       @files || Dir.glob("#{image_path}/*.{tif,jpg,jpeg,png}").sort.map do |file|
@@ -52,72 +51,19 @@ class IngestEphemeraCSV
           file_path: file,
           mime_type: case File.extname(file)
                      when ".tif" then "image/tiff"
-                     when ".jpeg",".jpg" then "image/jpeg"
+                     when ".jpeg", ".jpg" then "image/jpeg"
                      when ".png" then "image/png"
                      end,
           original_filename: File.basename(file),
           copyable: true
-          )          
+        )
       end
     end
   end
-  
+
   private
 
     def change_set
       @change_set ||= BoxlessEphemeraFolderChangeSet.new(EphemeraFolder.new)
-    end
-
-    def mdata_attributes
-      {
-        creator: mdata[:creator],
-        description: mdata[:description],
-        date_created: mdata[:date_created],
-        geo_subject:  mdata[:geographic_subject]
-      }
-    end        
-
-    def find_term(label: nil, code: nil, vocab: nil)
-      query_service.custom_queries.find_ephemera_term_by_label(label: label, code: code, parent_vocab_label: vocab).id
-    rescue
-      label
-    end
-
-    def title_attributes
-      { title: mdata[:title] }
-    end
-
-    def first_title
-      mdata[:title].first
-    end
-
-    def base_attributes
-      {
-        rights_statement: RightsStatements.copyright_not_evaluated.to_s
-      }
-    end
-
-    def files
-      @files || Dir.glob("#{dir}/*.{tif,jpg,jpeg,png}").sort.map do |file|
-        IngestableFile.new(
-          file_path: file,
-          mime_type: case File.extname(file)
-                     when ".tif" then "image/tiff"
-                     when ".jpeg",".jpg" then "image/jpeg"
-                     when ".png" then "image/png"
-                     end,
-          original_filename: File.basename(file),
-          copyable: true
-          )          
-      end
-    end
-
-    def csv_file
-      IngestableFile.new(
-        file_path: mdata,
-        mime_type: "text/csv",
-        original_filename: File.basename(mods),
-        copyable: true
-      )
     end
 end
