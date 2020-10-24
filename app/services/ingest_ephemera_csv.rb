65f1f8c088cde24b8b134276c6489a943b631920
Cliff Wulfman <cwulfman@gmail.com>
2020-10-24 15:06:09 -0400

    starts to implement methods for vocabularies

--- app/services/ingest_ephemera_csv.rb
+++ app/services/ingest_ephemera_csv.rb
@@ -15,7 +15,7 @@ class IngestEphemeraCSV
 
   def ingest
     mdata_table.collect do |row|
-      folder_data = FolderData.new(base_path: imgdir, **row.to_h)
+      folder_data = FolderData.new(base_path: imgdir, change_set_persister: change_set_persister,  **row.to_h)
       change_set.validate(folder_data.attributes)
       change_set.validate(files: folder_data.files)
       change_set.validate(append_id: project_id)
@@ -24,11 +24,14 @@ class IngestEphemeraCSV
   end
 
   class FolderData
-    attr_accessor :image_path, :fields
+    attr_accessor :image_path, :fields, :change_set_persister
+    delegate :metadata_adapter, to: :change_set_persister
+    delegate :query_service, :persister, to: :metadata_adapter
 
-    def initialize(base_path:, **arg_fields)
+    def initialize(base_path:, change_set_persister:,  **arg_fields)
       @image_path = File.join(base_path, arg_fields[:path])
       @fields = arg_fields.except(:path)
+      @change_set_persister = change_set_persister
     end
 
     # rubocop:disable Metrics/MethodLength
@@ -40,7 +43,7 @@ class IngestEphemeraCSV
         sort_title: Set.new(Array(fields[:sort_title])),
         alternative_title: Set.new(Array(fields[:alternative_title])),
         transliterated_title: Set.new(Array(fields[:transliterated_title])),
-        language: Set.new(Array(fields[:language])),
+        language: language,
         genre: fields[:genre],
         width: Set.new(Array(fields[:width])),
         height: Set.new(Array(fields[:height])),
@@ -53,7 +56,7 @@ class IngestEphemeraCSV
         publisher: Set.new(Array(fields[:publisher])),
         geographic_origin: Array(fields[:geographic_origin]),
         subject: Set.new(Array(fields[:subject])),
-        geo_subject: Set.new(Array(fields[:geo_subject])),
+        geo_subject: geo_subject,
         description: Set.new(Array(fields[:description])),
         date_created: Set.new(Array(fields[:date_created])),
         provenance: Set.new(Array(fields[:provenance])),
@@ -79,6 +82,70 @@ class IngestEphemeraCSV
           copyable: true
         )
       end
+    end
+
+    def date_created
+      return if self["date_created"] == "Unknown"
+      self["date_created"]
+    end
+    
+    def names
+      self["names"] || []
+    end
+    
+    def language
+      return unless fields[:language].present?
+      @language ||= find_or_create_term_by(label: ISO_639.find_by_code(resource["language"]).english_name.split(";").first).id
+    end
+    
+    def geo_origin
+      return unless fields[:geo_origin].present?
+      @geo_origin ||= find_or_create_term_by(label: fields[:geo_origin]).id
+    end
+    
+    def subject
+      return unless fields[:subjects].present?
+      fields[:subjects].uniq.map do |sub|
+        find_or_create_subject_by(category: sub["category"], topic: sub["topic"]).id
+      end
+    end
+    
+    def geo_subject
+      return unless fields[:geo_subject].present?
+      Array(find_term(label: Array(fields[:geo_subject]).first, vocab: "LAE Areas"))
+    end
+
+    
+    def find_term(label: nil, code: nil, vocab: nil)
+      query_service.custom_queries.find_ephemera_term_by_label(label: label, code: code, parent_vocab_label: vocab).id
+    rescue
+      label
+    end
+
+    def find_or_create_term_by(label:)
+      query_service.custom_queries.find_ephemera_term_by_label(label: label) ||
+        persister.save(resource: EphemeraTerm.new(label: label, member_of_vocabulary_id: imported_vocabulary.id))
+    end
+    
+    def imported_vocabulary
+      @imported_vocabulary ||= find_or_create_vocabulary_by(label: "Imported Terms")
+    end
+    
+    def find_or_create_vocabulary_by(label:, vocabulary_id: nil)
+      query_service.custom_queries.find_ephemera_vocabulary_by_label(label: label) ||
+        persister.save(resource: EphemeraVocabulary.new(label: label, member_of_vocabulary_id: vocabulary_id))
+    end
+    
+    def find_or_create_subject_by(category:, topic:)
+      query_service.custom_queries.find_ephemera_term_by_label(label: topic, parent_vocab_label: category) ||
+        create_subject_by(category: category, topic: topic)
+    rescue
+      create_subject_by(category: category, topic: topic)
+    end
+    
+    def create_subject_by(category:, topic:)
+      vocabulary = find_or_create_vocabulary_by(label: category, vocabulary_id: imported_vocabulary.id)
+      persister.save(resource: EphemeraTerm.new(label: topic, member_of_vocabulary_id: vocabulary.id))
     end
   end
 
