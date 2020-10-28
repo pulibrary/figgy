e956dcf34bfc823f342955a343e4dae650cc7b9b
Cliff Wulfman <cwulfman@gmail.com>
2020-10-28 16:25:46 -0400

    working on tests

---
+++ app/services/vocabulary_service.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class VocabularyService
+  attr_accessor :change_set_persister, :persist_if_not_found, :imported_vocabulary
+  delegate :metadata_adapter, to: :change_set_persister
+  delegate :query_service, :persister, to: :metadata_adapter
+
+  def initialize(change_set_persister:, persist_if_not_found: false)
+    @change_set_persister = change_set_persister
+    @persist_if_not_found = persist_if_not_found
+  end
+
+  class EphemeraVocabularyService < VocabularyService
+    def initialize(change_set_persister:, persist_if_not_found: true)
+      super(change_set_persister: change_set_persister,
+            persist_if_not_found: persist_if_not_found)
+    end
+    
+    def imported_vocabulary
+      @imported_vocabulary ||= find_vocabulary_by(label: "Imported Terms")
+    end
+    
+    def find_vocabulary_by(label:, vocabulary_id: nil)
+      vocab = query_service.custom_queries.find_ephemera_vocabulary_by_label(label: label)
+      return vocab if vocab
+      if persist_if_not_found
+        persister.save(resource: EphemeraVocabulary.new(label: label, member_of_vocabulary_id: vocabulary_id))
+      end
+    end
+    
+    def find_term(label: nil, code: nil, vocab: nil)
+      term = query_service.custom_queries.find_ephemera_term_by_label(label: label,
+                                                                      code: code,
+                                                                      parent_vocab_label: vocab)
+      return term.id if term
+      if persist_if_not_found
+        persister.save(resource: EphemeraTerm.new(label: label, member_of_vocabulary_id: imported_vocabulary.id))
+      end
+    end
+    
+    def find_subject_by(category:, topic:)
+      vocabulary = find_vocabulary_by(label: category, vocabulary_id: imported_vocabulary.id)
+      subject = query_service.custom_queries.find_ephemera_term_by_label(label: topic, parent_vocab_label: category)
+      return subject if subject
+      if persist_if_not_found
+
+        persister.save(resource: EphemeraTerm.new(label: topic, member_of_vocabulary_id: vocabulary.id))
+      end
+    end
+  end
+end
