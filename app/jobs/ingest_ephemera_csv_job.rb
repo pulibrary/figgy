7793aa68773515b6399eb37231b2666f1bdc3ba4
Cliff Wulfman <cwulfman@gmail.com>
2020-11-09 09:43:05 -0500

    removes outdated test

--- app/jobs/ingest_ephemera_csv_job.rb
+++ app/jobs/ingest_ephemera_csv_job.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 class IngestEphemeraCSVJob < ApplicationJob
-
   def perform(csvfile, basedir)
-
     logger.info "Ingesting csv file #{csvfile}"
     change_set_persister = ChangeSetPersister.new(
       metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
@@ -11,9 +9,7 @@ class IngestEphemeraCSVJob < ApplicationJob
     change_set_persister.queue = queue_name
     output = nil
     change_set_persister.buffer_into_index do |buffered_changeset_persister|
-
       output = IngestEphemeraCSV.new(csvfile, basedir, buffered_changeset_persister, logger).ingest
-
     end
     logger.info "Ingested #{csvfile}: #{output.count} objects"
   end
