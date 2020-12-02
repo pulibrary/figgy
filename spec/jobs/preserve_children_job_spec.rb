a045694460ec09f46c04a7e4d572fa2a45e11573
Trey Pendragon <tpendragon@princeton.edu>
2020-12-02 14:22:22 -0800

    PreserveChildrenJob should not error when object not found.
    Closes #2438

---
+++ spec/jobs/preserve_children_job_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PreserveChildrenJob do
+  it "does not error when given a non-existent ID" do
+    expect { described_class.perform_now(id: "none") }.not_to raise_error
+  end
+end
