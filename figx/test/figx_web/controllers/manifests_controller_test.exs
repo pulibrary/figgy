a0da3289c27731abd8ad97613e0b24525a8aadd0
Trey Pendragon <tpendragon@princeton.edu>
2021-04-23 12:59:55 -0700

    Run figx seed before Elixir test suite.
    Co-authored-by: Hector Correa <hector@hectorcorrea.com>
    Co-authored-by: Anna Headley <anna.headley@gmail.com>
    Co-authored-by: Eliot Jordan <eliot.jordan@gmail.com>

--- figx/test/figx_web/controllers/manifests_controller_test.exs
+++ figx/test/figx_web/controllers/manifests_controller_test.exs
@@ -6,8 +6,10 @@ defmodule FigxWeb.ManifestsControllerTest do
   end
 
   test "GET /manifest/id", %{conn: conn} do
-    resource_id = "hi"
+    resource_id = "597edce8-3a2f-41cd-be2b-182dae7b9a8f"
     conn = get(conn, "/manifest/#{resource_id}")
-    assert json_response(conn, 200)["id"] =~ "hi"
+    json = json_response(conn, 200)
+    assert json["id"] == "597edce8-3a2f-41cd-be2b-182dae7b9a8f"
+    assert json["label"] == ["Test Collection"]
   end
 end
