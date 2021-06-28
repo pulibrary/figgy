9547d2868b0e829c264ec1194deb7338dc519976
Eliot Jordan <eliotj@princeton.edu>
2021-06-28 17:16:47 -0500

    Remove numismatics import service

--- app/jobs/issue_import_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class IssueImportJob < ApplicationJob
-  def perform(issue_number:, collection_id:, depositor:, file_root:, db_adapter_name: "NumismaticsImportService::SqliteAdapter", **db_options)
-    db_adapter = db_adapter_name.constantize.new(db_options)
-    logger.info "Importing numismatic issue: #{issue_number}"
-    importer = NumismaticsImportService.new(db_adapter: db_adapter, collection_id: collection_id, depositor: depositor, logger: logger, file_root: file_root)
-    importer.ingest_issue(issue_number: issue_number)
-  end
-end
