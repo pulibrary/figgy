babd13aee162476f64f7115bfb9139841470cb8b
Thanya Begum <tbegum@princeton.edu>
2021-09-24 10:42:31 -0400

    Remove media resources folder

--- app/resources/media_resources/media_resources_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class MediaResourcesController < ResourceController
-  self.resource_class = MediaResource
-  self.change_set_persister = ::ChangeSetPersister.new(
-    metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
-    storage_adapter: Valkyrie.config.storage_adapter
-  )
-end
