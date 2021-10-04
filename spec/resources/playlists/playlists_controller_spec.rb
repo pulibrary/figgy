8b696c65a177561fca4e2be9fcbfddfc78193665
Eliot Jordan <eliotj@princeton.edu>
2021-10-04 12:39:24 -0500

    Merged playlists controller specs
    Co-authored-by: Anna Headley <anna.headley@gmail.com>
    Co-authored-by: Shaun Ellis <shaun@sdellis.com>

--- spec/resources/playlists/playlists_controller_spec.rb
+++ spec/resources/playlists/playlists_controller_spec.rb
@@ -13,9 +13,11 @@ RSpec.describe PlaylistsController, type: :controller do
   before do
     sign_in user if user
   end
+
   describe "new" do
     it_behaves_like "an access controlled new request"
   end
+
   describe "create" do
     let(:valid_params) do
       {
@@ -28,10 +30,12 @@ RSpec.describe PlaylistsController, type: :controller do
         visibility: "restricted"
       }
     end
+
     context "access control" do
       let(:params) { valid_params }
       it_behaves_like "an access controlled create request"
     end
+
     context "it creates a playlist with the media reserve filesets" do
       let(:user) { FactoryBot.create(:admin) }
       let(:params) { { recording_id: resource.id } }
@@ -55,12 +59,14 @@ RSpec.describe PlaylistsController, type: :controller do
       end
     end
   end
+
   describe "destroy" do
     context "access control" do
       let(:factory) { :playlist }
       it_behaves_like "an access controlled destroy request"
     end
   end
+
   describe "edit" do
     let(:user) { FactoryBot.create(:admin) }
     context "access control" do
@@ -68,6 +74,7 @@ RSpec.describe PlaylistsController, type: :controller do
       it_behaves_like "an access controlled edit request"
     end
   end
+
   describe "html update" do
     let(:user) { FactoryBot.create(:admin) }
 
@@ -136,36 +143,37 @@ RSpec.describe PlaylistsController, type: :controller do
         end
       end
     end
-    describe "#manifest" do
-      context "with a Playlist proxying to audio FileSets", run_real_characterization: true do
-        with_queue_adapter :inline
 
-        let(:tika_output) { tika_wav_output }
+    describe "#manifest", run_real_characterization: true do
+      with_queue_adapter :inline
 
-        let(:file1) { fixture_file_upload("files/audio_file.wav") }
-        let(:file2) { fixture_file_upload("av/la_demo_bag/data/32101047382484_1_pm.wav") }
-        let(:recording) { FactoryBot.create_for_repository(:scanned_resource, files: [file1, file2]) }
-        let(:file_set1) do
-          recording.decorate.decorated_file_sets.first
-        end
-        let(:file_set2) do
-          recording.decorate.decorated_file_sets.last
-        end
-        let(:proxy1) do
-          res = ProxyFileSet.new(proxied_file_id: file_set1.id)
-          cs = ProxyFileSetChangeSet.new(res)
-          change_set_persister.save(change_set: cs)
-        end
-        let(:proxy2) do
-          res = ProxyFileSet.new(proxied_file_id: file_set2.id)
-          cs = ProxyFileSetChangeSet.new(res)
-          change_set_persister.save(change_set: cs)
-        end
-        let(:resource) do
-          FactoryBot.create_for_repository(:playlist, member_ids: [proxy1.id, proxy2.id])
-        end
-        let(:user) { FactoryBot.create(:admin) }
+      let(:tika_output) { tika_wav_output }
 
+      let(:file1) { fixture_file_upload("files/audio_file.wav") }
+      let(:file2) { fixture_file_upload("av/la_demo_bag/data/32101047382484_1_pm.wav") }
+      let(:recording) { FactoryBot.create_for_repository(:scanned_resource, files: [file1, file2]) }
+      let(:file_set1) do
+        recording.decorate.decorated_file_sets.first
+      end
+      let(:file_set2) do
+        recording.decorate.decorated_file_sets.last
+      end
+      let(:proxy1) do
+        res = ProxyFileSet.new(proxied_file_id: file_set1.id, label: "Proxy Title")
+        cs = ProxyFileSetChangeSet.new(res)
+        change_set_persister.save(change_set: cs)
+      end
+      let(:proxy2) do
+        res = ProxyFileSet.new(proxied_file_id: file_set2.id, label: "Proxy Title2")
+        cs = ProxyFileSetChangeSet.new(res)
+        change_set_persister.save(change_set: cs)
+      end
+      let(:resource) do
+        FactoryBot.create_for_repository(:playlist, member_ids: [proxy1.id, proxy2.id])
+      end
+      let(:user) { FactoryBot.create(:admin) }
+
+      context "with a Playlist proxying to audio FileSets" do
         it "generates the IIIF Manifest" do
           get :manifest, params: { id: resource.id, format: :json }
 
@@ -198,23 +206,100 @@ RSpec.describe PlaylistsController, type: :controller do
           expect(last_annotation["body"]).to include("format" => "application/vnd.apple.mpegurl")
         end
       end
-    end
 
-    context "when an invalid resource ID is requested" do
-      it "returns a not found status response" do
-        get :manifest, params: { id: "invalid", format: :json }
+      context "when an invalid resource ID is requested" do
+        it "returns a not found status response" do
+          get :manifest, params: { id: "invalid", format: :json }
 
-        expect(response.status).to eq(200)
-        expect(response.body).not_to be_empty
-        response_message = JSON.parse(response.body)
+          expect(response.status).to eq(200)
+          expect(response.body).not_to be_empty
+          response_message = JSON.parse(response.body)
 
-        expect(response_message).to include("message" => "No manifest found for invalid")
+          expect(response_message).to include("message" => "No manifest found for invalid")
+        end
+      end
+
+      context "when authenticated" do
+        let(:user) { FactoryBot.create(:admin) }
+
+        before do
+          sign_in user
+        end
+
+        it "generates the manifest for the resource" do
+          get :manifest, params: { id: resource.id.to_s }, format: :json
+
+          expect(response.body).not_to be_empty
+          manifest_values = JSON.parse(response.body)
+          expect(manifest_values).not_to be_empty
+
+          expect(manifest_values).to include("items")
+          expect(manifest_values["items"].length).to eq(2)
+
+          first_canvas = manifest_values["items"].first
+          expect(first_canvas["label"]["@none"]).to eq ["Proxy Title"]
+
+          expect(first_canvas).to include("items")
+          expect(first_canvas["items"].length).to eq(1)
+          anno_page = first_canvas["items"].first
+          expect(anno_page).to include("items")
+          expect(anno_page["items"].length).to eq(1)
+          first_annotation = anno_page["items"].first
+          expect(first_annotation).to include("body")
+          expect(first_annotation["body"]).to include("format" => "application/vnd.apple.mpegurl")
+
+          last_canvas = manifest_values["items"].last
+          expect(last_canvas).to include("items")
+          expect(last_canvas["items"].length).to eq(1)
+          anno_page = last_canvas["items"].first
+          expect(anno_page).to include("items")
+          expect(anno_page["items"].length).to eq(1)
+          last_annotation = anno_page["items"].first
+          expect(last_annotation).to include("body")
+          expect(last_annotation["body"]).to include("format" => "application/vnd.apple.mpegurl")
+        end
+
+        context "when transmitting a HEAD request" do
+          it "responds with a link header specifying the title of the resource" do
+            head :manifest, params: { id: resource.id.to_s }, format: :json
+
+            expect(response).to be_successful
+            expect(response.headers).to include "Link"
+            expect(response.headers["Link"]).to eq "<http://test.host/concern/playlists/#{resource.id}/manifest>; rel=\"self\"; title=\"#{resource.title.first}\""
+          end
+        end
+      end
+
+      context "when the request contains an authorization token" do
+        let(:persisted) do
+          change_set = PlaylistChangeSet.new(resource)
+          change_set.validate(state: ["complete"])
+          change_set_persister.save(change_set: change_set)
+        end
+
+        it "generates a manifest with URLs containing the auth. token" do
+          get :manifest, params: { id: persisted.id.to_s, auth_token: persisted.auth_token }, format: :json
+
+          expect(response.body).not_to be_empty
+          manifest_values = JSON.parse(response.body)
+
+          expect(manifest_values).not_to be_empty
+          expect(manifest_values).to include("items")
+          expect(manifest_values["items"].length).to eq(2)
+
+          first_canvas = manifest_values["items"].first
+          first_annotation_page = first_canvas["items"].first
+          first_annotation = first_annotation_page["items"].first
+          file_node1 = file_set1.file_metadata.select(&:derivative?).first
+          expect(first_annotation["body"]).to include("id" => "http://www.example.com/downloads/#{file_set1.id}/file/#{file_node1.id}?auth_token=#{persisted.auth_token}")
+        end
       end
     end
   end
 
   describe "structure" do
     let(:user) { FactoryBot.create(:admin) }
+
     context "when not logged in" do
       let(:user) { nil }
       it "redirects to login or root" do
@@ -224,12 +309,14 @@ RSpec.describe PlaylistsController, type: :controller do
         expect(response).to be_redirect
       end
     end
+
     context "when a playlist doesn't exist" do
       it "raises an error" do
         get :structure, params: { id: "banana" }
         expect(response).to redirect_to_not_found
       end
     end
+
     context "when it does exist" do
       render_views
       it "renders a structure editor form" do
