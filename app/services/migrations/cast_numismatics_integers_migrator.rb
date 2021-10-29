52d01a3c8bc75b3282d5bc8ad87b3e90967dcf3a
Anna Headley <anna.headley@gmail.com>
2021-10-29 16:50:17 -0400

    Add CastNumismaticsIntegersMigrator

---
+++ app/services/migrations/cast_numismatics_integers_migrator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Migrations
+  class CastNumismaticsIntegersMigrator
+    def self.run
+      csp = ChangeSetPersister.default
+      [Numismatics::Accession, Numismatics::Issue, Numismatics::Coin].each do |klass|
+        csp.query_service.find_all_of_model(model: klass).each do |resource|
+          csp.save(change_set: ChangeSet.for(resource))
+        end
+      end
+    end
+  end
+end
