884ec4559c4f1e84eaabb47a7f850cf2243d63fa
Trey Pendragon <tpendragon@princeton.edu>
2021-11-08 21:24:53 -0800

    Fix path generation.

--- figx/config/staging.exs
+++ figx/config/staging.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :figx, FigxWeb.Endpoint,
-  url: [host: "figgy-staging", port: 443],
+  url: [host: "figgy-staging.princeton.edu", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
