427d693d3c7f25fac4aaa3d92737bb181dd0ca66
Trey Pendragon <tpendragon@princeton.edu>
2021-11-12 21:17:59 -0800

    Fix layouts.

--- figx/lib/figx_web/router.ex
+++ figx/lib/figx_web/router.ex
@@ -5,6 +5,7 @@ defmodule FigxWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
+    plug :put_root_layout, {FigxWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
