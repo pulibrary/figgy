8462be7039d9a7d1bcbe21e6cc34f4e39ee98fe0
Eliot Jordan <eliotj@princeton.edu>
2021-12-16 10:55:44 -0600

    Build correct base path depending on storage adapter
    Co-authored-by: Anna Headley <anna.headley@gmail.com>

--- app/services/mosaic_generator.rb
+++ app/services/mosaic_generator.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
 class MosaicGenerator
-  attr_reader :resource, :storage_adapter
+  attr_reader :resource
   # @param resource [RasterResource]
   def initialize(resource:)
     @resource = resource.decorate
   end
 
   def generate
-    path = Valkyrie::Storage::Disk::BucketedStorage.new(base_path: "s3://figgy-geo-staging").generate(resource: resource, original_filename: "mosaic.json", file: nil).to_s
+    Valkyrie::Storage::Disk::BucketedStorage.new(base_path: base_path).generate(resource: resource, original_filename: "mosaic.json", file: nil).to_s
   end
+
+  private
+
+    def base_path
+      if storage_adapter.is_a? Valkyrie::Storage::Shrine
+        "s3://#{storage_adapter.shrine.bucket.name}"
+      else
+        storage_adapter.storage_adapter.base_path.to_s
+      end
+    end
+
+    def storage_adapter
+      @storage_adapter ||= Valkyrie::StorageAdapter.find(:cloud_geo_derivatives)
+    end
 end
--- spec/services/mosaic_generator_spec.rb
+++ spec/services/mosaic_generator_spec.rb
@@ -1,16 +1,28 @@
 # frozen_string_literal: true
 require "rails_helper"
+require "shrine/storage/s3"
 
 RSpec.describe MosaicGenerator do
   describe "#generate" do
-    it "returns a path" do
-      raster_set = FactoryBot.create_for_repository(:raster_set)
-      generator = described_class.new(resource: raster_set)
-      base_path = Valkyrie::StorageAdapter.find(:cloud_geo_derivatives).storage_adapter.base_path
+    context "when using the disk storage adapter" do
+      it "returns a local path" do
+        raster_set = FactoryBot.create_for_repository(:raster_set, id: "331d70a5-4bd9-4a65-80e4-763c8f6b34fd")
+        generator = described_class.new(resource: raster_set)
+        expect(generator.generate).to end_with("tmp/cloud_geo_derivatives/33/1d/70/331d70a54bd94a6580e4763c8f6b34fd/mosaic.json")
+      end
+    end
 
-      path = Valkyrie::Storage::Disk::BucketedStorage.new(base_path: base_path).generate(resource: raster_set, original_filename: "mosaic.json", file: nil).to_s
-
-      expect(generator.generate).to eq path
+    context "when using the shrine storage adapter" do
+      it "returns an s3 path" do
+        bucket = instance_double(Aws::S3::Bucket, name: "figgy-bucket")
+        shrine = instance_double(Shrine::Storage::S3, bucket: bucket)
+        shrine_adapter = instance_double(Valkyrie::Storage::Shrine, shrine: shrine)
+        allow(shrine_adapter).to receive(:is_a?).and_return(true)
+        allow(Valkyrie::StorageAdapter).to receive(:find).and_return(shrine_adapter)
+        raster_set = FactoryBot.create_for_repository(:raster_set, id: "331d70a5-4bd9-4a65-80e4-763c8f6b34fd")
+        generator = described_class.new(resource: raster_set)
+        expect(generator.generate).to eq("s3://figgy-bucket/33/1d/70/331d70a54bd94a6580e4763c8f6b34fd/mosaic.json")
+      end
     end
   end
 end
