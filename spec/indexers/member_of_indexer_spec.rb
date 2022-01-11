e4ae72ebd7102fbc5af98a234ce83fb6017df2ff
Anna Headley <anna.headley@gmail.com>
2022-01-11 16:11:25 -0500

    Exclude raster set members from search results
    closes #4876

--- spec/indexers/member_of_indexer_spec.rb
+++ spec/indexers/member_of_indexer_spec.rb
@@ -3,12 +3,24 @@ require "rails_helper"
 
 RSpec.describe MemberOfIndexer do
   describe ".to_solr" do
-    it "indexes parent resource ids" do
-      child = FactoryBot.create_for_repository(:scanned_resource)
-      parent = FactoryBot.create_for_repository(:scanned_resource, member_ids: child.id)
-      output = described_class.new(resource: child).to_solr
+    context "for scanned resources" do
+      it "indexes parent resource ids" do
+        child = FactoryBot.create_for_repository(:scanned_resource)
+        parent = FactoryBot.create_for_repository(:scanned_resource, member_ids: child.id)
+        output = described_class.new(resource: child).to_solr
 
-      expect(output["member_of_ssim"]).to eq ["id-#{parent.id}"]
+        expect(output["member_of_ssim"]).to eq ["id-#{parent.id}"]
+      end
+    end
+
+    context "for raster resources" do
+      it "indexes parent resource ids" do
+        child = FactoryBot.create_for_repository(:raster_resource)
+        parent = FactoryBot.create_for_repository(:raster_resource, member_ids: child.id)
+        output = described_class.new(resource: child).to_solr
+
+        expect(output["member_of_ssim"]).to eq ["id-#{parent.id}"]
+      end
     end
   end
 end
