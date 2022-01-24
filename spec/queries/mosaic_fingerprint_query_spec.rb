63e738734d390b48740e4ccfb6050ea2aebbc38d
Trey Pendragon <tpendragon@princeton.edu>
2022-01-24 10:29:43 -0800

    Update mosaic fingerprint query to work arbitrarily deep.
    Closes #4889

--- spec/queries/mosaic_fingerprint_query_spec.rb
+++ spec/queries/mosaic_fingerprint_query_spec.rb
@@ -10,7 +10,43 @@ RSpec.describe MosaicFingerprintQuery do
     FactoryBot.create_for_repository(:raster_resource, member_ids: [child.id])
   end
 
+  def create_raster_scanned_map
+    raster_file_set1 = FactoryBot.create_for_repository(:geo_raster_cloud_file)
+    scanned_map_file_set1 = FactoryBot.create_for_repository(:geo_image_file_set)
+    raster1 = FactoryBot.create_for_repository(:raster_resource, member_ids: [raster_file_set1.id])
+    FactoryBot.create_for_repository(:scanned_map, member_ids: [raster1.id, scanned_map_file_set1.id])
+  end
+
   let(:query_service) { ChangeSetPersister.default.query_service }
+
+  context "when given a Scanned Map with Rasters" do
+    it "returns a uniquely identifiable fingerprint" do
+      scanned_map = create_raster_scanned_map
+      map_set = FactoryBot.create_for_repository(:scanned_map, member_ids: [scanned_map.id])
+
+      expect(query_service.custom_queries.mosaic_fingerprint_for(id: map_set.id)).to eq query_service.custom_queries.mosaic_fingerprint_for(id: map_set.id)
+    end
+    it "has the same fingerprint after deleting the scanned map file set" do
+      scanned_map = create_raster_scanned_map
+      map_set = FactoryBot.create_for_repository(:scanned_map, member_ids: [scanned_map.id])
+
+      start_fingerprint = query_service.custom_queries.mosaic_fingerprint_for(id: map_set.id)
+      ChangeSetPersister.default.persister.delete(resource: Wayfinder.for(scanned_map).file_sets.first)
+
+      expect(start_fingerprint).to eq query_service.custom_queries.mosaic_fingerprint_for(id: map_set.id)
+    end
+    it "has a different fingerprint after adding a new raster" do
+      scanned_map1 = create_raster_scanned_map
+      map_set = FactoryBot.create_for_repository(:scanned_map, member_ids: [scanned_map1.id])
+
+      start_fingerprint = query_service.custom_queries.mosaic_fingerprint_for(id: map_set.id)
+      scanned_map2 = create_raster_scanned_map
+      map_set.member_ids += [scanned_map2.id]
+      ChangeSetPersister.default.persister.save(resource: map_set)
+
+      expect(start_fingerprint).not_to eq query_service.custom_queries.mosaic_fingerprint_for(id: map_set.id)
+    end
+  end
 
   context "when given a RasterSet" do
     it "returns a uniquely identiable fingerprint" do
