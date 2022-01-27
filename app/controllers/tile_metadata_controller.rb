fc1bb0353aa8c23914a5d3465f59d5240360ed10
Eliot Jordan <eliotj@princeton.edu>
2022-01-27 13:09:38 -0600

    Generate tile metadata for MapSets with raster children

--- app/controllers/tile_metadata_controller.rb
+++ app/controllers/tile_metadata_controller.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 class TileMetadataController < ApplicationController
   attr_reader :resource
+  rescue_from MosaicService::Error, with: :not_found
 
   def metadata
     @resource = find_resource(params[:id])
-    if resource.is_a?(RasterResource) && resource.decorate.raster_set?
+    if resource.is_a?(RasterResource) || resource.is_a?(ScannedMap)
       respond_to do |f|
         f.json do
           render json: { uri: cached_mosaic_path }
         end
       end
     else
-      respond_to do |f|
-        f.json { head :not_found }
-      end
+      not_found
     end
   end
 
@@ -26,6 +25,12 @@ class TileMetadataController < ApplicationController
       # generating the document at the same time.
       Rails.cache.fetch("mosaic-manifest-#{resource.id}", expires_in: 600, race_condition_ttl: 60) do
         MosaicService.new(resource: resource).path
+      end
+    end
+
+    def not_found
+      respond_to do |format|
+        format.json { head :not_found }
       end
     end
 end
