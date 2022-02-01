52424b7753ac764827f945d4ef8d25d77fe093af
Eliot Jordan <eliotj@princeton.edu>
2022-02-01 12:34:24 -0600

    Modify TilePath to use cog endpoint for non-mosiac rasters

---
+++ app/values/tile_path.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class TilePath
+  attr_reader :resource
+  def initialize(resource)
+    @resource = resource
+  end
+
+  def tilejson
+    return unless valid?
+    "#{tileserver}/#{endpoint}/tilejson.json?id=#{id}"
+  end
+
+  private
+
+    def endpoint
+      if mosaic?
+        "mosaicjson"
+      else
+        "cog"
+      end
+    end
+
+    def id
+      resource.id.to_s.delete("-")
+    end
+
+    def tileserver
+      Figgy.config["tileserver"][:url]
+    end
+
+    def valid?
+      file_count = resource.decorate.try(:mosaic_file_count)
+      return false unless file_count&.positive?
+      true
+    end
+
+    def mosaic?
+      return true unless resource.is_a?(RasterResource)
+      return true if resource.decorate.decorated_raster_resources.count.positive?
+      false
+    end
+end
---
+++ spec/values/tile_path_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+describe TilePath do
+  describe "tilejson" do
+    context "with a MapSet that has Raster grandchildren" do
+      it "returns a mosaic tilejson path" do
+        scanned_map = FactoryBot.create_for_repository(:scanned_map_with_raster_children)
+        map_set = FactoryBot.create_for_repository(:scanned_map, member_ids: [scanned_map.id], id: "331d70a5-4bd9-4a65-80e4-763c8f6b34fd")
+        expect(described_class.new(map_set).tilejson).to eq "https://map-tiles-test.example.com/mosaicjson/tilejson.json?id=331d70a54bd94a6580e4763c8f6b34fd"
+      end
+    end
+
+    context "with a RasterResouce with a GeoTiff FileSet" do
+      it "returns a cog tilejson path" do
+        file_set = FactoryBot.create_for_repository(:geo_raster_cloud_file)
+        raster = FactoryBot.create_for_repository(:raster_resource, member_ids: [file_set.id], id: "331d70a5-4bd9-4a65-80e4-763c8f6b34fd")
+        expect(described_class.new(raster).tilejson).to eq "https://map-tiles-test.example.com/cog/tilejson.json?id=331d70a54bd94a6580e4763c8f6b34fd"
+      end
+    end
+  end
+end
