cbd6339a2b513f788a5a522dee25399b72837db3
Eliot Jordan <eliotj@princeton.edu>
2022-03-31 11:58:48 -0500

    Remove raster_set check from MosaicJob

--- spec/jobs/mosaic_job_spec.rb
+++ spec/jobs/mosaic_job_spec.rb
@@ -11,19 +11,11 @@ describe MosaicJob do
     allow(TileMetadataService).to receive(:new).and_return(mosaic_service)
   end
 
-  context "with a raster set resource" do
+  describe "#perform" do
     it "runs the TileMetadataService" do
       raster_set = FactoryBot.create_for_repository(:raster_set_with_files)
       described_class.perform_now(raster_set.id)
       expect(mosaic_service).to have_received(:path)
-    end
-  end
-
-  context "with a non-set raster resource" do
-    it "returns without calling the TileMetadataService" do
-      raster_resource = FactoryBot.create_for_repository(:raster_resource)
-      described_class.perform_now(raster_resource.id)
-      expect(mosaic_service).not_to have_received(:path)
     end
   end
 end
