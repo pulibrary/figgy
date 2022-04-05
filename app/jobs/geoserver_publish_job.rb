5b5de47484c4e250595128bf877043c2f4b2f0c0
Eliot Jordan <eliotj@princeton.edu>
2022-04-05 16:37:52 -0500

    Try some stuff

--- app/jobs/geoserver_publish_job.rb
+++ app/jobs/geoserver_publish_job.rb
@@ -9,22 +9,28 @@ class GeoserverPublishJob < ApplicationJob
 
     case event
     when "CREATE"
-      Geoserver::Publish.send(create_method, create_params)
+      create
     when "DELETE"
-      # Attempt to delete from both public and restricted
+      # Attempt to delete from both public and restricte
       # workspaces to make sure all traces of the file
       # are cleaned up on GeoServer.
       params["workspace"] = public_workspace
-      Geoserver::Publish.send(delete_method, delete_params)
+      delete
       params["workspace"] = authenticated_workspace
-      Geoserver::Publish.send(delete_method, delete_params)
+      delete
     when "UPDATE"
-      Geoserver::Publish.send(delete_method, delete_params)
-      Geoserver::Publish.send(create_method, create_params)
+      delete
+      create
     end
   end
 
   private
+
+    def create
+      Geoserver::Publish.send(create_method, create_params)
+    rescue => e
+      logger.info("Geoserver publish error: #{e.message}")
+    end
 
     def create_params
       {
@@ -38,6 +44,12 @@ class GeoserverPublishJob < ApplicationJob
     def create_method
       return :geotiff if layer_type == "geotiff"
       return :shapefile if layer_type == "shapefile"
+    end
+
+    def delete
+      Geoserver::Publish.send(delete_method, delete_params)
+    rescue => e
+      logger.info("Geoserver publish error: #{e.message}")
     end
 
     def delete_method
