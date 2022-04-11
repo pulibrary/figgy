85708e2fdc07f27ed843c8272eaafb2ae874f16f
Thanya Begum <tbegum@princeton.edu>
2022-04-11 12:23:51 -0400

    Migrate rasters with only one fileset

--- app/services/migrations/add_service_target_migrator.rb
+++ app/services/migrations/add_service_target_migrator.rb
@@ -10,12 +10,12 @@ module Migrations
 
     def run
       query_service.find_all_of_model(model: RasterResource).each do |raster_resource|
-        Wayfinder.for(raster_resource).file_sets.each do |file_set|
-          change_set = ChangeSet.for(file_set)
-          change_set.validate(service_targets: ["tiles"])
-          change_set_persister.save(change_set: change_set)
-          CreateDerivativesJob.perform_later(file_set.id.to_s)
-        end
+        file_sets = Wayfinder.for(raster_resource).file_sets
+        next unless file_sets.length == 1
+        change_set = ChangeSet.for(file_sets[0])
+        change_set.validate(service_targets: ["tiles"])
+        change_set_persister.save(change_set: change_set)
+        CreateDerivativesJob.perform_later(file_sets[0].id.to_s)
       end
     end
 
