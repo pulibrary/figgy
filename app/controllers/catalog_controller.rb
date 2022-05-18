5b8c0aeda5e6694b9f919fdd2451323225efb104
Trey Pendragon <tpendragon@princeton.edu>
2022-05-18 09:36:29 -0700

    Add bulk edit button back.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -117,6 +117,7 @@ class CatalogController < ApplicationController
     config.add_results_collection_tool(:sort_widget)
     config.add_results_collection_tool(:per_page_widget)
     config.add_results_collection_tool(:view_type_group)
+    config.add_results_collection_tool(:bulk_edit_button)
     config.add_results_document_tool(:bookmark, partial: "bookmark_control", if: :render_bookmarks_control?)
     config.add_nav_action(:bookmark, partial: "blacklight/nav/bookmark", if: :render_bookmarks_control?)
 
