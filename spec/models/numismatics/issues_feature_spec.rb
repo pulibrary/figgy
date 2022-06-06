82f746da2f86cd99574019c571dad238010a600f
Anna Headley <anna.headley@gmail.com>
2022-06-06 10:12:56 -0400

    Use less css hierarchy in test

--- spec/models/numismatics/issues_feature_spec.rb
+++ spec/models/numismatics/issues_feature_spec.rb
@@ -78,10 +78,10 @@ RSpec.feature "Numismatics::Issues" do
     expect(page).to have_css "a.btn.btn-sm.btn-primary.new-link", text: "New Master"
     expect(page).to have_css "a.btn.btn-sm.btn-primary.new-link", text: "New Reference"
     expect(page).to have_css "a.btn.btn-sm.btn-primary.new-link", text: "New Ruler"
-    expect(page).to have_css "div.panel.panel-default div.panel-body div.col-sm-6 div.form-group div.col-sm-6 div.form-group input#numismatics_issue_earliest_date"
-    expect(page).to have_css "div.panel.panel-default div.panel-body div.col-sm-6 div.form-group div.col-sm-6 div.form-group input#numismatics_issue_latest_date"
-    expect(page).to have_css "div.panel.panel-default div.panel-body div.col-sm-6 div.form-group div.col-sm-6 div.form-group input#numismatics_issue_era"
-    expect(page).to have_css "div.panel.panel-default div.panel-body div.col-sm-6 div.form-group div.col-sm-6 div.form-group input#numismatics_issue_object_date"
+    expect(page).to have_css "div.form-group input#numismatics_issue_earliest_date"
+    expect(page).to have_css "div.form-group input#numismatics_issue_latest_date"
+    expect(page).to have_css "div.form-group input#numismatics_issue_era"
+    expect(page).to have_css "div.form-group input#numismatics_issue_object_date"
   end
 
   context "when another issue already exists", js: true do
