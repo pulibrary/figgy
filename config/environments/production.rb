505686b4f73668cf5cdda1d408abb07cf69e7545
Trey Pendragon <tpendragon@princeton.edu>
2022-06-13 12:12:48 -0700

    Remove Uglifier, allow deploys.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = true
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = false
   config.log_level = :info
   config.i18n.fallbacks = true
