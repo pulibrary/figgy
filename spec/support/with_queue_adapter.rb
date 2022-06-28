c8a9f73c5e045f24cdd4ac29f04b377a8091c0f6
Eliot Jordan <eliotj@princeton.edu>
2022-06-28 11:20:34 -0500

    Work around Rails 6 issue with setting ActiveJob queue_adapter

--- spec/support/with_queue_adapter.rb
+++ spec/support/with_queue_adapter.rb
@@ -2,6 +2,9 @@ module WithQueueAdapter
   def with_queue_adapter(new_adapter)
     around do |example|
       begin
+        # Rails 6 overrides ActiveJob queue_adapter. Need to disable the test adapter
+        # See: https://github.com/rspec/rspec-rails/issues/2311
+        (ActiveJob::Base.descendants << ActiveJob::Base).each(&:disable_test_adapter)
         old_adapter = ActiveJob::Base.queue_adapter
         ActiveJob::Base.queue_adapter = new_adapter
         example.run
