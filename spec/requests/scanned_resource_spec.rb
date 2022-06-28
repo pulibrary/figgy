0516c4247ec4cb8bf48461b2b2db8a072196f5bb
Eliot Jordan <eliotj@princeton.edu>
2022-06-28 11:20:48 -0500

    Fix PDF generation

--- spec/requests/scanned_resource_spec.rb
+++ spec/requests/scanned_resource_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "ScannedResource requests", type: :request do
     expect(response.body).not_to be_empty
     expect(response.content_length).to be > 0
     expect(response.content_type).to eq "application/pdf"
-    expect(response.headers["Content-Disposition"]).to eq 'inline; filename="derivative_pdf.pdf"'
+    expect(response.headers["Content-Disposition"]).to eq "inline; filename=\"derivative_pdf.pdf\"; filename*=UTF-8''derivative_pdf.pdf"
   end
 
   context "when the PDF has already been generated once" do
