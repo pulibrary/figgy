e9bc7bce21ee926fe091c5428f08a05a858b94b7
Eliot Jordan <eliotj@princeton.edu>
2022-07-08 15:54:15 -0500

    Update non-problematic gems

--- spec/support/capybara_selenium.rb
+++ spec/support/capybara_selenium.rb
@@ -6,20 +6,20 @@ require "selenium-webdriver"
 #   the driver ":selenium" so we changed it from :headless_chrome"
 Capybara.register_driver(:selenium) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu disable-setuid-sandbox window-size=7680,4320] }
+    { "goog:chromeOptions": %w[headless disable-gpu disable-setuid-sandbox window-size=7680,4320] }
   )
+
   browser_options = ::Selenium::WebDriver::Chrome::Options.new
-  browser_options.args << "--headless"
-  browser_options.args << "--disable-gpu"
+  browser_options.add_argument("--headless")
+  browser_options.add_argument("--disable-gpu")
 
   http_client = Selenium::WebDriver::Remote::Http::Default.new
   http_client.read_timeout = 120
   http_client.open_timeout = 120
   Capybara::Selenium::Driver.new(app,
                                  browser: :chrome,
-                                 desired_capabilities: capabilities,
-                                 http_client: http_client,
-                                 options: browser_options)
+                                 capabilities: [capabilities, browser_options],
+                                 http_client: http_client)
 end
 
 Capybara.javascript_driver = :selenium
