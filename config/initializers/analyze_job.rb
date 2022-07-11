3d4392a0591e38de8b35734c64132ab3b3b8540f
Anna Headley <anna.headley@gmail.com>
2022-07-11 15:08:23 -0400

    Make tests run

--- config/initializers/analyze_job.rb
+++ config/initializers/analyze_job.rb
@@ -3,6 +3,8 @@
 # Monkey patch AnalyzeJob so that an ActiveJob::DeserializationError is rescued and
 # does not cause the job to be moved to the retry queue. This is used mainly for
 # OcrRequest blobs that are purged before AnalyzeJob is run.
-ActiveStorage::AnalyzeJob.class_eval do
-  discard_on ActiveJob::DeserializationError
+Rails.application.config.to_prepare do
+  ActiveStorage::AnalyzeJob.class_eval do
+    discard_on ActiveJob::DeserializationError
+  end
 end
