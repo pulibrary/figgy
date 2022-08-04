da7873babbcf2f7a7d0f73ba2c069c0014d84647
Trey Pendragon <tpendragon@princeton.edu>
2022-08-04 15:24:21 -0700

    Increase timeout for test solr.

--- spec/support/clean_solr.rb
+++ spec/support/clean_solr.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
-SOLR_TEST_URL = "http://#{ENV['lando_figgy_test_solr_conn_host'] || "127.0.0.1"}:#{ENV['TEST_JETTY_PORT'] || ENV['lando_figgy_test_solr_conn_port'] || 8984}/solr/figgy-core-test"
 RSpec.configure do |config|
   config.before(:each) do
-    client = RSolr.connect(url: SOLR_TEST_URL)
-    client.delete_by_query("*:*", params: { softCommit: true })
+    Blacklight.default_index.connection.delete_by_query("*:*", params: { softCommit: true })
   end
 end
