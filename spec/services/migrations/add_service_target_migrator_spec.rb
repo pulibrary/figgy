926b142a21c34a7f7d367883c38a22415fdd5249
Eliot Jordan <eliotj@princeton.edu>
2022-08-04 13:36:07 -0500

    Update service target migrator to handle multiple file sets

--- spec/services/migrations/add_service_target_migrator_spec.rb
+++ spec/services/migrations/add_service_target_migrator_spec.rb
@@ -4,17 +4,31 @@ require "rails_helper"
 
 RSpec.describe Migrations::AddServiceTargetMigrator do
   describe ".call" do
-    context "when given raster resources without a service target" do
+    context "when given raster resources without a service target with a single file set" do
       it "adds the service target tiles" do
-        file_set = FactoryBot.create_for_repository(:file_set)
-        FactoryBot.create_for_repository(:raster_resource, member_ids: file_set.id)
+        raster_fileset = FactoryBot.create_for_repository(:geo_raster_file_set)
+        FactoryBot.create_for_repository(:raster_resource, member_ids: raster_fileset.id)
 
         described_class.call
 
         query_service = Valkyrie.config.metadata_adapter.query_service
         raster_resource = query_service.find_all_of_model(model: RasterResource).first
         expect(Wayfinder.for(raster_resource).file_sets.first.service_targets).to eq ["tiles"]
-        expect(CreateDerivativesJob).to have_been_enqueued.once.with(file_set.id.to_s)
+        expect(CreateDerivativesJob).to have_been_enqueued.once.with(raster_fileset.id.to_s)
+      end
+    end
+
+    context "when given a raster resource without a service target and an fgdc file" do
+      it "adds the service target tiles" do
+        raster_fileset = FactoryBot.create_for_repository(:geo_raster_file_set)
+        fgdc_fileset = FactoryBot.create_for_repository(:geo_metadata_file_set)
+        FactoryBot.create_for_repository(:raster_resource, member_ids: [raster_fileset.id, fgdc_fileset.id])
+
+        described_class.call
+        query_service = Valkyrie.config.metadata_adapter.query_service
+        raster_resource = query_service.find_all_of_model(model: RasterResource).first
+        expect(Wayfinder.for(raster_resource).file_sets.first.service_targets).to eq ["tiles"]
+        expect(CreateDerivativesJob).to have_been_enqueued.once.with(raster_fileset.id.to_s)
       end
     end
   end
