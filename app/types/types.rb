c055226b305cf7bcb64c7f7df5e1a554b79bed42
Anna Headley <anna.headley@gmail.com>
2022-08-24 12:33:08 -0400

    Add custom DateEastern type
    Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>

--- app/types/types.rb
+++ app/types/types.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 module Types
   include Dry.Types(default: :nominal)
+
+  class CoercionError < StandardError; end
+
+  # TODO: should we remove this in favor of
+  # https://github.com/samvera/valkyrie/blob/60412eb1e93debd2c99745a011227c22f0da7157/lib/valkyrie/types.rb#L43
   URI = Dry::Types::Nominal
         .new(RDF::URI)
         .constructor do |input|
@@ -8,6 +13,16 @@ module Types
       RDF::URI.new(input.to_s)
     else
       input
+    end
+  end
+
+  DateEastern = Dry::Types::Nominal
+                .new(ActiveSupport::TimeWithZone)
+                .constructor do |input|
+    m, d, y = input.split("/")
+    raise(::Types::CoercionError, "expected format is M/D/YYYY") unless y.length == 4
+    ::Time.use_zone("Eastern Time (US & Canada)") do
+      ::Time.zone.parse("#{y}-#{m}-#{d}").midnight
     end
   end
 end
