5f0ac56f65539d7302d4b992d0922777839641bc
Trey Pendragon <tpendragon@princeton.edu>
2022-09-01 12:07:39 -0700

    Change ArchivalMediaBagJob for new workflow
    Now that we're using bulk ingest we can pass in the collections to be a
    member of, as well as the visibility. We no longer need to pull that
    info from the parent collection.

--- app/change_sets/recording_change_set.rb
+++ app/change_sets/recording_change_set.rb
@@ -21,6 +21,7 @@ class RecordingChangeSet < ChangeSet
   property :upload_set_id, multiple: false, require: false, type: Valkyrie::Types::ID
   property :embargo_date, multiple: false, required: false, type: Valkyrie::Types::String.optional
   property :notice_type, multiple: false, required: false
+  property :depositor, multiple: false, require: false
 
   # Virtual Attributes
   property :files, virtual: true, multiple: true, required: false
