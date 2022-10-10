6758c72dbecb043935be0c0691b86db6794e9a4c
Anna Headley <anna.headley@gmail.com>
2022-10-10 09:38:50 -0400

    Add notice type feature test

--- spec/features/scanned_resource_spec.rb
+++ spec/features/scanned_resource_spec.rb
@@ -15,7 +15,15 @@ RSpec.feature "Scanned Resource" do
 
     fill_in "Title", with: "Test Title"
     fill_in "Embargo Date", with: "1/14/2025"
+    # I'm not sure why we need visible: all but we seem to
+    notice_type_form_field = find_by_id("scanned_resource_notice_type", visible: "all")
+    notice_options = notice_type_form_field.find_all("option")
+    expect(notice_options.map(&:text)).to eq ["Harmful Content", "Explicit Content", "Senior Thesis"]
+#     within notice_type_form_field do
+#       select "Senior Thesis"
+#     end
     click_button "Save"
     expect(page).to have_content "Embargo Date"
+    # expect(page).to have_content "Senior Thesis"
   end
 end
