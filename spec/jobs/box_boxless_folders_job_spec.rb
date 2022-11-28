dcbbd59b5e23acd9536a47fc3648ae5abed70571
Anna Headley <anna.headley@gmail.com>
2022-11-28 15:21:06 -0500

    Add test to ensure other boxes aren't removed from projects

--- spec/jobs/box_boxless_folders_job_spec.rb
+++ spec/jobs/box_boxless_folders_job_spec.rb
@@ -10,9 +10,12 @@ RSpec.describe BoxBoxlessFoldersJob do
       # new box to hold the folders without boxes, one folder already added
       folder1 = FactoryBot.create_for_repository(:complete_ephemera_folder)
       box0 = FactoryBot.create_for_repository(:ephemera_box, member_ids: [folder1.id])
+      # pre-existing box with folder
+      folder4 = FactoryBot.create_for_repository(:complete_ephemera_folder)
+      box1 = FactoryBot.create_for_repository(:ephemera_box, member_ids: [folder4.id])
       project = FactoryBot.create_for_repository(
         :ephemera_project,
-        member_ids: [folder2.id, folder3.id, box0.id]
+        member_ids: [folder2.id, folder3.id, box0.id, box1.id]
       )
       expect(Wayfinder.for(project).ephemera_folders.count).to eq 2
       expect(Wayfinder.for(box0).ephemera_folders.count).to eq 1
@@ -23,7 +26,9 @@ RSpec.describe BoxBoxlessFoldersJob do
       box0 = query_service.find_by(id: box0.id)
 
       expect(Wayfinder.for(box0).ephemera_folders.count).to eq 3
+      expect(Wayfinder.for(box1).ephemera_folders.count).to eq 1
       expect(Wayfinder.for(project).ephemera_folders.count).to eq 0
+      expect(Wayfinder.for(project).ephemera_boxes.count).to eq 2
     end
   end
 
