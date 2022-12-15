47d16b914a1bae57385b0b3d41d896b8ba18914d
Anna Headley <anna.headley@gmail.com>
2022-12-15 12:58:14 -0500

    Mark the previous cloud_fixity Event current:false
    When a new one is created

--- app/jobs/update_fixity_job.rb
+++ app/jobs/update_fixity_job.rb
@@ -1,23 +1,52 @@
 # frozen_string_literal: true
 class UpdateFixityJob < ApplicationJob
   queue_as :super_low
+  delegate :query_service, to: :change_set_persister
 
+  attr_reader :resource_id, :child_property, :child_id
   # rubocop:disable Style/GuardClause
   def perform(status:, resource_id:, child_property:, child_id:)
+    @resource_id = resource_id
+    @child_property = child_property
+    @child_id = child_id
     event_change_set = EventChangeSet.new(Event.new)
-    event_change_set.validate(type: :cloud_fixity, status: status, resource_id: resource_id, child_property: child_property.to_sym, child_id: child_id)
+    event_change_set.validate(type: :cloud_fixity, status: status, resource_id: resource_id, child_property: child_property.to_sym, child_id: child_id, current: true)
     raise "Unable to update fixity. Invalid event: #{event_change_set.errors.full_messages.to_sentence}" unless event_change_set.valid?
-    change_set_persister.save(change_set: event_change_set)
+    change_set_persister.buffer_into_index do |buffered_change_set_persister|
+      buffered_change_set_persister.save(change_set: previous_event_change_set) if previous_event
+      buffered_change_set_persister.save(change_set: event_change_set)
+    end
     if status == "FAILURE"
       Honeybadger.notify("Cloud fixity failure on object with resource id: #{resource_id}, child property: #{child_property}, child id: #{child_id}")
     end
   end
   # rubocop:enable Style/GuardClause
+  # rubocop:enable Metrics/MethodLength
 
-  def change_set_persister
-    ::ChangeSetPersister.new(
-      metadata_adapter: Valkyrie::MetadataAdapter.find(:postgres),
-      storage_adapter: Valkyrie.config.storage_adapter
-    )
-  end
+  private
+
+    def previous_event_change_set
+      return unless previous_event
+      ChangeSet.for(previous_event).tap do |cs|
+        cs.validate(current: false)
+      end
+    end
+
+    def previous_event
+      @previous_event ||= query_service.custom_queries.find_by_property(
+        property: :metadata,
+        value: {
+          type: :cloud_fixity,
+          resource_id: Valkyrie::ID.new(resource_id),
+          child_property: child_property,
+          child_id: Valkyrie::ID.new(child_id),
+          current: true
+        },
+        model: Event
+      ).first
+    end
+
+    def change_set_persister
+      @change_set_persister ||= ChangeSetPersister.default
+    end
 end
--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -7,5 +7,9 @@ FactoryBot.define do
     to_create do |instance|
       Valkyrie.config.metadata_adapter.persister.save(resource: instance)
     end
+
+    factory :cloud_fixity_event do
+      type { :cloud_fixity }
+    end
   end
 end
