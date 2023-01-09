2ddc560359e039f27d796a56458bf93ef6acd2a4
Eliot Jordan <eliotj@princeton.edu>
2023-01-09 13:30:47 -0600

    Add deletion marker service job

---
+++ app/jobs/restore_deletion_marker_job.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+class RestoreDeletionMarkerJob < ApplicationJob
+  queue_as :default
+  delegate :query_service, to: :metadata_adapter
+
+  def perform(deletion_marker_id)
+    DeletionMarkerService.restore(deletion_marker_id)
+  end
+end
---
+++ spec/jobs/restore_deletion_marker_job_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+describe RestoreDeletionMarkerJob do
+  describe "#perform" do
+    before do
+      allow(DeletionMarkerService).to receive(:restore)
+    end
+
+    it "runs the DeletionMarkerService" do
+      id = SecureRandom.uuid
+      described_class.perform_now(id)
+      expect(DeletionMarkerService).to have_received(:restore).with(id)
+    end
+  end
+end
