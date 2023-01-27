eee99432a655eefeef41a6c2f11c1567820469b4
Trey Pendragon <tpendragon@princeton.edu>
2023-01-26 17:16:31 -0800

    Fix when the parent's created after the child.

--- app/jobs/preserve_children_job.rb
+++ app/jobs/preserve_children_job.rb
@@ -3,10 +3,14 @@ class PreserveChildrenJob < ApplicationJob
   delegate :metadata_adapter, to: :change_set_persister
   delegate :query_service, to: :metadata_adapter
 
-  def perform(id:)
+  def perform(id:, unpreserved_only: true)
     resource = query_service.find_by(id: id)
-    unpreserved_ids = query_service.custom_queries.find_never_preserved_child_ids(resource: resource)
-    unpreserved_ids.each do |member_id|
+    ids = if unpreserved_only
+            query_service.custom_queries.find_never_preserved_child_ids(resource: resource)
+          else
+            resource.member_ids || []
+          end
+    ids.each do |member_id|
       member = query_service.find_by(id: member_id)
       lock_tokens = member[Valkyrie::Persistence::Attributes::OPTIMISTIC_LOCK] || []
       lock_tokens = lock_tokens.map(&:serialize)
