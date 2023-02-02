83b67c4d23800a27bb4ed11e8238abe05d1fb38e
Eliot Jordan <eliotj@princeton.edu>
2023-02-02 14:41:51 -0600

    Turn off Blacklight search state filtering

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -84,6 +84,9 @@ class CatalogController < ApplicationController
     # Interferes with thumbnail display in FileSet manager
     config.track_search_session = false
 
+    # Allow all params
+    config.filter_search_state_fields = false
+
     # Add extra permitted params
     # https://github.com/projectblacklight/blacklight/blob/v7.28.0/lib/blacklight/parameters.rb#L84-L89
     # pul_controller_params aren't used by the search state. We're adding them
