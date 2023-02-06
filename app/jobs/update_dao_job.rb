8fbaf5cc3383d12fd5125a23eb0a8b7b2846fefb
Taylor Yamashita <taylory@princeton.edu>
2023-02-06 14:33:49 -0500

    Rubocop fix

--- app/jobs/update_dao_job.rb
+++ app/jobs/update_dao_job.rb
@@ -4,7 +4,7 @@ class UpdateDaoJob < ApplicationJob
   def perform(id)
     change_set = ChangeSet.for(change_set_persister.metadata_adapter.query_service.find_by(id: id))
     DaoUpdater.new(change_set: change_set, change_set_persister: change_set_persister).update!
-  
+
   rescue Valkyrie::Persistence::ObjectNotFoundError
     false
   end
