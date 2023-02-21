e0b548599ceda21d2a032fcdc0405e598fb5eb59
Trey Pendragon <tpendragon@princeton.edu>
2023-02-21 11:50:58 -0800

    Check field that has a comma in it.
    Makes sure that it escapes the commas for the CSV.

--- spec/values/csv_report_spec.rb
+++ spec/values/csv_report_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe CSVReport do
       resources = [FactoryBot.create_for_repository(:complete_scanned_resource, source_metadata_identifier: "123456", import_metadata: true)]
 
       # Lazy isn't necessary, but should be able to work.
-      report = described_class.new(resources.lazy, fields: [:title, :source_metadata_identifier, :identifier, :visibility, :state, :call_number])
+      report = described_class.new(resources.lazy, fields: [:title, :source_metadata_identifier, :identifier, :visibility, :state, :call_number, :extent])
 
       expect(report.csv_rows.first.to_h[:identifier]).to eq resources[0].identifier[0]
       expect(report.csv_rows.first.to_h[:title]).to eq "Earth rites : fertility rites in pre-industrial Britain / Janet and Colin Bord."
       expect(report.csv_rows.first.to_h[:call_number]).to eq "BL980.G7 B66 1982"
-      expect(report.headers).to eq ["Title", "Source metadata identifier", "Identifier", "Visibility", "State", "Call number"]
+      expect(report.headers).to eq ["Title", "Source metadata identifier", "Identifier", "Visibility", "State", "Call number", "Extent"]
 
       report.write(path: Rails.root.join("tmp", "csv_test.csv"))
       read = CSV.read(Rails.root.join("tmp", "csv_test.csv"), headers: true, header_converters: :symbol)
@@ -27,7 +27,8 @@ RSpec.describe CSVReport do
           identifier: "ark:/99999/fk4",
           visibility: "open",
           state: "complete",
-          call_number: "BL980.G7 B66 1982"
+          call_number: "BL980.G7 B66 1982",
+          extent: "xiv, 273 p. : ill. ; 24 cm."
         }
       )
     end
