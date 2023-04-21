b1351b79df142ae89fb34e2583aabf8f9b52e863
Eliot Jordan <eliotj@princeton.edu>
2023-04-21 12:26:51 -0500

    Update redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,14 +5,13 @@ require "redis"
 # the same initializer as Sidekiq. See:
 # https://github.com/redis/redis-rb/commit/9745e22db65ac294be51ed393b584c0f8b72ae98
 redis_config = YAML.safe_load(ERB.new(IO.read(Rails.root.join("config", "redis.yml"))).result, [], [], true)[Rails.env].with_indifferent_access
-redis_client = Redis.new(redis_config.merge(thread_safe: true))._client
-
+redis_url = "redis://#{redis_config['host']}:#{redis_config['port']}/0"
 Sidekiq::Client.reliable_push! unless Rails.env.test?
 Sidekiq.configure_server do |config|
-  config.redis = redis_client.options
+  config.redis = { url:  redis_url }
   config.super_fetch!
   config.reliable_scheduler!
 end
 Sidekiq.configure_client do |config|
-  config.redis = redis_client.options
+  config.redis = { url:  redis_url }
 end
