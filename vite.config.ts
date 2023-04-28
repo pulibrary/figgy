edeb03bc8506c7fe21959d02a6e98936a782ab2a
Anna Headley <anna.headley@gmail.com>
2023-04-28 15:38:12 -0400

    Add compression to vite deliverables

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue2'
+import { brotliCompressSync } from 'zlib'
+import gzipPlugin from 'rollup-plugin-gzip'
 
+// see compression documentation at https://vite-ruby.netlify.app/guide/deployment.html#compressing-assets-%F0%9F%93%A6
+// and https://github.com/ElMassimo/vite_ruby/discussions/101#discussioncomment-1019222
 export default defineConfig({
   base: './',
   resolve: {
@@ -12,11 +16,18 @@ export default defineConfig({
       '@images': resolve(__dirname, 'app/javascript/images'),
       '@components': resolve(__dirname, 'app/javascript/components'),
       '@helpers': resolve(__dirname, 'app/javascript/helpers'),
-      '@store': resolve(__dirname, 'app/javascript/store'),
-    },
+      '@store': resolve(__dirname, 'app/javascript/store')
+    }
   },
   plugins: [
     RubyPlugin(),
     vue(),
-  ],
+    // Create gzip copies of relevant assets
+    gzipPlugin(),
+    // Create brotli copies of relevant assets
+    gzipPlugin({
+      customCompression: (content) => brotliCompressSync(Buffer.from(content)),
+      fileName: '.br'
+    })
+  ]
 })
