dd280358dbebe4bf83b55a85d6916558029f7d84
Anna Headley <anna.headley@gmail.com>
2023-04-28 16:07:37 -0400

    Replace uvloaded event binding with poll on window.UV
    The event was firing before our js was loaded to watch it
    
    Co-authored-by: Trey Pendragon <tpendragon@users.noreply.github.com>
    Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com>

--- app/javascript/entrypoints/viewer.js
+++ app/javascript/entrypoints/viewer.js
@@ -1,4 +1,9 @@
 import UVManager from '@viewer/uv_manager'
 import 'leaflet/dist/leaflet.css'
 const UVManagerInstance = new UVManager()
-window.addEventListener('uvLoaded', UVManagerInstance.initialize.bind(UVManagerInstance), false)
+let timer = window.setInterval(() => {
+  if (window.UV !== undefined) {
+    UVManagerInstance.initialize()
+    window.clearInterval(timer)
+  }
+}, 5)
