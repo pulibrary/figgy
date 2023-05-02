a5acb0bc9c3930bb5e4e9264a178b811fdb8268f
Anna Headley <anna.headley@gmail.com>
2023-05-02 12:29:24 -0400

    Remove console logs from entrypoints/application.js

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -17,14 +17,6 @@ import FileUploader from '@components/file-uploader.vue'
 import Initializer from '@figgy/figgy_boot'
 import VueDetails from 'vue-details'
 
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
 Vue.use(system)
 Vue.component('v-details', VueDetails)
 
