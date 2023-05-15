86c54c3365a14ea964a155000400c234228c52ea
Anna Headley <anna.headley@gmail.com>
2023-05-15 10:55:09 -0400

    Update universalviewer from 3 to 4, use package via yarn
    closes #5778

--- public/uv/lib/uv-seadragon-extension-dependencies.js
+++
@@ -1,8 +0,0 @@
-define(function () {
-    return function (formats) {
-        return {
-            async: ['TreeComponent', 'GalleryComponent', 'MetadataComponent', 'openseadragon.min']
-            //async: ['TreeComponent', 'iiifgallery.proxy', 'GalleryComponent', 'MetadataComponent', 'openseadragon.min']
-        };
-    };
-});
