e45ef1a109ce6a686b78a70d33838e29ae85a8a1
Anna Headley <anna.headley@gmail.com>
2023-05-16 09:49:48 -0400

    Combine all metadata query filters into one
    closes #5788

--- app/queries/find_fixity_events.rb
+++ app/queries/find_fixity_events.rb
@@ -15,7 +15,6 @@ class FindFixityEvents
     <<-SQL
       SELECT * FROM orm_resources WHERE
       metadata @> ? AND
-      metadata @> '{"current": [true]}' AND
       internal_resource = 'Event'
       ORDER BY #{order_by_property} #{order_by}
       LIMIT ?
@@ -23,7 +22,7 @@ class FindFixityEvents
   end
 
   def find_fixity_events(sort: "ASC", limit: 50, order_by_property: "updated_at", status:, type:)
-    internal_array = { "status" => [status], "type" => [type] }
+    internal_array = { "status" => [status], "type" => [type], "current" => [true] }
     run_query(query(order_by_property: order_by_property, order_by: sort), internal_array.to_json, limit)
   end
 end
