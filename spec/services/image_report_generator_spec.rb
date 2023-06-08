281d70338cc9ce4ba50e1f75b3a11eb89aeab943
Trey Pendragon <tpendragon@princeton.edu>
2023-06-08 10:56:51 -0700

    Add resource out of report time frame.

--- spec/services/image_report_generator_spec.rb
+++ spec/services/image_report_generator_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe ImageReportGenerator do
   context "when given a collection with a variety of resources and visibilities" do
     it "outputs a CSV with a row per collection, and a column per visibility" do
       collection = FactoryBot.create_for_repository(:collection)
+      # Resource out of time frame - don't return
+      member = FactoryBot.create_for_repository(:complete_open_scanned_resource, member_ids: [FactoryBot.create_for_repository(:file_set).id], member_of_collection_ids: [collection.id])
       Timecop.travel(2021, 8, 30) do
         # 1 open resource
         member = FactoryBot.create_for_repository(:complete_open_scanned_resource, member_ids: [FactoryBot.create_for_repository(:file_set).id], member_of_collection_ids: [collection.id])
