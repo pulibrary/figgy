3303df3f3d8f1d89e92f45da00c6a259cb267f14
Shaun Ellis <shaun@sdellis.com>
2023-08-01 13:08:44 -0400

    fixes test for derivative files

--- spec/features/file_set_spec.rb
+++ spec/features/file_set_spec.rb
@@ -37,12 +37,21 @@ RSpec.feature "FileSet" do
     expect(page).to have_css ".preserved", text: "Last Success:"
   end
 
-  scenario "fileset fixity ui table informs the user of any files that are intentionally not preserved" do
-    file = fixture_file_upload("files/example.tif", "image/tiff")
-    resource = FactoryBot.create_for_repository(:scanned_resource, files: [file])
-    file_set = Wayfinder.for(resource).file_sets.first
+  context 'when there is a derivative file' do
+    with_queue_adapter :inline
+    scenario "the fileset fixity ui table informs the user of any files that are intentionally not preserved" do
+      file = fixture_file_upload("files/example.tif", "image/tiff")
+      resource = FactoryBot.create_for_repository(:complete_scanned_resource, files: [file])
+      file_set = Wayfinder.for(resource).file_sets.first
+      change_set_persister = ChangeSetPersister.default
+      query_service = change_set_persister.query_service
+      reloaded_resource = query_service.find_by(id: resource.id)
+      persisted_resource = change_set_persister.metadata_adapter.persister.save(resource: reloaded_resource)
+      change_set = ChangeSet.for(persisted_resource)
+      change_set_persister.save(change_set: change_set)
 
-    visit solr_document_path(id: file_set.id)
-    expect(page).to have_css ".not_preserved", text: "Derivative files are not preserved."
+      visit solr_document_path(id: file_set.id)
+      expect(page).to have_css ".not_preserved", text: "Derivative files are not preserved."
+    end
   end
 end
