ef39545141f61a9ddb13906923cb23b67f285532
Eliot Jordan <eliotj@princeton.edu>
2023-08-02 12:02:56 -0500

    Do not allow deletion of vocabularies with members

--- app/controllers/ephemera_vocabularies_controller.rb
+++ app/controllers/ephemera_vocabularies_controller.rb
@@ -14,4 +14,16 @@ class EphemeraVocabulariesController < ResourcesController
   def load_vocabularies
     @vocabularies = query_service.find_all_of_model(model: EphemeraVocabulary).map(&:decorate).sort_by(&:label)
   end
+
+  def around_delete_action
+    # Only allow deleting empty vocabularies. Avoids difficulty of restoring a
+    # full vocabulary from preservation and supporting restoring accidentally
+    # deleted vocabularies.
+    if Wayfinder.for(@change_set.resource).members_count.positive?
+      flash[:alert] = "Unable to delete a vocabulary with members."
+      redirect_to solr_document_path(@change_set.resource.id.to_s)
+    else
+      yield
+    end
+  end
 end
--- app/wayfinders/ephemera_vocabulary_wayfinder.rb
+++ app/wayfinders/ephemera_vocabulary_wayfinder.rb
@@ -3,4 +3,8 @@ class EphemeraVocabularyWayfinder < BaseWayfinder
   relationship_by_property :parent_vocabularies, property: :member_of_vocabulary_id, singular: true
   inverse_relationship_by_property :vocabularies, property: :member_of_vocabulary_id, singular: true, model: EphemeraVocabulary
   inverse_relationship_by_property :ephemera_terms, property: :member_of_vocabulary_id, singular: true, model: EphemeraTerm
+
+  def members_count
+    @members_count ||= query_service.custom_queries.count_inverse_relationship(resource: resource, property: :member_of_vocabulary_id)
+  end
 end
