dd740c78339d1d6ff8b80317cda9e915bcb7cd2b
Anna Headley <anna.headley@gmail.com>
2023-08-03 09:42:24 -0400

    Notify with the event id when it has no resource_id
    And don't show the error
    advances #5854

--- spec/views/fixity_dashboard/_fixity_table.html.erb_spec.rb
+++ spec/views/fixity_dashboard/_fixity_table.html.erb_spec.rb
@@ -9,12 +9,18 @@ RSpec.describe "fixity_dashboard/_fixity_table" do
     end
     let(:original_file) { instance_double FileMetadata }
 
-    before do
+    it "links to file set without parent" do
       render partial: "fixity_table", locals: { resources: [local_fixity_event.decorate] }
+      expect(rendered).to have_link "Page 1"
     end
 
-    it "links to file set without parent" do
-      expect(rendered).to have_link "Page 1"
+    context "when an event has no resource_id" do
+      it "notifies Honeybadger and doesn't show that one" do
+        event = FactoryBot.create_for_repository(:cloud_fixity_event, current: true)
+        allow(Honeybadger).to receive(:notify)
+        expect { render partial: "fixity_table", locals: { resources: [event.decorate] } }.not_to raise_error
+        expect(Honeybadger).to have_received(:notify).with("Event #{event.id} has no resource_id")
+      end
     end
   end
 end
