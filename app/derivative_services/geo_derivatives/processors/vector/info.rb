e8fb0dd6ae237b04e8f4425ffbf85ece43cef70f
Eliot Jordan <eliotj@princeton.edu>
2023-09-06 11:19:22 -0500

    Use orginfo json formatted output

--- app/derivative_services/geo_derivatives/processors/vector/info.rb
+++ app/derivative_services/geo_derivatives/processors/vector/info.rb
@@ -3,11 +3,21 @@ module GeoDerivatives
   module Processors
     module Vector
       class Info
-        attr_accessor :doc
+        attr_accessor :path
         attr_writer :name, :driver
 
         def initialize(path)
-          @doc = ogrinfo(path)
+          @path = path
+        end
+
+        # Parsed json out from ogrinfo
+        # @return [Hash]
+        def doc
+          @doc ||= begin
+                     JSON.parse(ogrinfo(path))
+                   rescue
+                     {}
+                   end
         end
 
         # Returns the vector dataset name
@@ -40,7 +50,7 @@ module GeoDerivatives
           # @param path [String] path to vector file or shapefile directory
           # @return [String] output of ogrinfo
           def ogrinfo(path)
-            stdout, _stderr, _status = Open3.capture3("ogrinfo", "-ro", "-so", "-al", path.to_s)
+            stdout, _stderr, _status = Open3.capture3("ogrinfo", "-json", "-ro", "-so", "-al", path.to_s)
             stdout
           end
 
@@ -48,38 +58,33 @@ module GeoDerivatives
           # the vector dataset name.
           # @return [String] vector dataset name
           def vector_name
-            match = /(?<=Layer name:\s).*?(?=\n)/.match(doc)
-            match ? match[0] : ""
+            doc.dig("layers", 0, "name") || ""
           end
 
           # Given an output string from the ogrinfo command, returns
           # the ogr driver used to read dataset.
           # @return [String] ogr driver name
           def driver_name
-            match = /(?<=driver\s`).*?(?=')/.match(doc)
-            match ? match[0] : ""
+            doc.fetch("driverShortName", "")
           end
 
           # Given an output string from the ogrinfo command, returns
           # the vector geometry type.
           # @return [String] vector geom
           def vector_geom
-            match = /(?<=Geometry:\s).*?(?=\n)/.match(doc)
-            geom = match ? match[0] : ""
+            geom = doc.dig("layers", 0, "geometryFields", 0, "type") || ""
+
             # Transform OGR-style 'Line String' into GeoJSON 'Line'
-            geom == "Line String" ? "Line" : geom
+            geom == "LineString" ? "Line" : geom
           end
 
           # Given an output string from the ogrinfo command, returns
           # the vector bounding box.
           # @return [Hash] vector bounds
           def vector_bounds
-            match = /(?<=Extent:\s).*?(?=\n)/.match(doc)
-            extent = match ? match[0] : ""
+            extent = doc.dig("layers", 0, "geometryFields", 0, "extent").map { |c| c.truncate(6) }
 
-            # remove parens and spaces, split into array, and assign elements to variables
-            w, s, e, n = extent.delete(" ").gsub(")-(", ",").delete("(").delete(")").split(",")
-            { north: n.to_f, east: e.to_f, south: s.to_f, west: w.to_f }
+            { north: extent[3].to_f, east: extent[2].to_f, south: extent[1].to_f, west: extent[0].to_f }
           end
       end
     end
