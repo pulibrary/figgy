39c4182e9f74d025aa076c1697850ce52e1ddcd6
Trey Pendragon <tpendragon@princeton.edu>
2023-09-26 09:57:29 -0700

    Delete events when PreservationObjects are deleted.
    Closes #5925

--- spec/change_sets/preservation_object_change_set_spec.rb
+++ spec/change_sets/preservation_object_change_set_spec.rb
@@ -5,10 +5,22 @@ RSpec.describe PreservationObjectChangeSet do
   subject(:change_set) { described_class.new(resource) }
 
   let(:resource) { FactoryBot.build(:preservation_object) }
+  let(:change_set_persister) { ChangeSetPersister.default }
 
   describe "#preserve?" do
     it "is not preserved" do
       expect(change_set.preserve?).to be false
+    end
+  end
+
+  describe "deletion" do
+    it "deletes events that reference it" do
+      preservation_object = FactoryBot.create_for_repository(:preservation_object)
+      event = FactoryBot.create_for_repository(:event, resource_id: preservation_object.id)
+
+      change_set_persister.delete(change_set: ChangeSet.for(preservation_object))
+
+      expect { change_set_persister.query_service.find_by(id: event.id) }.to raise_error Valkyrie::Persistence::ObjectNotFoundError
     end
   end
 end
