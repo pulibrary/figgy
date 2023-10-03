2469564e1bc9d61834dcea30059f85ff4c726e47
Shaun Ellis <shaun@sdellis.com>
2023-10-03 15:47:44 -0400

    clean up

--- app/javascript/figgy/derivative_form.js
+++ app/javascript/figgy/derivative_form.js
@@ -1,7 +1,6 @@
 export default class DerivativeForm {
   constructor() {
     this.element = $(".rederive")
-    // this.element.click(this.onclick)
     var that = this
     this.element.click(function(event) {
       event.preventDefault()
@@ -30,32 +29,3 @@ export default class DerivativeForm {
     })
   }
 }
-
-// export default class DerivativeForm {
-//   constructor() {
-//     this.form = $(".rederive")
-//     this.element = this.form.find('button')
-//     this.element.click(this.onclick)
-//   }
-//
-//   onclick(event) {
-//     $.ajax({
-//       type: "PUT",
-//       url: $(this).parents('.rederive').attr('action'),
-//       data: $(this).parents('.rederive').serializeArray(),
-//       dataType: 'json'
-//     }).done(function(data, textStatus, response) {
-//       $('.flash-message span.text')
-//         .text("Derivatives are being regenerated")
-//         .parent()
-//         .removeAttr('hidden')
-//     }).fail(function(response, textStatus, errorThrown) {
-//       $('.flash-message span.text')
-//         .text("Derivatives cannot be regenerated")
-//         .parent()
-//         .removeClass('alert-success')
-//         .addClass('alert-danger')
-//         .removeAttr('hidden')
-//     })
-//   }
-// }
