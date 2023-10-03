62a9a806adf8051ed8f244bbc2747cfad79e2def
Shaun Ellis <shaun@sdellis.com>
2023-10-03 15:28:29 -0400

    allows for generating derivatives

--- app/javascript/figgy/derivative_form.js
+++ app/javascript/figgy/derivative_form.js
@@ -1,14 +1,18 @@
 export default class DerivativeForm {
   constructor() {
-    this.form = $(".rederive")
-    this.element = this.form.find('button')
-    this.element.click(this.onclick)
+    this.element = $(".rederive")
+    // this.element.click(this.onclick)
+    var that = this
+    this.element.click(function(event) {
+      event.preventDefault()
+      that.onclick(event)
+    })
   }
 
   onclick(event) {
     $.ajax({
       type: "PUT",
-      url: $(this).parents('.rederive').attr('action'),
+      url: event.currentTarget.attributes.href.value,
       data: $(this).parents('.rederive').serializeArray(),
       dataType: 'json'
     }).done(function(data, textStatus, response) {
@@ -26,3 +30,32 @@ export default class DerivativeForm {
     })
   }
 }
+
+// export default class DerivativeForm {
+//   constructor() {
+//     this.form = $(".rederive")
+//     this.element = this.form.find('button')
+//     this.element.click(this.onclick)
+//   }
+//
+//   onclick(event) {
+//     $.ajax({
+//       type: "PUT",
+//       url: $(this).parents('.rederive').attr('action'),
+//       data: $(this).parents('.rederive').serializeArray(),
+//       dataType: 'json'
+//     }).done(function(data, textStatus, response) {
+//       $('.flash-message span.text')
+//         .text("Derivatives are being regenerated")
+//         .parent()
+//         .removeAttr('hidden')
+//     }).fail(function(response, textStatus, errorThrown) {
+//       $('.flash-message span.text')
+//         .text("Derivatives cannot be regenerated")
+//         .parent()
+//         .removeClass('alert-success')
+//         .addClass('alert-danger')
+//         .removeAttr('hidden')
+//     })
+//   }
+// }
