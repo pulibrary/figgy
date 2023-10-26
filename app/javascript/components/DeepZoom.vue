ba155a32ef840493d9f9e3d079aa31e8d189b124
Shaun Ellis <shaun@sdellis.com>
2023-10-26 12:13:51 -0400

    zoom modal finishing css touches

--- app/javascript/components/DeepZoom.vue
+++ app/javascript/components/DeepZoom.vue
@@ -1,9 +1,8 @@
 <template>
-  <wrapper class="lux-overlay lux-fullscreen">
+  <wrapper style="max-width:100%;">
     <div class="lux-modal">
-      Foobar  Foobar  Foobar  Foobar  Foobar  Foobar  Foobar  Foobar  Foobar
       <heading level="h2">
-        Zoom <small>on the selected item</small>
+        Zoom <small>on the item</small>
         <input-button
           @button-clicked="hideZoom()"
           class="expand-collapse"
@@ -130,24 +129,7 @@ small {
 
 .lux-modal {
   background: #FFFFFF;
-}
-
-.lux-overlay.lux-fullscreen {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.85);
-  background: url(data:;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAgY0hSTQAAeiYAAICEAAD6AAAAgOgAAHUwAADqYAAAOpgAABdwnLpRPAAAABl0RVh0U29mdHdhcmUAUGFpbnQuTkVUIHYzLjUuNUmK/OAAAAATSURBVBhXY2RgYNgHxGAAYuwDAA78AjwwRoQYAAAAAElFTkSuQmCC)
-    repeat scroll transparent\9; /* ie fallback png background image */
-  z-index: 9999;
-  color: white;
-  border-radius: 4px;
-  border-color: #ffffff;
+  padding: 1em;
 }
 
 .lux-osd {
@@ -165,6 +147,7 @@ small {
   padding: 10px;
   height: 20em;
   width: 100%;
+  margin: 0;
 }
 
 h3.lux-osd-title {
