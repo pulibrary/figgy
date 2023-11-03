c41f2056a411a6a9846219d6f1e7a6c7f0534aeb
Eliot Jordan <eliotj@princeton.edu>
2023-11-03 13:36:31 -0500

    Only queue up one MosaicJob per resource at a time

--- app/jobs/mosaic_job.rb
+++ app/jobs/mosaic_job.rb
@@ -4,12 +4,25 @@ class MosaicJob < ApplicationJob
   queue_as :low
   delegate :query_service, to: :metadata_adapter
 
+  attr_reader :resource_id
   def perform(resource_id)
-    resource = query_service.find_by(id: Valkyrie::ID.new(resource_id))
+    @resource_id = resource_id
+    return if currently_enqueued?
     TileMetadataService.new(resource: resource).path
   end
 
   private
+
+    def currently_enqueued?
+      queue = Sidekiq::Queue.new("low")
+      job = queue.find { |j| j.item.dig("args", 0, "job_class") == "MosaicJob" && j.item.dig("args", 0, "arguments", 0) == resource_id }
+      return true if job
+      false
+    end
+
+    def resource
+      query_service.find_by(id: Valkyrie::ID.new(resource_id))
+    end
 
     def metadata_adapter
       Valkyrie::MetadataAdapter.find(:indexing_persister)
--- spec/jobs/mosaic_job_spec.rb
+++ spec/jobs/mosaic_job_spec.rb
@@ -17,5 +17,38 @@ describe MosaicJob do
       described_class.perform_now(raster_set.id)
       expect(mosaic_service).to have_received(:path)
     end
+
+    context "when a MosaicJob is already enqueued" do
+      before do
+      end
+      it "does not run the TileMetadataService" do
+        raster_set = FactoryBot.create_for_repository(:raster_set_with_files)
+        job_item = {
+          "retry" => true,
+          "queue" => "low",
+          "class" => "ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper",
+          "wrapped" => "MosaicJob",
+          "args" =>
+                      [{ "job_class" => "MosaicJob",
+                         "job_id" => "12cfd9ff-5555-6666-8892-f44d386967d3",
+                         "provider_job_id" => nil,
+                         "queue_name" => "low",
+                         "priority" => nil,
+                         "arguments" => [raster_set.id.to_s],
+                         "executions" => 0,
+                         "exception_executions" => {},
+                         "locale" => "en",
+                         "timezone" => "UTC",
+                         "enqueued_at" => "2023-11-03T18:26:41Z" }],
+          "jid" => "73ff1c5050ce8adba5feab66",
+          "created_at" => 1_699_036_001.1491642,
+          "enqueued_at" => 1_699_036_001.1493726
+        }
+        job_record = instance_double(Sidekiq::JobRecord, item: job_item)
+        allow(Sidekiq::Queue).to receive(:new).and_return([job_record])
+        described_class.perform_now(raster_set.id)
+        expect(mosaic_service).not_to have_received(:path)
+      end
+    end
   end
 end
