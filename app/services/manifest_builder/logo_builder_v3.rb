1c5b2f637fca8fa791f5565432889cf6d746d158
Anna Headley <845363+hackartisan@users.noreply.github.com>
2023-12-06 11:13:40 -0500

    Add a comment
    Co-authored-by: Sean Warren <seanwarren77@users.noreply.github.com>

--- app/services/manifest_builder/logo_builder_v3.rb
+++ app/services/manifest_builder/logo_builder_v3.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class ManifestBuilder
+  # This class makes one small changes from ManifestBuilder::LogoBuilder, but we
+  # do not inherit from that class because it will likely be deleted as part of
+  # the work for https://github.com/pulibrary/figgy/issues/5747
   class LogoBuilderV3
     attr_reader :resource
 
