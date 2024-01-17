3834e708ddebebac363356f909561b99ceecb5f2
Shaun Ellis <shaun@sdellis.com>
2024-01-17 17:35:15 -0500

    Transforms tree into Figgy-friendly structure for saving

--- app/javascript/store/tree/index.js
+++ app/javascript/store/tree/index.js
@@ -2,6 +2,9 @@ export const treeState = {
   selected: null,
   cut: null,
   structure: {},
+  modified: false,
+  loadState: "NOT_LOADED",
+  saveState: "NOT_SAVED",
 }
 
 export const treeMutations = {
@@ -13,17 +16,29 @@ export const treeMutations = {
   },
   CREATE_FOLDER(state, structure) {
     state.structure = structure
+    state.modified = true
   },
   DELETE_FOLDER(state, structure) {
     state.structure = structure
+    state.modified = true
   },
   SAVE_LABEL(state, structure) {
     state.structure = structure
+    state.modified = true
   },
   SELECT_TREEITEM(state, selectedId) {
     state.selected = selectedId
   },
   SET_STRUCTURE(state, structureObject) {
     state.structure = structureObject
+  },
+  SET_MODIFIED(state, bool) {
+    state.modified = bool
+  },
+  CHANGE_STRUCTURE_LOAD_STATE(state, loadState) {
+    state.loadState = loadState
+  },
+  SAVED_STRUCTURE_STATE(state, saveStatus) {
+    state.saveState = saveStatus
   },
 }
