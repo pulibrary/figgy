fe5a4d6688f95ace7ad19ba3da23db353f42bddf
Trey Pendragon <tpendragon@princeton.edu>
2024-02-28 12:02:45 -0800

    Fix tests.

--- spec/controllers/downloads_controller_spec.rb
+++ spec/controllers/downloads_controller_spec.rb
@@ -2,7 +2,6 @@
 require "rails_helper"
 
 RSpec.describe DownloadsController do
-  include ActiveJob::TestHelper
   let(:meta) { Valkyrie.config.metadata_adapter }
   let(:disk) { Valkyrie.config.storage_adapter }
   let(:change_set_persister) { ChangeSetPersister.new(metadata_adapter: meta, storage_adapter: disk) }
@@ -132,21 +131,21 @@ RSpec.describe DownloadsController do
     end
 
     context "with an HLS playlist FileSet and ?as=stream" do
-      it "doesn't append an auth token if not given", run_real_derivatives: true, run_real_characterization: true do
-        stub_ezid
-        output = nil
-        perform_enqueued_jobs do
+      context "with no auth token given" do
+        with_queue_adapter :inline
+        it "doesn't append one", run_real_derivatives: true, run_real_characterization: true do
+          stub_ezid
           output = FactoryBot.create_for_repository(:scanned_resource_with_video_and_captions, state: "complete")
+          file_set = Wayfinder.for(output).file_sets.first
+          file_metadata = file_set.file_metadata.find(&:hls_manifest?)
+
+          get :show, params: { resource_id: file_set.id.to_s, id: file_metadata.id.to_s, as: "stream", format: "m3u8" }
+
+          expect(response).to be_successful
+          playlist = M3u8::Playlist.read(response.body)
+          expect(playlist.items.length).to eq 1
+          expect(playlist.items[0].uri).to eq "/downloads/#{file_set.id}/file/#{file_metadata.id}"
         end
-        file_set = Wayfinder.for(output).file_sets.first
-        file_metadata = file_set.file_metadata.find(&:hls_manifest?)
-
-        get :show, params: { resource_id: file_set.id.to_s, id: file_metadata.id.to_s, as: "stream", format: "m3u8" }
-
-        expect(response).to be_successful
-        playlist = M3u8::Playlist.read(response.body)
-        expect(playlist.items.length).to eq 1
-        expect(playlist.items[0].uri).to eq "/downloads/#{file_set.id}/file/#{file_metadata.id}"
       end
       it "creates a primary playlist with the auth token" do
         token = AuthToken.create!(group: ["admin"], label: "admin_token")
