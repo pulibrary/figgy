07667ea270b2be1a66e7979ef38edf4c77a33a38
Shaun Ellis <shaun@sdellis.com>
2024-04-03 17:21:58 -0400

    adds create new folder feature test

--- spec/features/struct_manager_spec.rb
+++ spec/features/struct_manager_spec.rb
@@ -6,8 +6,9 @@ RSpec.feature "Structure Manager", js: true do
   let(:user) { FactoryBot.create(:admin) }
   let(:adapter) { Valkyrie::MetadataAdapter.find(:indexing_persister) }
   let(:file) { fixture_file_upload("files/example.tif", "image/tiff") }
+  let(:file2) { fixture_file_upload("files/example.tif", "image/tiff") }
   let(:resource) do
-    FactoryBot.create_for_repository(:scanned_resource, files: [file])
+    FactoryBot.create_for_repository(:scanned_resource, files: [file, file2])
   end
 
   before do
@@ -19,6 +20,10 @@ RSpec.feature "Structure Manager", js: true do
     expect(page).to have_css ".lux-structManager"
     find(".folder-container > div:first-child").click
     expect(page).to have_css ".folder-container.selected"
+    find("button.lux-dropdown-button").click
+    find("button.lux-menu-item", :text=> "Create New Folder (Ctrl-n)").click
+    expect(page).to have_css("div.folder-label", text: "Untitled")
+
     #test keyboard shortcuts
     #page.native.send_keys :control, :shift, 'n'
     # expect(page).to have_text "Untitled"
