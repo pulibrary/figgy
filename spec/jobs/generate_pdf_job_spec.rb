bc7410a44f2e12c560c55a5482fc533e6b4fac5c
Trey Pendragon <tpendragon@princeton.edu>
2024-04-04 09:55:55 -0700

    Remove unneeded spec.

--- spec/jobs/generate_pdf_job_spec.rb
+++ spec/jobs/generate_pdf_job_spec.rb
@@ -3,15 +3,6 @@ require "rails_helper"
 
 describe GeneratePdfJob do
   describe "#perform" do
-    it "wraps PDFService" do
-      resource = FactoryBot.create_for_repository(:scanned_resource)
-      pdf_service_double = instance_double(PDFService)
-      allow(PDFService).to receive(:new).and_return(pdf_service_double)
-      allow(pdf_service_double).to receive(:find_or_generate)
-
-      described_class.perform_now(resource_id: resource.id)
-      expect(pdf_service_double).to have_received(:find_or_generate)
-    end
     it "broadcasts status messages" do
       file = fixture_file_upload("files/example.tif", "image/tiff")
       file2 = fixture_file_upload("files/example.tif", "image/tiff")
