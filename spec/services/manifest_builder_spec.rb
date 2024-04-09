5419e43e779435c1e6766f2e84fa280331cc4087
Sean Warren <seanwarren77@gmail.com>
2024-04-09 10:33:37 -0400

    moved numismatic manifest builder specs
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- spec/services/manifest_builder_spec.rb
+++ spec/services/manifest_builder_spec.rb
@@ -594,30 +594,6 @@ RSpec.describe ManifestBuilder do
     end
   end
 
-  context "when given a numismatic issue" do
-    subject(:manifest_builder) { described_class.new(query_service.find_by(id: numismatic_issue.id)) }
-    let(:file) { fixture_file_upload("files/example.tif", "image/tiff") }
-    let(:numismatic_issue) { FactoryBot.create_for_repository(:numismatic_issue) }
-    let(:change_set) { Numismatics::IssueChangeSet.new(numismatic_issue, member_ids: [coin1.id, coin2.id, coin3.id]) }
-    let(:coin1) { FactoryBot.create_for_repository(:coin, files: [file1]) }
-    let(:coin2) { FactoryBot.create_for_repository(:coin, files: [file2]) }
-    let(:coin3) { FactoryBot.create_for_repository(:coin) }
-    let(:file1) { fixture_file_upload("files/abstract.tiff", "image/tiff") }
-    let(:file2) { fixture_file_upload("files/abstract.tiff", "image/tiff") }
-    before do
-      numismatic_issue
-      change_set_persister.save(change_set: change_set)
-    end
-    it "builds a IIIF document with only the coins that have images" do
-      output = manifest_builder.build
-      expect(output["@type"]).to eq "sc:Collection"
-      expect(output["manifests"].length).to eq 2
-      expect(output["manifests"][0]["label"]).to eq ["Coin: 1"]
-      expect(output["manifests"][0]["@id"]).to eq "http://www.example.com/concern/numismatics/coins/#{coin1.id}/manifest"
-      expect(output["manifests"][1]["label"]).to eq ["Coin: 2"]
-    end
-  end
-
   context "when given a PDF ScannedResource", run_real_characterization: true do
     let(:file) { fixture_file_upload("files/sample.pdf", "application/pdf") }
     let(:change_set) { ScannedResourceChangeSet.new(scanned_resource, files: [file]) }
