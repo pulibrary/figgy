77528c153706b6f6a4ca29ea303facaccf69c682
Trey Pendragon <tpendragon@princeton.edu>
2024-04-15 14:24:04 -0700

    Disk adapter should retry on mount errors up to 5 times.
    Work towards #6338

---
+++ app/adapters/retrying_disk_adapter.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+class RetryingDiskAdapter
+  attr_reader :inner_storage_adapter
+  delegate :handles?, :supports?, :find_by, :delete, :upload, to: :inner_storage_adapter
+  def initialize(inner_storage_adapter)
+    @inner_storage_adapter = inner_storage_adapter
+  end
+
+  def upload(...)
+    with_rescue([Errno::EPIPE, Errno::EAGAIN], retries: 5) do
+      inner_storage_adapter.upload(...)
+    end
+  end
+
+  def with_rescue(exceptions, retries: 5)
+    try = 0
+    begin
+      yield try
+    rescue *exceptions => exc
+      try += 1
+      try <= retries ? retry : raise(exc)
+    end
+  end
+end
