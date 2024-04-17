f544ee1fd698b8c4bdbf64530dddb72d92014332
Eliot Jordan <eliotj@princeton.edu>
2024-04-17 08:56:16 -0500

    Upgrades LUX and Vue2 to Vue3 in Figgy

--- app/javascript/entrypoints/viewer.js
+++ app/javascript/entrypoints/viewer.js
@@ -1,4 +1,4 @@
-import UVManager from '@viewer/uv_manager'
+import UVManager from '@/viewer/uv_manager'
 import 'leaflet/dist/leaflet.css'
 const UVManagerInstance = new UVManager()
 let timer = window.setInterval(() => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,25 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import RubyPlugin from 'vite-plugin-ruby'
-import vue from '@vitejs/plugin-vue2'
+import vue from '@vitejs/plugin-vue'
 import { brotliCompressSync } from 'zlib'
 import gzipPlugin from 'rollup-plugin-gzip'
 
 // see compression documentation at https://vite-ruby.netlify.app/guide/deployment.html#compressing-assets-%F0%9F%93%A6
 // and https://github.com/ElMassimo/vite_ruby/discussions/101#discussioncomment-1019222
 export default defineConfig({
-  base: './',
   resolve: {
     alias: {
-      '@figgy': resolve(__dirname, 'app/javascript/figgy'),
-      '@channels': resolve(__dirname, 'app/javascript/channels'),
-      '@viewer': resolve(__dirname, 'app/javascript/viewer'),
-      '@images': resolve(__dirname, 'app/javascript/images'),
-      '@components': resolve(__dirname, 'app/javascript/components'),
-      '@helpers': resolve(__dirname, 'app/javascript/helpers'),
-      '@store': resolve(__dirname, 'app/javascript/store')
+      vue: 'vue/dist/vue.esm-bundler'
     }
   },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./test/setup.js']
+    setupFiles: ['./test/setup.js'],
+    alias: {
+      '@/': './app/javascript'
+    }
   },
   plugins: [
     RubyPlugin(),
