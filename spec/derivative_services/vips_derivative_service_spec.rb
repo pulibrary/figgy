847d129625f57c041599cb9f372ca87573d92477
Trey Pendragon <tpendragon@princeton.edu>
2024-04-25 08:16:06 -0700

    Generate derivatives for tiffs with sub-byte values.
    Closes #6359
    
    Special thanks to @jcupitt!

--- spec/derivative_services/vips_derivative_service_spec.rb
+++ spec/derivative_services/vips_derivative_service_spec.rb
@@ -73,6 +73,21 @@ RSpec.describe VIPSDerivativeService do
     end
   end
 
+  context "fractional byte value source", run_real_derivatives: true do
+    let(:file) { fixture_file_upload("files/small_fractional_tif.tif", "image/tiff") }
+
+    it "creates a tiff and attaches it to the fileset" do
+      derivative_service.new(id: valid_change_set.id).create_derivatives
+
+      reloaded = query_service.find_by(id: valid_resource.id)
+      derivative = reloaded.derivative_file
+
+      expect(derivative).to be_present
+      derivative_file = Valkyrie::StorageAdapter.find_by(id: derivative.file_identifiers.first)
+      expect(derivative_file.read).not_to be_blank
+    end
+  end
+
   context "with an existing TIFF intermediate file", run_real_derivatives: true do
     let(:storage_adapter) { Valkyrie::StorageAdapter.find(:disk_via_copy) }
     let(:scanned_resource) { FactoryBot.create_for_repository(:scanned_resource, files: [file]) }
