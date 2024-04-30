c575619bfeb42a35ed0545644b21034214f62a75
Trey Pendragon <tpendragon@princeton.edu>
2024-04-30 13:40:39 -0700

    Remove vue-details.

--- app/javascript/test/setup.js
+++ app/javascript/test/setup.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import _ from 'lodash'
-import VueDetails from 'vue-details'
 import system from 'lux-design-system'
 
 Vue.use(system)
 Vue.config.productionTip = false
-Vue.component('v-details', VueDetails)
 vi.unmock('lodash')
 _.debounce = vi.fn((fn) => fn);
 
