c6453986e70a7833791fc2522fc49e3bcf9a66ea
Eliot Jordan <eliotj@princeton.edu>
2024-05-15 16:08:36 -0500

    Create spec support file for preservation and fixity helpers

--- spec/features/health_status_spec.rb
+++ spec/features/health_status_spec.rb
@@ -79,18 +79,4 @@ RSpec.feature "Heath Status" do
     find("button", text: "Show Problematic Resources").click
     expect(page).to have_link(resource.title.first, href: /catalog\/#{resource.id}/, target: "_blank")
   end
-
-  def create_preservation_object(event_status:, resource_id:, event_type:)
-    metadata_node = FileMetadata.new(id: SecureRandom.uuid)
-    preservation_object = FactoryBot.create_for_repository(:preservation_object, preserved_object_id: resource_id, metadata_node: metadata_node)
-    FactoryBot.create_for_repository(
-      :event,
-      type: event_type,
-      status: event_status,
-      resource_id: preservation_object.id,
-      child_id: metadata_node.id,
-      child_property: :metadata_node,
-      current: true
-    )
-  end
 end
--- spec/values/health_report_spec.rb
+++ spec/values/health_report_spec.rb
@@ -416,27 +416,5 @@ RSpec.describe HealthReport do
         end
       end
     end
-
-    # rubocop:disable Metrics/MethodLength
-    def create_file_set(cloud_fixity_status:)
-      file_set = FactoryBot.create_for_repository(:file_set)
-      create_preservation_object(resource_id: file_set.id, event_status: cloud_fixity_status, event_type: :cloud_fixity)
-      file_set
-    end
-
-    def create_preservation_object(event_status:, resource_id:, event_type:)
-      metadata_node = FileMetadata.new(id: SecureRandom.uuid)
-      preservation_object = FactoryBot.create_for_repository(:preservation_object, preserved_object_id: resource_id, metadata_node: metadata_node)
-      FactoryBot.create_for_repository(
-        :event,
-        type: event_type,
-        status: event_status,
-        resource_id: preservation_object.id,
-        child_id: metadata_node.id,
-        child_property: :metadata_node,
-        current: true
-      )
-    end
-    # rubocop:enable Metrics/MethodLength
   end
 end
