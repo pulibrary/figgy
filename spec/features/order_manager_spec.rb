04c95afa934384d4a3edb61b8f5393712d4b3e36
Shaun Ellis <shaun@sdellis.com>
2024-05-22 16:46:42 -0400

    Fixed rubocop changes Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- spec/features/order_manager_spec.rb
+++ spec/features/order_manager_spec.rb
@@ -26,7 +26,7 @@ RSpec.feature "Order Manager", js: true do
     expect(page).to have_css(".lux-card-selected", text: "File Set 1")
 
     # test for updating member label
-    find_field('itemLabel', with: 'File Set 1').set('Page 1')
+    find_field("itemLabel", with: "File Set 1").set("Page 1")
     expect(page).to have_css(".lux-card > p", text: "Page 1")
 
     # test selecting multiple members
@@ -37,8 +37,8 @@ RSpec.feature "Order Manager", js: true do
     expect(page).to have_css(".lux-card-selected", text: "File Set 2")
 
     # test generating labels for multiple members
-    find_field('unitLabel').set('p.')
-    find_field('startNum').set('10')
+    find_field("unitLabel").set("p.")
+    find_field("startNum").set("10")
     # capybara throws error on all of these approaches
     # -- this is likely because the lux-input-checkbox hides the default checkbox and restyles it
     # -- checking this box reveals the bracketLocation select input which also cannot be tested with capybara
@@ -54,14 +54,14 @@ RSpec.feature "Order Manager", js: true do
     find("#twoUp option[value='true']").select_option
     expect(page).to have_css(".lux-card-selected", text: "f. 10r./10v.")
     expect(page).to have_css(".lux-card-selected", text: "f. 11r./11v.")
-    find_field('twoUpSeparator', with: '/').set('-')
+    find_field("twoUpSeparator", with: "/").set("-")
     expect(page).to have_css(".lux-card-selected", text: "f. 10r.-10v.")
     expect(page).to have_css(".lux-card-selected", text: "f. 11r.-11v.")
-    find_field('frontLabel', with: 'r.').set('(recto)')
-    find_field('backLabel', with: 'v.').set('(verso)')
+    find_field("frontLabel", with: "r.").set("(recto)")
+    find_field("backLabel", with: "v.").set("(verso)")
     expect(page).to have_css(".lux-card-selected", text: "f. 10(recto)-10(verso)")
     expect(page).to have_css(".lux-card-selected", text: "f. 11(recto)-11(verso)")
-    # note: startWith has been broken before the vue3 upgrade
+    # NOTE: startWith has been broken before the vue3 upgrade
     # see: https://github.com/pulibrary/figgy/issues/6400
     # find("#startWith option[value='back']").select_option
     # expect(page).to have_css(".lux-card-selected", text: "f. 10(verso)-10(recto)")
@@ -76,15 +76,14 @@ RSpec.feature "Order Manager", js: true do
     find("button.lux-menu-item", text: "Paste After").click
     first = page.all(".lux-card")[0]
     last = page.all(".lux-card").last
-    expect(first.has_text? ("f. 11(recto)-11(verso)")).to be(true)
-    expect(last.has_text? ("f. 10(recto)-10(verso)")).to be(true)
+    expect(first.has_text?("f. 11(recto)-11(verso)")).to be(true)
+    expect(last.has_text?("f. 10(recto)-10(verso)")).to be(true)
     expect(page).to have_css(".lux-alert-info > span", text: "Page order has changed.")
-
 
     # test the deep zoom functionality
     page.all(".lux-card")[0].click
     viewer = page.all("#viewer")[0]
-    expect(viewer.has_element? ("canvas")).to be(true)
+    expect(viewer.has_element?("canvas")).to be(true)
 
     # test that saving will provide a success notice and preserve changes
     expect(page).not_to have_css(".lux-alert", text: "Your work has been saved!")
@@ -92,6 +91,6 @@ RSpec.feature "Order Manager", js: true do
     expect(page).to have_css(".lux-alert", text: "Your work has been saved!")
     refresh
     first = page.all(".lux-card")[0]
-    expect(first.has_text? ("f. 11(recto)-11(verso)")).to be(true)
+    expect(first.has_text?("f. 11(recto)-11(verso)")).to be(true)
   end
 end
