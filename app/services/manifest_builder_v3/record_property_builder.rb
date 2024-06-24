8583c308a22858c8c5a8ee1ba94b06ae0ed576a5
Anna Headley <845363+hackartisan@users.noreply.github.com>
2024-06-24 16:46:07 -0400

    Fix v3 manifests for resources with postercanvas

--- app/services/manifest_builder_v3/record_property_builder.rb
+++ app/services/manifest_builder_v3/record_property_builder.rb
@@ -17,6 +17,7 @@ class ManifestBuilderV3
         }
       end
 
+      manifest["posterCanvas"] = poster_canvas_builder.canvas unless poster_image_record.nil?
       manifest
     end
 
@@ -26,6 +27,43 @@ class ManifestBuilderV3
 
       def viewing_direction
         (record.respond_to?(:viewing_direction) && record.send(:viewing_direction))
+      end
+
+      def poster_canvas_builder
+        canvas_builder_factory.canvas_builder_factory.new(poster_image_record, record)
+      end
+
+      def poster_image_record
+        return nil if image_file_sets.empty? && member_image_file_sets.empty?
+
+        image_file_set = if !image_file_sets.empty?
+                           image_file_sets.first
+                         else
+                           member_image_file_sets.first
+                         end
+
+        ManifestBuilder::LeafNode.new(image_file_set, record)
+      end
+
+      def image_file_sets
+        file_sets.select(&:image?)
+      end
+
+      def member_image_file_sets
+        return [] unless decorated.respond_to?(:volumes)
+
+        values = decorated.volumes.map(&:file_sets)
+        values.flatten!
+        values.select(&:image?)
+      end
+
+      def file_sets
+        return [] unless decorated.respond_to?(:file_sets)
+        decorated.file_sets
+      end
+
+      def decorated
+        resource.decorate
       end
   end
 end
