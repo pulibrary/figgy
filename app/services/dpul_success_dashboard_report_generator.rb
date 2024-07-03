22b5b09658e24c8e6d1e5cb449712faaec7ed3b4
Shaun Ellis <shaun@sdellis.com>
2024-07-03 16:39:27 -0400

    allows for custom date range

--- app/services/dpul_success_dashboard_report_generator.rb
+++ app/services/dpul_success_dashboard_report_generator.rb
@@ -10,22 +10,17 @@ class DpulSuccessDashboardReportGenerator
   end
 
   def plausible_api_request
-    # This is the API call we want, code will be modified. The Bearer token (plausible API key) must be sent for success
-    # https://plausible.io/api/v1/stats/aggregate?site_id=dpul.princeton.edu&period=custom&date=2024-06-01,2024-07-01&metrics=visitors,pageviews,bounce_rate,visit_duration
-    # Figgy.config["plausible_api_key"]
-    
     request = Faraday.new(url: 'https://plausible.io') do |conn|
       # Need help
-      #conn.request :authorization, 'Bearer', '#{Figgy.config["plausible_api_key"]}'
+      conn.request :authorization, 'Bearer', Figgy.config["plausible_api_key"]
       conn.adapter Faraday.default_adapter
       conn.headers['Content-Type'] = 'application/json'
       conn.params['site_id'] = 'dpul.princeton.edu'
-      conn.params['period'] = 'month'
-      #conn.params['date'] = @date_range.first.strftime("%Y/%m/%d") + ',' + @date_range.last.strftime("%Y/%m/%d")
+      conn.params['period'] = 'custom'
+      conn.params['date'] = @date_range.first.strftime("%Y-%m-%d") + ',' + @date_range.last.iso8601
       conn.params['metrics'] = 'visitors,pageviews,bounce_rate,visit_duration'
     end
     response = request.get("/api/v1/stats/aggregate")
-    #response = request.get("/api/v1/stats/aggregate?site_id=dpul.princeton.edu&period=custom&date=2024-06-01,2024-07-01&metrics=visitors,pageviews,bounce_rate,visit_duration")
     stats = JSON.parse(response.body).first.last
   end
 end
