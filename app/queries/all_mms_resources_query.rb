ec2f738c039b4c2e1704951e7e2186534e4853a3
Anna Headley <845363+hackartisan@users.noreply.github.com>
2024-12-06 09:40:06 -0500

    Add mms title regex query

--- app/queries/all_mms_resources_query.rb
+++ app/queries/all_mms_resources_query.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class AllMmsResourcesQuery
   def self.queries
-    [:all_mms_resources]
+    [:all_mms_resources, :mms_title_resources]
   end
 
   attr_reader :query_service
@@ -19,6 +19,19 @@ class AllMmsResourcesQuery
     relation = relation.where(internal_resource: "EphemeraFolder").invert
     relation = relation.where(created_at: created_at) if created_at
     relation = relation.where(Sequel.lit("metadata ->> 'source_metadata_identifier' ~ '99[0-9]+6421'"))
+    relation.map do |object|
+      resource_factory.to_resource(object: object)
+    end
+  end
+
+  def mms_title_resources(created_at: nil)
+    relation = orm_class.use_cursor
+    relation = relation.where(internal_resource: "FileSet").invert
+    relation = relation.where(internal_resource: "Event").invert
+    relation = relation.where(internal_resource: "PreservationObject").invert
+    relation = relation.where(internal_resource: "EphemeraFolder").invert
+    relation = relation.where(created_at: created_at) if created_at
+    relation = relation.where(Sequel.lit("metadata ->> 'title' ~ '99[0-9]+6421'"))
     relation.map do |object|
       resource_factory.to_resource(object: object)
     end
