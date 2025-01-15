f21fe4a5cee52e390f845a7e7704c6f4830cd242
Anna Headley <845363+hackartisan@users.noreply.github.com>
2025-01-15 13:47:13 -0500

    staging1 is fixed; add it back as a deploy target
    refs #6593

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-# server "figgy-web-staging1", user: "deploy", roles: %w[app db web worker]
+server "figgy-web-staging1", user: "deploy", roles: %w[app db web worker]
 server "figgy-web-staging2", user: "deploy", roles: %w[app web worker]
 set :google_fixity_request_topic, "figgy-staging-fixity-request"
 set :google_service_account, "figgy-staging@pulibrary-figgy-storage-1.iam.gserviceaccount.com"
