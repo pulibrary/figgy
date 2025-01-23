117d1015d89153280b1a75964d73293f2ec0c102
Trey Pendragon <tpendragon@princeton.edu>
2025-01-23 11:25:21 -0800

    Return to using real objects.

--- app/services/mms_report_generator/report_resource.rb
+++ app/services/mms_report_generator/report_resource.rb
@@ -2,14 +2,14 @@
 class MmsReportGenerator::ReportResource
   # Database fields we need to generate the fields for this report
   def self.resource_fields
-    [
-      :id,
-      :internal_resource,
-      Sequel[:metadata].pg_jsonb["visibility"][0].as(:visibility),
-      Sequel[:metadata].pg_jsonb["portion_note"][0].as(:portion_note),
-      Sequel[:metadata].pg_jsonb["state"][0].as(:state),
-      Sequel[:metadata].pg_jsonb["source_metadata_identifier"][0].as(:source_metadata_identifier)
-    ]
+    # [
+    #   :id,
+    #   :internal_resource,
+    #   Sequel[:metadata].pg_jsonb["visibility"][0].as(:visibility),
+    #   Sequel[:metadata].pg_jsonb["portion_note"][0].as(:portion_note),
+    #   Sequel[:metadata].pg_jsonb["state"][0].as(:state),
+    #   Sequel[:metadata].pg_jsonb["source_metadata_identifier"][0].as(:source_metadata_identifier)
+    # ]
   end
 
   attr_reader :resource
@@ -18,23 +18,25 @@ class MmsReportGenerator::ReportResource
   end
 
   def mms_id
-    resource[:source_metadata_identifier]
+    # resource[:source_metadata_identifier]
+    resource.source_metadata_identifier.first
   end
 
   def public_readable?
-    resource[:state] == "complete" || resource[:state] == "flagged"
+    # resource[:state] == "complete" || resource[:state] == "flagged"
+    resource.decorate.public_readable_state?
   end
 
   def to_hash
     {
       visibility: visibility,
-      portion_note: resource[:portion_note],
-      iiif_manifest_url: helper.manifest_url(resource[:internal_resource].constantize.new(id: resource[:id]))
+      portion_note: Array.wrap(resource.portion_note).first,
+      iiif_manifest_url: helper.manifest_url(resource)
     }
   end
 
   def visibility
-    visibility_controlled_vocabulary = ControlledVocabulary.for(:visibility).find(resource[:visibility])
+    visibility_controlled_vocabulary = ControlledVocabulary.for(:visibility).find(resource.visibility.first)
     {
       value: visibility_controlled_vocabulary.value,
       label: visibility_controlled_vocabulary.label,
