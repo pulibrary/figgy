73a48ad46b9cd0dbf6c0fc651d7b44df487f7e81
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-04-16 10:21:08 -0400

    wip

--- app/services/dspace_multivolume_ingester.rb
+++ app/services/dspace_multivolume_ingester.rb
@@ -1,55 +1,15 @@
 # frozen_string_literal: true
 class DspaceMultivolumeIngester < DspaceCollectionIngester
-  def ingest_items(**attrs)
-    persisted = []
-
-    items.each_with_index do |item, index|
-      unless @limit.nil?
-        if index >= @limit
-          @limit = 0
-          break
-        end
-      end
-      item_attrs = attrs.dup
-
-      item_handle = item["handle"]
-      logger.info "Preparing to ingest the member Item #{item_handle}..."
-      item_ingester = DspaceIngester.new(handle: item_handle, logger: @logger, dspace_api_token: @dspace_api_token)
-      # Reduces the number of API requests
-      item_ingester.id = item["id"]
-      resource = item_ingester.ingest_now!(**item_attrs)
-      persisted.append(resource)
-    end
-
-    persisted.flatten
-  end
-
-  def change_set_persister
-    ChangeSetPersister.default
-  end
-
-  def persist_resource(**attrs)
-    resource = ScannedResource.new
-    resource_change_set = change_set.new(resource)
-    raise("Invalid attributes: #{resource_change_set.errors.full_messages.to_sentence}") unless resource_change_set.validate(**attrs)
-
-    change_set_persister.save(change_set: resource_change_set)
+  def ark_url
+    "http://arks.princeton.edu/ark:/#{ark}"
   end
 
   def query_service
     Valkyrie.config.metadata_adapter.query_service
   end
 
-  def find_or_persist_resource(**attrs)
-    results = query_service.custom_queries.find_many_by_property(property: :identifier, values: [ark])
-    persisted = results.last
-    return persisted unless persisted.nil?
-
-    persist_resource(**attrs)
-  end
-
-  def ark_url
-    "http://arks.princeton.edu/ark:/#{ark}"
+  def change_set_persister
+    ChangeSetPersister.default
   end
 
   def ingest!(**attrs)
@@ -65,6 +25,7 @@ class DspaceMultivolumeIngester < DspaceCollectionIngester
     attrs[:member_ids] = member_ids
 
     @title = rest_resource["name"]
+    # This should be removed
     @publisher = @title
     attrs[:title] = @title
     attrs[:source_metadata_identifier] = source_metadata_identifier
@@ -73,4 +34,46 @@ class DspaceMultivolumeIngester < DspaceCollectionIngester
     persisted = find_or_persist_resource(**attrs)
     persisted
   end
+
+  private
+
+    def ingest_items(**attrs)
+      persisted = []
+
+      items.each_with_index do |item, index|
+        unless @limit.nil?
+          if index >= @limit
+            @limit = 0
+            break
+          end
+        end
+        item_attrs = attrs.dup
+
+        item_handle = item["handle"]
+        logger.info "Preparing to ingest the member Item #{item_handle}..."
+        item_ingester = DspaceIngester.new(handle: item_handle, logger: @logger, dspace_api_token: @dspace_api_token)
+        # Reduces the number of API requests
+        item_ingester.id = item["id"]
+        resource = item_ingester.ingest_now!(**item_attrs)
+        persisted.append(resource)
+      end
+
+      persisted.flatten
+    end
+
+    def persist_resource(**attrs)
+      resource = ScannedResource.new
+      resource_change_set = change_set.new(resource)
+      raise("Invalid attributes: #{resource_change_set.errors.full_messages.to_sentence}") unless resource_change_set.validate(**attrs)
+
+      change_set_persister.save(change_set: resource_change_set)
+    end
+
+    def find_or_persist_resource(**attrs)
+      results = query_service.custom_queries.find_many_by_property(property: :identifier, values: [ark])
+      persisted = results.last
+      return persisted unless persisted.nil?
+
+      persist_resource(**attrs)
+    end
 end
