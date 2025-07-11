b78291a3977497ed7ea6ae9c9db367829efd8847
Eliot Jordan <eliotj@princeton.edu>
2025-07-11 14:20:27 -0500

    Increase memcached item size to 3MB

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -8,6 +8,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.active_storage.service = :local
-  config.cache_store = :mem_cache_store, "figgy-web-staging1.princeton.edu"
+  config.cache_store = :mem_cache_store, "figgy-web-staging1.princeton.edu", { value_max_bytes: 3_000_000 }
   config.action_cable.allowed_request_origins = ["https://figgy-staging.princeton.edu"]
 end
