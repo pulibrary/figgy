4e3f073c8f19d851a191c309997fed0f65473f29
Trey Pendragon <tpendragon@princeton.edu>
2025-08-05 09:58:32 -0700

    Downloads Controller should handle fallbacks.
    Closes #6758

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -73,7 +73,7 @@ class DownloadsController < ApplicationController
 
   def binary_file
     return unless file_desc
-    @binary_file ||= storage_adapter.find_by(id: file_desc.file_identifiers.first)
+    @binary_file ||= Valkyrie::StorageAdapter.find_by(id: file_desc.file_identifiers.first)
   end
 
   class FileWithMetadata < Valkyrie::Resource
