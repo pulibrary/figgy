db4faed81d563e1b7e2e41edcf7668182ad3fc77
Eliot Jordan <eliot.jordan@gmail.com>
2025-11-04 09:43:32 -0600

    Improvements to make published_at migrator more resilient

--- app/services/migrations/add_published_at_migrator.rb
+++ app/services/migrations/add_published_at_migrator.rb
@@ -15,27 +15,42 @@ module Migrations
         resource_factory.to_resource(object: object)
       end
       resources.each do |resource|
-        decorator = resource.decorate
         next unless resource.respond_to?(:published_at)
+        decorator = resource.decorate
         next unless decorator.respond_to?(:published_state?) && decorator.published_state?
         change_set = ChangeSet.for(resource)
         next unless change_set.respond_to?(:published_at)
-        change_set.published_at = change_set.updated_at
+        change_set.resource.published_at = resource.updated_at
         change_set_persister.save(change_set: change_set)
+      rescue
+        next
       end
     end
 
     # Rather than make a query that's only used by this migration, just do one
     # inline here.
     def relation
-      orm_class.use_cursor
+      orm_class.use_cursor(skip_transaction: true)
                .exclude(internal_resource: [FileSet, PreservationObject, DeletionMarker, Event, EphemeraTerm].map(&:to_s))
                .where(Sequel.lit("metadata->'published_at' IS NULL"))
                .lazy
     end
 
     def change_set_persister
-      @change_set_persister ||= ChangeSetPersister.default
+      @change_set_persister ||= ::ChangeSetPersister::Basic.new(
+        metadata_adapter: Valkyrie::MetadataAdapter.find(:indexing_persister),
+        storage_adapter: Valkyrie.config.storage_adapter,
+        handlers: handlers
+      )
+    end
+
+    # Only use preservation handler for performance.
+    def handlers
+      {
+        after_save_commit: [
+          ChangeSetPersister::PreserveResource
+        ]
+      }
     end
   end
 end
