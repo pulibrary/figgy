97bb53b3314ce29217ba5478798d06c9e2cdb208
Trey Pendragon <tpendragon@princeton.edu>
2025-12-09 13:42:29 -0800

    Check the size of files before accessing them to detect Tigerdata down.
    Closes #6941

--- app/adapters/retrying_disk_adapter.rb
+++ app/adapters/retrying_disk_adapter.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 class RetryingDiskAdapter
   attr_reader :inner_storage_adapter
-  delegate :handles?, :supports?, :find_by, :delete, :upload, :base_path, to: :inner_storage_adapter
+  delegate :handles?, :supports?, :find_by, :delete, :upload, :base_path, :file_path, to: :inner_storage_adapter
   def initialize(inner_storage_adapter)
     @inner_storage_adapter = inner_storage_adapter
+  end
+
+  def find_by(id:)
+    result = inner_storage_adapter.find_by(id: id)
+    result.new(io: DelayCheckFile.new(result.io, file_path(id)))
   end
 
   def upload(**args)
