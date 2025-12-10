dcd035d85c77b9f15df543eff5bd83ccb7383824
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-12-10 12:47:33 -0500

    Add PreservationAuditRunner
    Meeting initial use cases
    
    need to handle batch success and failure / retries scenario

--- app/jobs/preservation_check_job.rb
+++ app/jobs/preservation_check_job.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
+require "json"
 
 class PreservationCheckJob
   # this job is used with a Sidekiq::Batch, so it needs to be a Sidekiq::Job
   include Sidekiq::Job
+  sidekiq_options queue: "super_low"
 
   # rubocop:disable Style/GuardClause
   def perform(resource_id, audit_id, opts = {})
@@ -11,7 +13,7 @@ class PreservationCheckJob
     # if it should't preserve we don't care about it
     return unless ChangeSet.for(resource).preserve?
 
-    if incorrectly_preserved?(resource, opts)
+    if incorrectly_preserved?(resource, prepare_opts(opts))
       PreservationCheckFailure.create(
         preservation_audit_id: audit_id,
         resource_id: resource_id
@@ -19,6 +21,11 @@ class PreservationCheckJob
     end
   end
   # rubocop:enable Style/GuardClause
+
+  def prepare_opts(opts)
+    return opts if opts.is_a?(Hash)
+    JSON.parse(opts, symbolize_names: true)
+  end
 
   # Preservation object doesn't exist, is missing a metadata or binary node, or the checksums don't match.
   def incorrectly_preserved?(resource, opts)
---
+++ app/services/preservation_audit_runner.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+# Runs preservation checks as background jobs, grouping failures under a
+# PreservationAudit object
+class PreservationAuditRunner
+  # run an entire audit of all preservable resources
+  def self.run(skip_metadata_checksum: false)
+    new(skip_metadata_checksum: skip_metadata_checksum).run
+  end
+
+  # run a new audit over the failed checks of a given audit
+  def self.rerun(audit)
+    new(rerun_audit: audit).run
+  end
+
+  attr_reader :skip_metadata_checksum, :rerun_audit
+
+  def initialize(skip_metadata_checksum: false, rerun_audit: nil)
+    @skip_metadata_checksum = skip_metadata_checksum
+    @rerun_audit = rerun_audit
+  end
+
+  def run
+    batch = Sidekiq::Batch.new
+    audit = PreservationAudit.create(
+      status: "in_process",
+      extent: determine_extent,
+      batch_id: batch.bid
+    )
+    # batch.on(:success, MyCallback, :to => user.email)
+    batch.jobs do
+      # TODO: think about sending these in slices to another job that adds the jobs
+      ids.each do |id|
+        PreservationCheckJob.perform_async(id, audit.id, job_opts)
+      end
+    end
+  end
+
+  private
+
+    def determine_extent
+      if @rerun_audit
+        "partial"
+      else
+        "full"
+      end
+    end
+
+    def ids
+      if @rerun_audit
+        @rerun_audit.preservation_check_failures.map(&:resource_id)
+      else
+        query_service.custom_queries.all_ids(except_models: unpreserved_models)
+      end
+    end
+
+    def job_opts
+      @job_opts ||= {}.tap do |h|
+        h[:skip_metadata_checksum] = true if @skip_metadata_checksum
+      end.to_json
+    end
+
+    def unpreserved_models
+      [
+        DeletionMarker,
+        Event,
+        PreservationObject,
+        CDL::ResourceChargeList
+      ]
+    end
+
+    def query_service
+      Valkyrie.config.metadata_adapter.query_service
+    end
+end
