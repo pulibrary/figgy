2d174ceb778cba0536477efc6302dd3f0b00693a
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-12-12 15:25:50 -0500

    Add callbacks for cases when jobs have errors

--- app/models/preservation_audit.rb
+++ app/models/preservation_audit.rb
@@ -4,7 +4,7 @@ class PreservationAudit < ApplicationRecord
 
   validates :batch_id, presence: true
   validates :status, inclusion: {
-    in: %w[in_process success failure], message: "%{value} is not a valid status"
+    in: %w[in_process success failure complete dead], message: "%{value} is not a valid status"
   }
   validates :extent, inclusion: {
     in: %w[full partial], message: "%{value} is not a valid extent"
