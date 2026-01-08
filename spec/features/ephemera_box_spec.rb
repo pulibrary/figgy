7683cd09ab3963f5a53c48272389bdc9f4b27c84
hackartisan <845363+hackartisan@users.noreply.github.com>
2026-01-08 10:46:06 -0500

    Add button on ephemera box show page: links to folders search result
    closes #6973

--- spec/features/ephemera_box_spec.rb
+++ spec/features/ephemera_box_spec.rb
@@ -85,4 +85,41 @@ RSpec.feature "Ephemera Boxes" do
       end
     end
   end
+
+  describe "viewing a box" do
+    # make another box with a folder in it so we can confirm these don't show
+    # up in filtered results
+    let(:ephemera_folder) do
+      FactoryBot.create_for_repository(:ephemera_folder)
+    end
+    let(:ephemera_folder2) do
+      FactoryBot.create_for_repository(:ephemera_folder)
+    end
+    let(:ephemera_box) do
+      FactoryBot.create_for_repository(:ephemera_box, member_ids: [ephemera_folder.id])
+    end
+    let(:ephemera_box2) do
+      FactoryBot.create_for_repository(:ephemera_box, member_ids: [ephemera_folder2.id])
+    end
+    let(:ephemera_project) do
+      FactoryBot.create_for_repository(:ephemera_project, member_ids: [ephemera_box.id, ephemera_box2.id])
+    end
+
+    it "links to a search result limited to folders in that specific box" do
+      # These don't get properly indexed until they're saved, and the project
+      # has to exist first
+      change_set_persister.save(change_set: ChangeSet.for(ephemera_folder))
+      change_set_persister.save(change_set: ChangeSet.for(ephemera_folder2))
+      change_set_persister.save(change_set: ChangeSet.for(ephemera_box))
+      change_set_persister.save(change_set: ChangeSet.for(ephemera_box2))
+
+      visit "/catalog/#{ephemera_box.id}"
+
+      page.click_link("View all 1 items in this box")
+
+      expect(page).to have_selector("span.constraint-value", text: "Ephemera Project Test Project")
+      expect(page).to have_selector("span.constraint-value", text: "Ephemera Box #{ephemera_box.id}")
+      expect(page).to have_selector("h3.document-title-heading", count: 1)
+    end
+  end
 end
--- spec/views/catalog/_members_ephemera_box.html.erb_spec.rb
+++ spec/views/catalog/_members_ephemera_box.html.erb_spec.rb
@@ -3,21 +3,24 @@ require "rails_helper"
 
 RSpec.describe "catalog/_members_ephemera_box" do
   context "when it's a box with folders" do
-    let(:parent) { FactoryBot.create_for_repository(:ephemera_box, member_ids: [child.id]) }
-    let(:child) { FactoryBot.create_for_repository(:ephemera_folder) }
-    let(:document) { Valkyrie::MetadataAdapter.find(:index_solr).resource_factory.from_resource(resource: parent) }
-    let(:solr_document) { SolrDocument.new(document) }
-    before do
+    it "shows them and links to them" do
+      folder = FactoryBot.create_for_repository(:ephemera_folder)
+      box = FactoryBot.create_for_repository(:ephemera_box, member_ids: [folder.id])
+      FactoryBot.create_for_repository(:ephemera_project, member_ids: [box.id])
+      document = Valkyrie::MetadataAdapter.find(:index_solr).resource_factory.from_resource(resource: box)
+      solr_document = SolrDocument.new(document)
+
       assign :document, solr_document
       render
-    end
 
-    it "shows them" do
       expect(rendered).to have_selector "h2", text: "Folders"
       expect(rendered).to have_selector "th", text: "Folder Number"
-      expect(rendered).to have_selector "table.datatable[data-ajax='/concern/ephemera_boxes/#{parent.id}/folders']"
+      expect(rendered).to have_selector "table.datatable[data-ajax='/concern/ephemera_boxes/#{box.id}/folders']"
+
+      expect(rendered).to have_link("View all 1 items in this box")
     end
   end
+
   context "when it's a project with folders" do
     let(:parent) { FactoryBot.create_for_repository(:ephemera_project, member_ids: [child.id]) }
     let(:child) { FactoryBot.create_for_repository(:ephemera_folder) }
@@ -28,10 +31,11 @@ RSpec.describe "catalog/_members_ephemera_box" do
       render
     end
 
-    it "shows them" do
+    it "shows them and doesn't link to them" do
       expect(rendered).to have_selector "h2", text: "Folders"
       expect(rendered).not_to have_selector "th", text: "Folder Number"
       expect(rendered).to have_selector "table.datatable[data-ajax='/concern/ephemera_projects/#{parent.id}/folders']"
+      expect(rendered).not_to have_link "View all 1 items"
     end
   end
 end
