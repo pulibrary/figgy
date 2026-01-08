f3045534cdd39592ea7c23466adacbb76af45d6f
Eliot Jordan <eliotj@princeton.edu>
2026-01-08 14:55:10 -0600

    Allow Logger.debug when stubbing rabbit

--- spec/support/stub_rabbit.rb
+++ spec/support/stub_rabbit.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
       # silence bunny logging
       logger = instance_double(Logger)
       allow(logger).to receive(:warn)
+      allow(logger).to receive(:debug)
       allow_any_instance_of(Bunny::Session).to receive(:init_default_logger).and_return(logger)
     end
   end
