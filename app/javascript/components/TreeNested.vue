a3b2a748f326c3da8aee5b7f63adede1a46e398a
Oh, Hey! <shaun@sdellis.com>
2026-02-05 17:01:35 -0500

    WIP: Basic nested drag and drop example is almost working.
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

---
+++ app/javascript/components/TreeNested.vue
@@ -0,0 +1,38 @@
+<template>
+  <vue-draggable
+    class="dragArea"
+    tag="ul"
+    :list="tasks"
+    :group="{ name: 'g1' }"
+    item-key="name"
+  >
+    <template #item="{ element }">
+      <li>
+        <p>{{ element.name }}</p>
+        <tree-nested :tasks="element.tasks" />
+      </li>
+    </template>
+  </vue-draggable>
+</template>
+<script>
+import { VueDraggable } from "vue-draggable-plus"
+
+export default {
+  props: {
+    tasks: {
+      required: true,
+      type: Array
+    }
+  },
+  components: {
+    VueDraggable
+  },
+  name: "tree-nested"
+};
+</script>
+<style scoped>
+.dragArea {
+  min-height: 50px;
+  outline: 1px dashed;
+}
+</style>
