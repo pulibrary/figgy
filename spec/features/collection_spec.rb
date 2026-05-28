e58d503445db9a2cd33e041a64a4603cd243e6aa
Oh, Hey! <shaun@sdellis.com>
2026-05-28 14:16:56 -0400

    Adds two tests, one for ensuring the cropper renders and one to ensure crop coordinates are preserved when collection is edited
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- spec/features/collection_spec.rb
+++ spec/features/collection_spec.rb
@@ -22,26 +22,31 @@ RSpec.feature "Collection" do
     expect(page).to have_field "Tagline", with: collection.tagline
     expect(page).to have_button "Load"
 
-    # Has hidden collection banner image url field
-    element = find('#collection_banner_image_url', visible: false)
-    expect(element.value).to eq collection.banner_image_url
-
     # renders rich text editor for description
     element = find("trix-editor > div")
     expect(element.text).to eq collection.description.first
+
+    # Has hidden collection banner image url field
+    element = find('#collection_banner_image_url', visible: false)
+    expect(element.value).to eq collection.banner_image_url
+    # This .openseadragon-container element only gets created when the Vue component successfully renders
+    expect(page).to have_css '.openseadragon-container'
+
+    fill_in "Tagline", with: "This is a short tagline."
+    # Submit the form
+    click_button "Save"
+    # Expect the banner_image_url to be the same
+    expect(page).to have_css 'li.rendered_banner_image > img[src="https://iiif-cloud.princeton.edu/iiif/2/60%2Fb5%2Fe5%2F60b5e5365600450db52dbe4d7f92b8cc%2Fintermediate_file/642,2316,3854,2569/full/0/default.jpg"]'
   end
 
-  context "with a banner image url" do
-    scenario "viewing a collection" do
+  context "viewing a collection" do
+    scenario "with a banner image url" do
       collection = FactoryBot.create_for_repository(:collection)
       visit solr_document_path(id: collection.id)
       expect(page).to have_text "Rendered Banner Image"
       expect(page).to have_css "li.rendered_banner_image > img"
     end
-  end
-
-  context "without a banner image url" do
-    scenario "viewing a collection" do
+    scenario "without a banner image url" do
       collection = FactoryBot.create_for_repository(:collection, banner_image_url: nil)
       visit solr_document_path(id: collection.id)
 
