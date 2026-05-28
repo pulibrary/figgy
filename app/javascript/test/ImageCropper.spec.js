991b24f92333b59947cf16dd8c43683cad8b88af
Oh, Hey! <shaun@sdellis.com>
2026-05-28 16:05:42 -0400

    simplifies and updates ImageCropper component test
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- app/javascript/test/ImageCropper.spec.js
+++ app/javascript/test/ImageCropper.spec.js
@@ -3,16 +3,10 @@ import ImageCropper from "../components/ImageCropper.vue"
 import OpenSeadragon from 'openseadragon'
 
 let wrapper
-let options
 
 describe("ImageCropper.vue", () => {
   beforeEach(() => {
-    wrapper = mount(ImageCropper, {
-      global: {
-        options,
-        // stubs: ["heading","input-radio","text-style"],
-      }
-    })
+    wrapper = shallowMount(ImageCropper)
 
     // We don't want to actually run the initialization, we're just here to test
     // a function
@@ -20,7 +14,6 @@ describe("ImageCropper.vue", () => {
   })
 
   it('parses an info.json url', () => {
-    // expect(wrapper.vm.parseUrl('https://iiif-cloud.princeton.edu/iiif/2/60%2Fb5%2Fe5%2F60b5e5365600450db52dbe4d7f92b8cc%2Fintermediate_file/642,2316,3854,2569/full/0/default.jpg'))
     expect(
       wrapper.vm.parseUrl('https://iiif-cloud.princeton.edu/iiif/2/60%2Fb5%2Fe5%2F60b5e5365600450db52dbe4d7f92b8cc%2Fintermediate_file/info.json')
     ).toEqual(
@@ -32,7 +25,7 @@ describe("ImageCropper.vue", () => {
 
   it('parses a full IIIF image api url', () => {
     expect(
-      wrapper.vm.parseUrl('https://iiif-cloud.princeton.edu/iiif/2/74%2Fea%2Fee%2F74eaee0426ab49a6923dd6bcc401a334%2Fintermediate_file/full/full/0/default.jpg')
+      wrapper.vm.parseUrl('https://iiif-cloud.princeton.edu/iiif/2/74%2Fea%2Fee%2F74eaee0426ab49a6923dd6bcc401a334%2Fintermediate_file/full/2131,/0/default.jpg')
     ).toEqual(
       {
         "infoUrl": "https://iiif-cloud.princeton.edu/iiif/2/74%2Fea%2Fee%2F74eaee0426ab49a6923dd6bcc401a334%2Fintermediate_file/info.json",
