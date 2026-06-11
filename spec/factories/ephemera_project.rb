0f3376127c9421aa81923642b991d6ad076e5c73
Oh, Hey! <shaun@sdellis.com>
2026-06-11 17:37:20 -0400

    update tests

--- spec/factories/ephemera_project.rb
+++ spec/factories/ephemera_project.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     publish { true }
     tagline { "project tagline" }
     description { "project extended description" }
-    banner_image_url { "https://iiif-cloud.princeton.edu/iiif/2/60%2Fb5%2Fe5%2F60b5e5365600450db52dbe4d7f92b8cc%2Fintermediate_file/642,2316,3854,2569/full/0/default.jpg" }
+    banner_image_url { "https://iiif-cloud.princeton.edu/iiif/2/60%2Fb5%2Fe5%2F60b5e5365600450db52dbe4d7f92b8cc%2Fintermediate_file/642,2316,3854,2569/750,/0/default.jpg" }
     to_create do |instance|
       Valkyrie.config.metadata_adapter.persister.save(resource: instance)
     end
