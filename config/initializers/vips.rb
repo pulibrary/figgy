595f96d87b605b1a29af4018c3c514b657b1e0fc
Trey Pendragon <tpendragon@princeton.edu>
2026-08-15 19:38:22 -0700

    Get vips/selenium working.

---
+++ config/initializers/vips.rb
@@ -0,0 +1,6 @@
+# Enable the external loaders we depend on for Figgy.
+# See https://www.libvips.org/2022/05/28/What's-new-in-8.13.html and https://github.com/libvips/ruby-vips/pull/382
+Rails.application.config.after_initialize do
+  Vips.block("VipsForeignLoadPdf", false)
+  Vips.block("VipsForeignLoadMagick7File", false)
+end
