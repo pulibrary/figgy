1aae3bc18f4fa89d814ca33be0a9075933ef004d
Trey Pendragon <tpendragon@princeton.edu>
2026-08-27 12:26:46 -0700

    Big simplication.

--- app/javascript/figgy/modal_viewer.js
+++ app/javascript/figgy/modal_viewer.js
@@ -9,24 +9,18 @@ export default class ModalViewer {
       event.preventDefault()
       let manifest_url = $(event.currentTarget).attr("data-modal-manifest")
       let osd_viewer = $("picture[data-openseadragon]")
-      let new_source = $("<source>", { class: "osd-image", src: manifest_url, media: "openseadragon" })
       $("#viewer-modal").modal()
       osd_viewer.height($(window).height()-100)
       if(this.osd !== undefined) {
-        let viewer = this.osd.data("osdViewer")
+        let viewer = this.osd
         viewer.open(manifest_url)
       } else {
         osd_viewer.html("")
-        new_source.appendTo(osd_viewer)
-        let sources = osd_viewer.find('source[media="openseadragon"]');
-        sources = $.map(sources, function(e) {
-          return $(e).attr('src')
-        })
         this.osd = OpenSeadragon({
           element: osd_viewer[0],
           prefixUrl: '',
           navImages: navImages,
-          tileSources: sources
+          tileSources: manifest_url
         })
       }
       return true
