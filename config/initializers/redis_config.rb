c9cd763ebde2135bc95a6978877339b093964aec
Trey Pendragon <tpendragon@princeton.edu>
2026-09-11 11:58:51 -0700

    Use Redis Cluster in Staging (#7358)
    * Try redis cluster.
    
    * Hmm this mattered?
    
    * Update Redis.
    
    * Move this so CI runs it.

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,20 +1,26 @@
 require "redis"
 module RedisConfig
   def config
-    @config ||= config_yaml.with_indifferent_access
+    @config ||= config_yaml
   end
 
   def url
     @url ||= "redis://#{config[:host]}:#{config[:port]}/#{config[:db]}"
   end
 
+  # Added for testing, to unset variables.
+  def reset!
+    @config = nil
+    @url = nil
+  end
+
   private
 
     def config_yaml
-      YAML.safe_load(ERB.new(IO.read(Rails.root.join("config", "redis.yml"))).result, aliases: true)[Rails.env]
+      YAML.safe_load(ERB.new(IO.read(Rails.root.join("config", "redis.yml"))).result, aliases: true, symbolize_names: true)[Rails.env.to_sym]
     end
 
-    module_function :config, :url, :config_yaml
+    module_function :config, :url, :config_yaml, :reset!
 end
 
 RedisConfig.url
---
+++ spec/lib/redis_config_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe RedisConfig do
+  describe ".config" do
+    context "when in staging" do
+      before do
+        allow(Rails).to receive(:env).and_return("staging")
+        RedisConfig.reset!
+      end
+      after do
+        RedisConfig.reset!
+      end
+      it "returns a config with sentinels" do
+        expect(described_class.config[:sentinels]).not_to be_blank
+        expect(described_class.config[:sentinels][0][:host]).not_to be_blank
+      end
+    end
+  end
+end
