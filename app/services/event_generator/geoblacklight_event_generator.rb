269a874622a8c07e408ec6a7ad966f6158bda2a5
Eliot Jordan <eliot.jordan@gmail.com>
2026-09-14 12:54:33 -0500

    Put bulk Pulmap indexing on super_low queue

--- app/services/event_generator/geoblacklight_event_generator.rb
+++ app/services/event_generator/geoblacklight_event_generator.rb
@@ -14,7 +14,7 @@ class EventGenerator
     def record_created(record); end
 
     def record_deleted(record)
-      PulmapDeleteJob.perform_later(slug: slug(record), commit: commit?)
+      PulmapDeleteJob.set(queue: sidekiq_queue).perform_later(slug: slug(record), commit: commit?)
     end
 
     def record_updated(record)
@@ -22,7 +22,7 @@ class EventGenerator
       if state == "takedown"
         record_deleted(record)
       elsif state == "complete"
-        PulmapIndexJob.perform_later(document: document(record), commit: commit?)
+        PulmapIndexJob.set(queue: sidekiq_queue).perform_later(document: document(record), commit: commit?)
       end
     end
 
@@ -39,6 +39,14 @@ class EventGenerator
     end
 
     private
+
+      def sidekiq_queue
+        if bulk
+          :super_low
+        else
+          :high
+        end
+      end
 
       def commit?
         !bulk
